d3bc782c16f26b7c1701b9a9ae3b71602e4b0a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:47 +0000

    Add package drf-standardized-responses/0.1.0/drf_standardized_responses-0.1.0-py3-none-any.whl

---
+++ packages/drf-standardized-responses/drf_standardized_responses-0.1.0-py3-none-any.whl/drf_standardized_responses/__init__.py
@@ -0,0 +1,7 @@
+"""
+DRF ResponseKit - Standardized API Response Framework for Django REST Framework
+"""
+
+__version__ = '0.1.0'
+
+default_app_config = 'drf_standardized_responses.apps.DrfStandardizedResponsesConfig'
