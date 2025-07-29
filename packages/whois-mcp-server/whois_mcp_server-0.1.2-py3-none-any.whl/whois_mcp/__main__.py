87f1fb2358895b09960e1248a3a408538b9a00ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:34 +0000

    Add package whois-mcp-server/0.1.2/whois_mcp_server-0.1.2-py3-none-any.whl

---
+++ packages/whois-mcp-server/whois_mcp_server-0.1.2-py3-none-any.whl/whois_mcp/__main__.py
@@ -0,0 +1,12 @@
+from mcp.server.fastmcp import FastMCP
+from whois_mcp.whois_api import whois_query
+
+# MCP 实例
+mcp = FastMCP("whois-mcp-server")
+
+@mcp.tool()
+def mcp_whois_query(domain: str):
+    return whois_query(domain)
+
+if __name__ == "__main__":
+    mcp.run(transport='sse')
