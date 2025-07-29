ca38db5227a6efca4030ebb788c580527a769602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:19 +0000

    Add package zerocut/0.1.9/zerocut-0.1.9-py3-none-any.whl

---
+++ packages/zerocut/zerocut-0.1.9-py3-none-any.whl/zerocut/frames/sample.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+import os
+
+from ..frame import Frame
+
+
+class SimpleWhiteFrame(Frame):
+    def __init__(self):
+        super().__init__(
+            frame_image=Image.open(os.path.join(os.path.dirname(__file__), "samples", "white.png")).convert("RGBA"),
+            positions=[(64, 64), (64, 628), (64, 1192), (64, 1756)],
+            photo_size=(876, 500),
+        )
+
+
+class SimpleBlackFrame(Frame):
+    def __init__(self):
+        super().__init__(
+            frame_image=Image.open(os.path.join(os.path.dirname(__file__), "samples", "black.png")).convert("RGBA"),
+            positions=[(64, 64), (64, 628), (64, 1192), (64, 1756)],
+            photo_size=(876, 500),
+        )
