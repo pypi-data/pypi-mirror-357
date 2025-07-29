99446802e24174ae7e26b74ffc2213c15c149b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:56 +0000

    Add package calicropyield/0.1.1/calicropyield-0.1.1.tar.gz

---
+++ packages/calicropyield/calicropyield-0.1.1.tar.gz/calicropyield-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calicropyield",
+    version="0.1.1",
+    description="A multi-modal data downloader and processing library for California crop yield benchmarking",
+    author="Hamid Kamangir",
+    author_email="hamid.kamangir@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pandas",
+        "xarray",
+        "rasterio",
+        "geopandas",
+        "shapely",
+        "gdown",
+        "numpy"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
