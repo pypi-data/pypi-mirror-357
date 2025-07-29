0620ce318bd00cb1770e67dfd37b33f2f88fa133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:25 +0000

    Add package dodo-lookup/0.2.3/dodo_lookup-0.2.3.tar.gz

---
+++ packages/dodo-lookup/dodo_lookup-0.2.3.tar.gz/dodo_lookup-0.2.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Long description for PyPI (from README.md)
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="dodo-lookup",
+    version="0.2.3",
+    description="A minimal and colorful terminal tool to check domain availability across TLDs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Oguzhan Budak",
+    url="https://github.com/yourusername/dodo",  # istersen GitHub linkin
+    packages=find_packages(),
+    install_requires=[
+        "python-whois",
+        "termcolor",
+        "pyfiglet",
+        "prettytable",
+        "tqdm"
+    ],
+    package_data={
+        "dodo": ["tlds.txt"],
+    },
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "dodo = dodo.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Topic :: Internet :: Name Service (DNS)",
+    ],
+    python_requires=">=3.7",
+)
