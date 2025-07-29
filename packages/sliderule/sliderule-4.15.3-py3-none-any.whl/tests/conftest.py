983e30197c69e5aeb81eaa6f4041c73f4c8affa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:42 +0000

    Add package sliderule/4.15.3/sliderule-4.15.3-py3-none-any.whl

---
+++ packages/sliderule/sliderule-4.15.3-py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,47 @@
+import pytest
+import logging
+import sliderule
+
+logging.basicConfig(level=logging.DEBUG)
+
+def pytest_addoption(parser):
+    parser.addoption("--domain", action="store", default="slideruleearth.io")
+    parser.addoption("--organization", action="store", default="sliderule")
+    parser.addoption("--desired_nodes", action="store", default=None)
+    parser.addoption("--performance", action="store_true", default=False)
+
+@pytest.fixture(scope='session')
+def domain(request):
+    value = request.config.option.domain
+    if value is None:
+        pytest.skip()
+    return value
+
+@pytest.fixture(scope='session')
+def organization(request):
+    value = request.config.option.organization
+    if value == "None":
+        value = None
+    return value
+
+@pytest.fixture(scope='session')
+def desired_nodes(request):
+    value = request.config.option.desired_nodes
+    if value is not None:
+        if value == "None":
+            value = None
+        else:
+            value = int(value)
+    return value
+
+@pytest.fixture(scope='function')
+def init(domain, organization, desired_nodes):
+    return sliderule.init(domain, verbose=True, loglevel=logging.DEBUG, organization=organization, desired_nodes=desired_nodes)
+
+@pytest.fixture(scope='session')
+def performance(request):
+    return request.config.option.performance
+
+def pytest_runtest_setup(item):
+    if item.get_closest_marker("external"):
+        item.add_marker(pytest.mark.xfail(reason="external service", strict=False))
