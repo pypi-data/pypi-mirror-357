f4ee5d62c0eb44a1ccda78ff4bafbbea7285bc3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:22 +0000

    Add package comfyui-manager/4.0.0b2/comfyui_manager-4.0.0b2-py3-none-any.whl

---
+++ packages/comfyui-manager/comfyui_manager-4.0.0b2-py3-none-any.whl/comfyui_manager/__init__.py
@@ -0,0 +1,45 @@
+import os
+import logging
+
+def prestartup():
+    from . import prestartup_script  # noqa: F401
+    logging.info('[PRE] ComfyUI-Manager')
+
+
+def start():
+    from comfy.cli_args import args
+
+    logging.info('[START] ComfyUI-Manager')
+    from .common import cm_global     # noqa: F401
+
+    if not args.disable_manager:
+        if args.enable_manager_legacy_ui:
+            try:
+                from .legacy import manager_server  # noqa: F401
+                from .legacy import share_3rdparty  # noqa: F401
+                import nodes
+
+                logging.info("[ComfyUI-Manager] Legacy UI is enabled.")
+                nodes.EXTENSION_WEB_DIRS['comfyui-manager-legacy'] = os.path.join(os.path.dirname(__file__), 'js')
+            except Exception as e:
+                print("Error enabling legacy ComfyUI Manager frontend:", e)
+        else:
+            from .glob import manager_server  # noqa: F401
+            from .glob import share_3rdparty  # noqa: F401
+
+
+def should_be_disabled(fullpath:str) -> bool:
+    """
+    1. Disables the legacy ComfyUI-Manager.
+    2. The blocklist can be expanded later based on policies.
+    """
+    from comfy.cli_args import args
+
+    if not args.disable_manager:
+        # In cases where installation is done via a zip archive, the directory name may not be comfyui-manager, and it may not contain a git repository.
+        # It is assumed that any installed legacy ComfyUI-Manager will have at least 'comfyui-manager' in its directory name.
+        dir_name = os.path.basename(fullpath).lower()
+        if 'comfyui-manager' in dir_name:
+            return True
+
+    return False
