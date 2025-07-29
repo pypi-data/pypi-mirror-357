6bdbcb7a22877b7c49825ad2fb5124e32f72492c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:38 +0000

    Add package selenium-auto-refresh/1.0.0/selenium_auto_refresh-1.0.0-py3-none-any.whl

---
+++ packages/selenium-auto-refresh/selenium_auto_refresh-1.0.0-py3-none-any.whl/selenium_auto_refresh/__init__.py
@@ -0,0 +1,11 @@
+"""
+Selenium Auto Refresh - Automatically refresh Chrome tabs at specified intervals
+"""
+
+__version__ = "1.0.0"
+__author__ = "Lalit Pal"
+__email__ = "happytaak8@gmail.com"
+
+from .refresher import ChromeRefresher
+
+__all__ = ["ChromeRefresher"]
