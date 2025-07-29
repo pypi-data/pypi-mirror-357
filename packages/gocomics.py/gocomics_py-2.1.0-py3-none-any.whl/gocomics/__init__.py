087df7e4956de17cc4bd63bf692c6b4f57c88f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:33 +0000

    Add package gocomics.py/2.1.0/gocomics_py-2.1.0-py3-none-any.whl

---
+++ packages/gocomics.py/gocomics_py-2.1.0-py3-none-any.whl/gocomics/__init__.py
@@ -0,0 +1,20 @@
+"""
+GoComics Scraper
+~~~~~~~~~~~~~~~~~~~
+
+A basic scraper for GoComics.
+
+:copyright: (c) 2025 Omkaar
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = 'gocomics'
+__author__ = 'Omkaar'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2025 Omkaar'
+__version__ = '2.1.0'
+
+
+from .comic import *
+from .utils import *
