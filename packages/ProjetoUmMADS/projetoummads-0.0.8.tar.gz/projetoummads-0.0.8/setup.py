f15b461eaa9e65ccb4b13bc7f6bb4bf6435f3c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:39 +0000

    Add package ProjetoUmMADS/0.0.8/projetoummads-0.0.8.tar.gz

---
+++ packages/ProjetoUmMADS/projetoummads-0.0.8.tar.gz/projetoummads-0.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ProjetoUmMADS',
+    version='0.0.8',
+    author='JoaoPinheiro1311',
+    author_email='a040698@ipmaia.pt',
+    description='Projeto de MADS - Grupo 7 - Gestão de Stocks',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/JoaoPinheiro1311/ProjetoUmMADS',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
