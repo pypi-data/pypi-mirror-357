cffaaeef240084220a81b10da33466db47487621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:29 +0000

    Add package hyundai-kia-connect-api/3.42.0/hyundai_kia_connect_api-3.42.0.tar.gz

---
+++ packages/hyundai-kia-connect-api/hyundai_kia_connect_api-3.42.0.tar.gz/hyundai_kia_connect_api-3.42.0/hyundai_kia_connect_api/utils.py
@@ -0,0 +1,76 @@
+# pylint:disable=bare-except,missing-function-docstring,invalid-name,broad-exception-caught
+"""utils.py"""
+
+import datetime
+import re
+
+
+def get_child_value(data, key):
+    value = data
+    for x in key.split("."):
+        try:
+            value = value[x]
+        except Exception:
+            try:
+                value = value[int(x)]
+            except Exception:
+                value = None
+    return value
+
+
+def get_float(value):
+    if value is None:
+        return None
+    if isinstance(value, float):
+        return value
+    if isinstance(value, int):
+        return float(value)
+    if isinstance(value, str):
+        try:
+            return float(value)
+        except ValueError:
+            return value  # original fallback
+    return value  # original fallback
+
+
+def get_hex_temp_into_index(value):
+    if value is not None:
+        value = value.replace("H", "")
+        value = int(value, 16)
+        return value
+    else:
+        return None
+
+
+def get_index_into_hex_temp(value):
+    if value is not None:
+        value = hex(value).split("x")
+        value = value[1] + "H"
+        value = value.zfill(3).upper()
+        return value
+    else:
+        return None
+
+
+def parse_datetime(value, timezone) -> datetime.datetime:
+    if value is None:
+        return datetime.datetime(2000, 1, 1, tzinfo=timezone)
+
+    value = value.replace("-", "").replace("T", "").replace(":", "").replace("Z", "")
+    m = re.match(r"(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})", value)
+    return datetime.datetime(
+        year=int(m.group(1)),
+        month=int(m.group(2)),
+        day=int(m.group(3)),
+        hour=int(m.group(4)),
+        minute=int(m.group(5)),
+        second=int(m.group(6)),
+        tzinfo=timezone,
+    )
+
+
+def get_safe_local_datetime(date: datetime) -> datetime:
+    """get safe local datetime"""
+    if date is not None and hasattr(date, "tzinfo") and date.tzinfo is not None:
+        date = date.astimezone()
+    return date
