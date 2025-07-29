24f3d77b95ee1591f770a650345dcbb0918f5846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:28 +0000

    Add package TheAcademyAI/1.0.0/TheAcademyAI-1.0.0.tar.gz

---
+++ packages/TheAcademyAI/TheAcademyAI-1.0.0.tar.gz/TheAcademyAI-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='TheAcademyAI',
+    version='1.0.0',
+    description='The Academy AI for beginners course',
+    py_modules=["AcademyAI"],
+    package_dir={'':'src'},
+)
