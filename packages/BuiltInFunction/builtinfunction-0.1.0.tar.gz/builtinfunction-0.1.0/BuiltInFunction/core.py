6e37ef0bc035ba7a3cf5d5301ce28ffcfce6327a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:36 +0000

    Add package BuiltInFunction/0.1.0/builtinfunction-0.1.0.tar.gz

---
+++ packages/BuiltInFunction/builtinfunction-0.1.0.tar.gz/builtinfunction-0.1.0/BuiltInFunction/core.py
@@ -0,0 +1,3 @@
+# mybuiltinfunc/core.py
+def say_hello(name):
+    return f"Hello, {name}!"
---
+++ packages/BuiltInFunction/builtinfunction-0.1.0.tar.gz/builtinfunction-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BuiltInFunction",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Shahbaz",
+    description="Custom built-in-like function",
+    python_requires=">=3.6"
+)
