b774c29bf8001e738752c7f2fac68d9c67f0aa9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:11 +0000

    Add package aipymemtimeprofiler/0.6.0/aipymemtimeprofiler-0.6.0.tar.gz

---
+++ packages/aipymemtimeprofiler/aipymemtimeprofiler-0.6.0.tar.gz/aipymemtimeprofiler-0.6.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aipymemtimeprofiler",
+    version="0.6.0",
+    author="Nikhil Lingadhal",
+    description="AI-enabled time and memory profiler for Python applications",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nikhillingadhal1999/AIPyMemTimeProfiler",
+    license="MIT",
+    packages=find_packages(),  # auto-detects submodules in aipymemtimeprofiler/
+    install_requires=[
+        "line_profiler==4.2.0",
+        "psutil==7.0.0",
+        "Pympler==1.1",
+        "rich",
+        "Flask",
+        "memory_profiler",
+        "setuptools",
+        "wheel",
+        "twine",
+        "ollama",
+        "packaging>=23.2,<25"
+    ],
+    entry_points={
+        "console_scripts": [
+            "profile_code = aipymemtimeprofiler.executable.execute:run_with_profiler"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
