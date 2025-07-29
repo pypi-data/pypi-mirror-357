047d643687fdceddbb2f4adcb6eba65156084016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:44 +0000

    Add package json-loader-with-include/0.3.2/json_loader_with_include-0.3.2.tar.gz

---
+++ packages/json-loader-with-include/json_loader_with_include-0.3.2.tar.gz/json_loader_with_include-0.3.2/json_loader_with_include/__init__.py
@@ -0,0 +1,9 @@
+import warnings
+
+warnings.warn(
+    "DEPRECATED: 'json-loader-with-include' has been renamed to 'json-plus'. "
+    "Please switch to 'pip install json-plus' for future updates. "
+    "This package will no longer receive updates.",
+    DeprecationWarning,
+    stacklevel=2
+)
