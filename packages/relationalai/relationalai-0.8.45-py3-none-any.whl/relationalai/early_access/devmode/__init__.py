21584435522e4e7febfb4903e2800efe8055bcb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:58 +0000

    Add package relationalai/0.8.45/relationalai-0.8.45-py3-none-any.whl

---
+++ packages/relationalai/relationalai-0.8.45-py3-none-any.whl/relationalai/early_access/devmode/__init__.py
@@ -0,0 +1,4 @@
+from .compiler import Compiler
+from .executor import Executor
+
+__all__ = ["Compiler", "Executor"]
---
+++ packages/relationalai/relationalai-0.8.45-py3-none-any.whl/relationalai/early_access/rel/rewrite/__init__.py
@@ -0,0 +1,11 @@
+from .cdc import CDC
+from .extract_common import ExtractCommon
+from .flatten import Flatten
+from .quantify_vars import QuantifyVars
+
+__all__ = [
+    "CDC",
+    "ExtractCommon",
+    "Flatten",
+    "QuantifyVars",
+]
