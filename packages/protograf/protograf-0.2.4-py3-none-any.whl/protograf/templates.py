c2c281a76df344ab8960c1c8aecbe9a9ca66c482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:53 +0000

    Add package protograf/0.2.4/protograf-0.2.4-py3-none-any.whl

---
+++ packages/protograf/protograf-0.2.4-py3-none-any.whl/protograf/templates.py
@@ -0,0 +1,23 @@
+"""
+Pre-defined templates for protograf
+"""
+
+# lib
+# third party
+from reportlab.lib.units import cm, inch
+
+# project
+
+
+CARDS = [
+    {"poker": {"height": 8.8, "width": 6.3, "units": cm}},
+    {"tarot": {"height": 13.0, "width": 7.5, "units": cm}},
+]
+TILES = [
+    {"catan": {"height": 4.5, "width": 4.5, "units": cm}},
+    {"bluemoon": {"height": 5.0, "width": 5.0, "units": cm}},
+]
+COUNTERS = [
+    {"AH": {"height": 0.5, "width": 0.5, "units": inch}},
+    {"SPI": {"height": 0.5, "width": 0.5, "units": inch}},
+]
