34a248e0e12d9d0b3fa331872f0334d74921fc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:33 +0000

    Add package crazyagent/1.0.4/crazyagent-1.0.4-py3-none-any.whl

---
+++ packages/crazyagent/crazyagent-1.0.4-py3-none-any.whl/crazyagent/toolkit/__init__.py
@@ -0,0 +1,16 @@
+from ._external import get_weather, search_image
+from ._private import (
+    send_email, 
+    configure_email_service,
+    fetch_and_save,
+    configure_save_dir
+)
+
+__all__ = [
+    'get_weather',
+    'search_image',
+    'send_email',
+    'configure_email_service',
+    'fetch_and_save',
+    'configure_save_dir'
+]
