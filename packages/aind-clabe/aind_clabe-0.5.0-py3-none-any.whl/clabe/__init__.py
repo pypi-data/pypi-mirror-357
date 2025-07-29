1fb706633cf88bdbd55d5c46445b1f88e8c1d4c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:50 +0000

    Add package aind-clabe/0.5.0/aind_clabe-0.5.0-py3-none-any.whl

---
+++ packages/aind-clabe/aind_clabe-0.5.0-py3-none-any.whl/clabe/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.5.0"
+
+import logging
+
+from .logging_helper import datetime_fmt, fmt, rich_handler
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=fmt,
+    datefmt=datetime_fmt,
+    handlers=[rich_handler],
+)
