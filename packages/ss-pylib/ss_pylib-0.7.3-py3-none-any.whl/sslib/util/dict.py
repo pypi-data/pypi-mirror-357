a3091eb499cf6f8b9a75cca5a12ffd1a0541e5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:09 +0000

    Add package ss-pylib/0.7.3/ss_pylib-0.7.3-py3-none-any.whl

---
+++ packages/ss-pylib/ss_pylib-0.7.3-py3-none-any.whl/sslib/util/dict.py
@@ -0,0 +1,12 @@
+class DictUtil:
+  @staticmethod
+  def get(key:str, source:dict, fallback:any = None) -> any:
+    return source[key] if source[key] is not None else fallback
+
+  @staticmethod
+  def clean(source:dict) -> dict:
+    ret = {}
+    for k, v in source.items():
+      if v is not None:
+        ret[k] = v
+    return ret
