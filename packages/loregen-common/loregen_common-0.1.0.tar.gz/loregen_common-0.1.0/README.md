ec44bb4319a1a5520eb5eb33ca36631e9b2857c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:07 +0000

    Add package loregen-common/0.1.0/loregen_common-0.1.0.tar.gz

---
+++ packages/loregen-common/loregen_common-0.1.0.tar.gz/loregen_common-0.1.0/README.md
@@ -0,0 +1,25 @@
+# loregen-common
+
+Common classes for the loregen project.
+
+## Installation
+
+```bash
+pip install loregen-common
+```
+
+## Usage
+
+```python
+from loregen.backend.common import models, utils, openrouter
+
+# Use the common utilities and models
+```
+
+## Development
+
+This package is built using setuptools and can be installed in development mode:
+
+```bash
+pip install -e .
+``` 
---
+++ packages/loregen-common/loregen_common-0.1.0.tar.gz/loregen_common-0.1.0/src/loregen/backend/common/utils.py
@@ -0,0 +1,14 @@
+# from langchain.chat_models import init_chat_model
+from langchain_core.language_models import BaseChatModel
+from loregen.backend.common import models
+
+
+def load_chat_model(fully_specified_name: str) -> BaseChatModel:
+    """Load a chat model from a fully specified name.
+
+    Args:
+        fully_specified_name (str): String in the format 'provider/model'.
+    """
+    # provider, model = fully_specified_name.split("/", maxsplit=1)
+    # return init_chat_model(model, model_provider=provider)
+    return models[fully_specified_name]
