714625e1a490530b1a3978ae804814f95bd44767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:27 +0000

    Add package keras-hub-nightly/0.22.0.dev202506230420/keras_hub_nightly-0.22.0.dev202506230420.tar.gz

---
+++ packages/keras-hub-nightly/keras_hub_nightly-0.22.0.dev202506230420.tar.gz/keras_hub_nightly-0.22.0.dev202506230420/keras_hub/src/models/mistral/mistral_presets.py
@@ -0,0 +1,29 @@
+"""Mistral model preset configurations."""
+
+# Metadata for loading pretrained model weights.
+backbone_presets = {
+    "mistral_7b_en": {
+        "metadata": {
+            "description": "Mistral 7B base model",
+            "params": 7241732096,
+            "path": "mistral",
+        },
+        "kaggle_handle": "kaggle://keras/mistral/keras/mistral_7b_en/8",
+    },
+    "mistral_instruct_7b_en": {
+        "metadata": {
+            "description": "Mistral 7B instruct model",
+            "params": 7241732096,
+            "path": "mistral",
+        },
+        "kaggle_handle": "kaggle://keras/mistral/keras/mistral_instruct_7b_en/8",
+    },
+    "mistral_0.2_instruct_7b_en": {
+        "metadata": {
+            "description": "Mistral 7B instruct Version 0.2 model",
+            "params": 7241732096,
+            "path": "mistral",
+        },
+        "kaggle_handle": "kaggle://keras/mistral/keras/mistral_0.2_instruct_7b_en/3",
+    },
+}
