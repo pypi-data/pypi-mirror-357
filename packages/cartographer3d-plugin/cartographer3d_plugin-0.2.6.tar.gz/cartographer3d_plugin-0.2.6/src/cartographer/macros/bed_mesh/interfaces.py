b04e1736114fedec17f68c47bd8b3e873e58fabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:08 +0000

    Add package cartographer3d-plugin/0.2.6/cartographer3d_plugin-0.2.6.tar.gz

---
+++ packages/cartographer3d-plugin/cartographer3d_plugin-0.2.6.tar.gz/cartographer3d_plugin-0.2.6/src/cartographer/macros/bed_mesh/interfaces.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Iterator, Protocol
+
+from typing_extensions import TypeAlias
+
+if TYPE_CHECKING:
+    from cartographer.interfaces.printer import Position
+
+Point: TypeAlias = "tuple[float, float]"
+
+
+class PathGenerator(Protocol):
+    def generate_path(self, points: list[Point]) -> Iterator[Point]: ...
+
+
+class BedMeshAdapter(Protocol):
+    def apply_mesh(self, mesh_points: list[Position], profile_name: str | None = None): ...
