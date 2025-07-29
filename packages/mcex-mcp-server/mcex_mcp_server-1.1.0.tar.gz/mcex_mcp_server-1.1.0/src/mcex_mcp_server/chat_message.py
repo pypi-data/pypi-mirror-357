b2dacef2e1fd32dd0b204783cd22c529eb0cf5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:12 +0000

    Add package mcex-mcp-server/1.1.0/mcex_mcp_server-1.1.0.tar.gz

---
+++ packages/mcex-mcp-server/mcex_mcp_server-1.1.0.tar.gz/mcex_mcp_server-1.1.0/src/mcex_mcp_server/chat_message.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import json
+
+# ACCESS_TOKEN =  "pat_GUtnXTPjUrwqiSNEnwIr78rNRk6rUQiO1G866gfk27cqeOLBSpe9VMLu9zH7Kvjh"
+def chat(content, bot_id, access_token):
+    url = "https://api.coze.cn/v3/chat"
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "bot_id": bot_id,
+        "user_id": "123456789",
+        "stream": False,
+        "auto_save_history": True,
+        "additional_messages": [
+            {
+                "role": "user",
+                "content": content,
+                "content_type": "text"
+            }
+        ]
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.json()
+
+def retrieve(chat_id,conversation_id, access_token):
+    url = 'https://api.coze.cn/v3/chat/retrieve'
+    params = {
+        'chat_id': chat_id,
+        'conversation_id': conversation_id
+    }
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    return response.json()
+
+def list_message(chat_id,conversation_id, access_token):
+    url = f"https://api.coze.cn/v3/chat/message/list?chat_id={chat_id}&conversation_id={conversation_id}"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+if __name__ == '__main__':
+    print( list_message("7518999110549143567", "7518999110549127183"))
+
