5e6c71cdf38ea86388f3f63d9fc3fc371c3ebee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:21 +0000

    Add package fabric-maverick/0.1.0/fabric_maverick-0.1.0.tar.gz

---
+++ packages/fabric-maverick/fabric_maverick-0.1.0.tar.gz/fabric_maverick-0.1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fabric_maverick',
+    version='0.1.0',
+    description='A Fabric Package for Semantic/Dataset validation',
+    author='Nisarg Patel',
+    author_email='nisargp@maqsoftware.com',
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+        "Framework :: Jupyter"
+    ],
+    python_requires=">=3.10,<3.12"
+)
+
+# python3 setup.py sdist
+# python3 -m build 
+# python3 -m  twine upload dist/* 
+
+#“major.minor” versioning with developmental releases, release candidates and post-releases for minor corrections:
+# 0.9
+# 1.0.dev1
+# 1.0.dev2
+# 1.0.dev3
+# 1.0.dev4
+# 1.0c1
+# 1.0c2
+# 1.0
+# 1.0.post1
+# 1.1.dev1
+# license-files = ["LICENSE", "AUTHORS.md"]
