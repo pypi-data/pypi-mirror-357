c01485b9e0015d88720cc6e3ad2f206a1b6af362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:13 +0000

    Add package pydreo-cloud/0.0.3/pydreo_cloud-0.0.3-py3-none-any.whl

---
+++ packages/pydreo-cloud/pydreo_cloud-0.0.3-py3-none-any.whl/dreo/__init__.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------
+# Library to login to Dreo cloud, get device list and device status information.
+#
+# (C) 2025 Brooke Wang
+# Released under MIT License
+# email developer@dreo.com
+# -----------------------------------------------------------
