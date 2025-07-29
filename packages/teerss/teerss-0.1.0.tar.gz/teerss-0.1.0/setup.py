3b044e4a794994b1e166b2ea1991dbc807b1dc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:28 +0000

    Add package teerss/0.1.0/teerss-0.1.0.tar.gz

---
+++ packages/teerss/teerss-0.1.0.tar.gz/teerss-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="teerss",
+    version="0.1.0",
+    author="Keith Henderson",
+    description="A terminal-based RSS reader built with Rich",
+    packages=find_packages(),
+    install_requires=[
+        "feedparser",
+        "requests",
+        "readability-lxml",
+        "beautifulsoup4",
+        "rich",
+    ],
+    entry_points={"console_scripts": ["teerss = teerss.main:main"]},
+    python_requires=">=3.7",
+)
