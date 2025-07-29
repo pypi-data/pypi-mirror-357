28fdba6465e7b2607284fa106cbbd5fc81e1d8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:26 +0000

    Add package alina3864-pqr/0.1.1/alina3864_pqr-0.1.1.tar.gz

---
+++ packages/alina3864-pqr/alina3864_pqr-0.1.1.tar.gz/alina3864_pqr-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alina3864-pqr",   # <- MUST be a unique name
+    version="0.1.1",
+    packages=find_packages(),
+    description="A test package",
+    
+    author="Alina Kaushar Mansuri",
+    author_email="your@email.com",
+    url="https://github.com/yourname/repo",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
