c31c0607bdcce861c3ab03ad880f8d07fb5bcd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:11 +0000

    Add package ultrasound-acquisition-configuration/1.2.0/ultrasound_acquisition_configuration-1.2.0-cp39-cp39-win32.whl

---
+++ packages/ultrasound-acquisition-configuration/ultrasound_acquisition_configuration-1.2.0-cp39-cp39-win32.whl/ultrasound_acquisition_configuration/include/Uac-1.2.0_static/uac/excitation.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <urx/excitation.h>
+
+#include <uac/detail/compare.h>  // IWYU pragma: keep
+#include <uac/hw_config.h>
+
+namespace uac {
+
+struct Excitation : urx::Excitation {
+  bool operator==(const Excitation& other) const {
+    return urx::Excitation::operator==(other) && hw_config == other.hw_config;
+  }
+
+  bool operator!=(const Excitation& other) const { return !operator==(other); }
+
+  HwConfig hw_config;
+};
+
+}  // namespace uac
