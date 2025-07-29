ada5c63098ccadeac84735d8d996126a4ea5a986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:06 +0000

    Add package eric-sse/0.7.8/eric_sse-0.7.8-py3-none-any.whl

---
+++ packages/eric-sse/eric_sse-0.7.8-py3-none-any.whl/eric_sse/__init__.py
@@ -0,0 +1,16 @@
+import logging
+from logging import getLogger, StreamHandler, Logger
+
+
+def get_logger() -> Logger:
+    logger = getLogger(__name__)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            fmt='[%(asctime)s][ERIC][%(levelname)s]%(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        handler = StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
