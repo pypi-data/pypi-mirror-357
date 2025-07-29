421327bfecbc461bd5186ae55e04fa9b7794ea44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:44 +0000

    Add package port-scanner-rq/1.1.0/port_scanner_rq-1.1.0.tar.gz

---
+++ packages/port-scanner-rq/port_scanner_rq-1.1.0.tar.gz/port_scanner_rq-1.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    data=readme.read()
+
+setup(
+    name='port_scanner_rq',
+    version='1.1.0',
+    author='xRangeroQ',
+    author_email='xRangeroQ@email.com',
+    description='A high-speed threaded TCP port scanner.',
+    long_description=data,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    python_requires='>=3.6'
+)
