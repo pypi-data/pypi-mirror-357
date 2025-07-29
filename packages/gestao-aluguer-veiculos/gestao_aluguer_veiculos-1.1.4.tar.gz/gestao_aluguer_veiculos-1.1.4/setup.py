8952e9b0cb81c8c4e09b41c14c94215d2b6327f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:43 +0000

    Add package gestao-aluguer-veiculos/1.1.4/gestao_aluguer_veiculos-1.1.4.tar.gz

---
+++ packages/gestao-aluguer-veiculos/gestao_aluguer_veiculos-1.1.4.tar.gz/gestao_aluguer_veiculos-1.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gestao_aluguer_veiculos",
+    version="1.1.4",
+    description="Gestão de aluguer de automóveis",
+    author="Diogo Fernandes, João Braga, Gonçalo Condesso, Victor Ramos",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
