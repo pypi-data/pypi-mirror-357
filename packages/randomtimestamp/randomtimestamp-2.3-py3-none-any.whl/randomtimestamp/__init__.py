d0f354fe93d68769a8f08852496b6b3bbb428ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:26 +0000

    Add package randomtimestamp/2.3/randomtimestamp-2.3-py3-none-any.whl

---
+++ packages/randomtimestamp/randomtimestamp-2.3-py3-none-any.whl/randomtimestamp/__init__.py
@@ -0,0 +1,13 @@
+"""
+Python module to generate random timestamps.
+"""
+
+__title__ = 'randomtimestamp'
+__version__ = '2.3'
+__author__ = "ByteBaker"
+__license__ = 'GPL v3.0'
+
+
+from .functions import random_date, random_time, randomtimestamp
+
+__all__ = ['random_date', 'random_time', 'randomtimestamp']
---
+++ packages/randomtimestamp/randomtimestamp-2.3-py3-none-any.whl/randomtimestamp/__main__.py
@@ -0,0 +1,9 @@
+from .functions import randomtimestamp
+
+
+def main():
+    print(randomtimestamp(start_year=1950, text=True))
+
+
+if __name__ == "__main__":
+    main()
