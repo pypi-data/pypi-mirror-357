71fdd2d8444c20d142d65e92c4e236deb4155bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:43 +0000

    Add package rdetoolkit/1.3.0/rdetoolkit-1.3.0-cp313-cp313-musllinux_1_2_armv7l.whl

---
+++ packages/rdetoolkit/rdetoolkit-1.3.0-cp313-cp313-musllinux_1_2_armv7l.whl/rdetoolkit/processing/factories.pyi
@@ -0,0 +1,46 @@
+import abc
+from abc import ABC, abstractmethod
+from enum import Enum
+from rdetoolkit.processing.pipeline import Pipeline as Pipeline
+
+class ProcessingMode(Enum):
+    RDEFORMAT = 'rdeformat'
+    MULTIDATATILE = 'multidatatile'
+    EXCELINVOICE = 'excelinvoice'
+    INVOICE = 'invoice'
+    SMARTTABLEINVOICE = 'smarttableinvoice'
+
+class PipelineBuilder(ABC, metaclass=abc.ABCMeta):
+    @abstractmethod
+    def build(self) -> Pipeline: ...
+
+class RDEFormatPipelineBuilder(PipelineBuilder):
+    def build(self) -> Pipeline: ...
+
+class MultiFilePipelineBuilder(PipelineBuilder):
+    def build(self) -> Pipeline: ...
+
+class ExcelInvoicePipelineBuilder(PipelineBuilder):
+    def build(self) -> Pipeline: ...
+
+class InvoicePipelineBuilder(PipelineBuilder):
+    def build(self) -> Pipeline: ...
+
+class SmartTableInvoicePipelineBuilder(PipelineBuilder):
+    def build(self) -> Pipeline: ...
+
+class PipelineFactory:
+    @classmethod
+    def create_pipeline(cls, mode: str | ProcessingMode) -> Pipeline: ...
+    @classmethod
+    def get_supported_modes(cls) -> list[str]: ...
+    @staticmethod
+    def create_rdeformat_pipeline() -> Pipeline: ...
+    @staticmethod
+    def create_multifile_pipeline() -> Pipeline: ...
+    @staticmethod
+    def create_excel_pipeline() -> Pipeline: ...
+    @staticmethod
+    def create_invoice_pipeline() -> Pipeline: ...
+    @staticmethod
+    def create_smarttable_invoice_pipeline() -> Pipeline: ...
