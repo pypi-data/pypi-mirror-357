32b07605236585010e51318d48c8c5e8819d1644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:09 +0000

    Add package ultrasound-acquisition-configuration/1.2.0/ultrasound_acquisition_configuration-1.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_34_x86_64.whl

---
+++ packages/ultrasound-acquisition-configuration/ultrasound_acquisition_configuration-1.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_34_x86_64.whl/ultrasound_acquisition_configuration/include/Uac-1.2.0_static/uac/group.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <algorithm>
+
+#include <urx/group.h>
+
+#include <uac/detail/compare.h>  // IWYU pragma: keep
+#include <uac/event.h>
+#include <uac/igroup.h>
+
+namespace uac {
+
+struct Group : urx::detail::Group<Event>, IGroup {
+  bool operator==(const IGroup& other) const override {
+    const Group* pointer = dynamic_cast<const Group*>(&other);
+    return pointer != nullptr && urx::detail::Group<Event>::operator==(*pointer) &&
+           IGroup::operator==(other);
+  }
+
+  bool operator!=(const Group& other) const { return !operator==(other); }
+};
+
+}  // namespace uac
