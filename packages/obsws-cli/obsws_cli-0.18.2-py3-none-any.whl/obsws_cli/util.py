1b289755548adcf521fd140919d32bc6a58ee382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:41 +0000

    Add package obsws-cli/0.18.2/obsws_cli-0.18.2-py3-none-any.whl

---
+++ packages/obsws-cli/obsws_cli-0.18.2-py3-none-any.whl/obsws_cli/util.py
@@ -0,0 +1,20 @@
+"""module contains utility functions for the obsws_cli package."""
+
+import os
+
+import typer
+
+
+def snakecase_to_titlecase(snake_str: str) -> str:
+    """Convert a snake_case string to a title case string."""
+    return snake_str.replace('_', ' ').title()
+
+
+def check_mark(ctx: typer.Context, value: bool, empty_if_false: bool = False) -> str:
+    """Return a check mark or cross mark based on the boolean value."""
+    if empty_if_false and not value:
+        return ''
+
+    if os.getenv('NO_COLOR', '') != '' or ctx.obj['style'].name == 'no_colour':
+        return '✓' if value else '✗'
+    return '✅' if value else '❌'
