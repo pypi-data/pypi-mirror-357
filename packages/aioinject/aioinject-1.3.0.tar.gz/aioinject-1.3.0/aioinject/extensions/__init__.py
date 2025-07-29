e55b6294aa7f9e4c4250789d189df10c92e12322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:47 +0000

    Add package aioinject/1.3.0/aioinject-1.3.0.tar.gz

---
+++ packages/aioinject/aioinject-1.3.0.tar.gz/aioinject-1.3.0/aioinject/extensions/__init__.py
@@ -0,0 +1,20 @@
+from aioinject.extensions._abc import (
+    Extension,
+    LifespanExtension,
+    LifespanSyncExtension,
+    OnInitExtension,
+    OnResolveExtension,
+    OnResolveSyncExtension,
+    ProviderExtension,
+)
+
+
+__all__ = [
+    "Extension",
+    "LifespanExtension",
+    "LifespanSyncExtension",
+    "OnInitExtension",
+    "OnResolveExtension",
+    "OnResolveSyncExtension",
+    "ProviderExtension",
+]
