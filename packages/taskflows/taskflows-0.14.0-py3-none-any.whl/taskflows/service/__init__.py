963b7fe3a343ecad339210856db209d48aefa6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:04 +0000

    Add package taskflows/0.14.0/taskflows-0.14.0-py3-none-any.whl

---
+++ packages/taskflows/taskflows-0.14.0-py3-none-any.whl/taskflows/service/__init__.py
@@ -0,0 +1,21 @@
+from .constraints import (
+    CPUPressure,
+    CPUs,
+    HardwareConstraint,
+    IOPressure,
+    Memory,
+    MemoryPressure,
+    SystemLoadConstraint,
+)
+from .docker import ContainerLimits, DockerContainer, DockerImage, Ulimit, Volume
+from .entrypoints import CLIGroup, async_entrypoint, parse_str_kwargs
+from .schedule import Calendar, Periodic, Schedule
+from .service import (
+    DockerRunService,
+    DockerStartService,
+    MambaEnv,
+    RestartLimits,
+    RestartPolicy,
+    Service,
+    extract_service_name,
+)
