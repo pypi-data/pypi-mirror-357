ad0dd42fccd4cf23477ae98f65ffa4677f965768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:17 +0000

    Add package protonmail-api-client/2.4.3/protonmail_api_client-2.4.3-py3-none-any.whl

---
+++ packages/protonmail-api-client/protonmail_api_client-2.4.3-py3-none-any.whl/protonmail/exceptions.py
@@ -0,0 +1,47 @@
+"""Exceptions"""
+
+
+class SendMessageError(Exception):
+    """Error when try to send message"""
+
+
+class InvalidTwoFactorCode(Exception):
+    """Invalid Two-Factor Authentication(2FA) code"""
+
+
+class NoKeysForDecryptThisMessage(Exception):
+    """
+    No keys for decrypt this message,
+    maybe you created new key then re-login,
+    if you deleted key then you can't decrypt this message
+    """
+
+
+class LoadSessionError(Exception):
+    """Error while load session, maybe this session file was created in other version? then re-login"""
+
+
+class AddressNotFound(Exception):
+    """Email address was not found"""
+
+
+class CantUploadAttachment(Exception):
+    """Error when try to upload attachment"""
+
+
+class CantGetLabels(Exception):
+    """Error when try to get labels"""
+
+class CantSetLabel(Exception):
+    """Error when try to set label for a message"""
+
+class CantUnsetLabel(Exception):
+    """Error when try to unset label for a message"""
+
+
+class CantSolveImageCaptcha(Exception):
+    """Error when try to solve image CAPTCHA, maybe this image hard, just retry login"""
+
+
+class InvalidCaptcha(Exception):
+    """Error when solved CAPTCHA, but something wrong"""
