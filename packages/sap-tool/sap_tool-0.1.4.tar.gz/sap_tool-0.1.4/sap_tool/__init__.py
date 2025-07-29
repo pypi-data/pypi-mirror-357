731af4dc1cdf4e7eb487257aeb76716c98370646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:52 +0000

    Add package sap-tool/0.1.4/sap_tool-0.1.4.tar.gz

---
+++ packages/sap-tool/sap_tool-0.1.4.tar.gz/sap_tool-0.1.4/sap_tool/__init__.py
@@ -0,0 +1,40 @@
+from typing import Callable
+import inspect
+
+__all__ = ["Tool"]
+
+class Tool:
+    def __init__(self, function: Callable, name: str, description: str):
+        self.function = function
+        self.name = name
+        self.description = description
+        self.python_type_to_json_schema = {
+            "str": "string",
+            "float": "number",
+            "int": "integer",
+            "bool": "boolean",
+            "dict": "object",
+            "list": "array",
+            "None": "null"
+        }
+
+    def formulate_tool_schema(self):
+        parameters = inspect.signature(self.function).parameters
+        properties = {}
+        for parm_name, parm in parameters.items():
+            properties[parm_name] = {"type": self.python_type_to_json_schema[str(parm.annotation.__name__)]}
+        print(properties)  # Example output: {'a': {'type': 'integer'}, 'b': {'type': 'string'}}
+        schema = {
+            "type": "function",
+            "function": {
+                "name": self.name,
+                "description": self.description,
+                "parameters": {
+                    "type": "object", 
+                    "properties": properties,
+                    "required": [parm_name for parm_name, parm in parameters.items()]
+                }
+            }
+        }
+        print(schema)  # Example output: {'type': 'function', 'function': {'name': 'foo', 'description': 'A test function.', 'parameters': {'type': 'object', 'properties': {'a': {'type': 'integer'}, 'b': {'type': 'string'}}, 'required': ['a', 'b']}}}
+        return schema
