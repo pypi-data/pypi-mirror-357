d95717b9d4e332545c090bbe3654defae69f6ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:34 +0000

    Add package stac-check/1.11.0/stac_check-1.11.0.tar.gz

---
+++ packages/stac-check/stac_check-1.11.0.tar.gz/stac_check-1.11.0/setup.py
@@ -0,0 +1,50 @@
+"""stac-check setup.py
+"""
+
+from setuptools import find_packages, setup
+
+__version__ = "1.11.0"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="stac_check",
+    version=__version__,
+    description="Linting and validation tool for STAC assets",
+    url="https://github.com/stac-utils/stac-check",
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    setup_requires=["setuptools"],
+    install_requires=[
+        "requests>=2.32.3",
+        "jsonschema>=4.23.0",
+        "click>=8.1.8",
+        "stac-validator~=3.9.1",
+        "PyYAML",
+        "python-dotenv",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "requests-mock",
+            "types-setuptools",
+            "stac-validator[pydantic]~=3.9.1",
+        ],
+        "docs": [
+            "sphinx>=4.0.0",
+            "sphinx_rtd_theme>=1.0.0",
+            "myst-parser>=0.18.0",
+            "sphinx-autodoc-typehints>=1.18.0",
+        ],
+        "pydantic": ["stac-validator[pydantic]~=3.9.1"],
+    },
+    entry_points={"console_scripts": ["stac-check=stac_check.cli:main"]},
+    author="Jonathan Healy",
+    author_email="jonathan.d.healy@gmail.com",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+    tests_require=["pytest"],
+)
