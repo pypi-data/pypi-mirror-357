2c380ed9a876105ca48b44563adc8aeefa3db315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:44 +0000

    Add package simple-fastapi-backend-server/0.1.3/simple_fastapi_backend_server-0.1.3.tar.gz

---
+++ packages/simple-fastapi-backend-server/simple_fastapi_backend_server-0.1.3.tar.gz/simple_fastapi_backend_server-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simple-fastapi-backend-server",
+    version="0.1.3",  # incremented version
+    packages=find_packages(),
+    install_requires=["fastapi", "uvicorn"],
+    entry_points={
+        "console_scripts": [
+            "simple-fastapi-backend-server = simple_fastapi_backend_server.main:start"
+        ]
+    },
+    author="Ataur Rahman",
+    description="A simple FastAPI wrapper package for quick testing of auth endpoints.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+)
