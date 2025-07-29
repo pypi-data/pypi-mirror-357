98d284ab384b6c4bf9a17a7b402d9addc57940f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:20 +0000

    Add package affewer3323/0.1.0/affewer3323-0.1.0.tar.gz

---
+++ packages/affewer3323/affewer3323-0.1.0.tar.gz/affewer3323-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="affewer3323",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple math package",
+    author="Your Name",
+    author_email="your.email@example.com",
+    python_requires=">=3.6",
+)
