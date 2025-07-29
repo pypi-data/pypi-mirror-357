05f82c9a02037709da69e2861b001c68ce761aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:36 +0000

    Add package ladyrick/0.5.1/ladyrick-0.5.1-py3-none-any.whl

---
+++ packages/ladyrick/ladyrick-0.5.1-py3-none-any.whl/ladyrick/__init__.py
@@ -0,0 +1,35 @@
+"ladyrick's tools"
+
+from typing import TYPE_CHECKING
+
+
+def __getattr__(name):
+    from importlib import import_module
+
+    if name in ("pretty_print",):
+        m = import_module("ladyrick.pprint")
+    elif name in ("class_name", "utc_8_now", "get_timestr"):
+        m = import_module("ladyrick.utils")
+    elif name in ("dump", "Dump", "V", "Vars"):
+        m = import_module("ladyrick.vars")
+    elif name in ("debugpy", "embed"):
+        m = import_module("ladyrick.debug")
+    elif name in ("parallel_print", "rich_print", "print_col", "print_table"):
+        m = import_module("ladyrick.print_utils")
+    elif name in ("rank", "print_rank_0", "print_rank_last"):
+        m = import_module("ladyrick.torch")
+    elif name in ("type_like",):
+        m = import_module("ladyrick.typing")
+    else:
+        raise AttributeError(name)
+    return getattr(m, name)
+
+
+if TYPE_CHECKING:
+    from ladyrick.debug import debugpy, embed  # noqa
+    from ladyrick.pprint import pretty_print  # noqa
+    from ladyrick.print_utils import parallel_print, print_col, print_table, rich_print  # noqa
+    from ladyrick.torch import print_rank_0, print_rank_last, rank  # noqa
+    from ladyrick.typing import type_like  # noqa
+    from ladyrick.utils import class_name, get_timestr, utc_8_now  # noqa
+    from ladyrick.vars import Dump, V, Vars, dump  # noqa
