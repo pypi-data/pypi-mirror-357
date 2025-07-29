cda77122deccae4c76b85e9f87bfa1aa7a06367d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:04 +0000

    Add package sportsfeatures/0.0.101/sportsfeatures-0.0.101.tar.gz

---
+++ packages/sportsfeatures/sportsfeatures-0.0.101.tar.gz/sportsfeatures-0.0.101/sportsfeatures/entity_type.py
@@ -0,0 +1,12 @@
+"""An enum containing the different entity types."""
+
+from enum import StrEnum, auto
+
+
+class EntityType(StrEnum):
+    """The type of entity being represented."""
+
+    PLAYER = auto()
+    TEAM = auto()
+    VENUE = auto()
+    COACH = auto()
