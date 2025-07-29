cea518d6e6ae43df97cd4a45b0f4c97907c3e5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:19 +0000

    Add package jdxpy/0.1.0/jdxpy-0.1.0-py3-none-any.whl

---
+++ packages/jdxpy/jdxpy-0.1.0-py3-none-any.whl/dx_py/core.py
@@ -0,0 +1,18 @@
+"""
+Core functionality for JDxPy library.
+"""
+
+
+def hello_dxpy() -> str:
+    """
+    基本的な例示関数です。
+
+    Returns:
+        str: 実行結果のメッセージ
+
+    Examples:
+        >>> result = hello_dxpy()
+        >>> print(result)
+        Hello from DxPy!
+    """
+    return "Hello from DxPy!"
