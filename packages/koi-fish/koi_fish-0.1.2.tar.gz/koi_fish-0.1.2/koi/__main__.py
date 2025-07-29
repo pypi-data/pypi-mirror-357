4a94b95679920df53399d820ad7530eb182cdf7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:16 +0000

    Add package koi-fish/0.1.2/koi_fish-0.1.2.tar.gz

---
+++ packages/koi-fish/koi_fish-0.1.2.tar.gz/koi_fish-0.1.2/koi/__main__.py
@@ -0,0 +1,28 @@
+import argparse
+
+from . import __version__
+from .runner import Runner
+
+
+def get_command_line_args():
+    parser = argparse.ArgumentParser(
+        prog="koi_fish",
+        description="CLI automation tool",
+    )
+    parser.add_argument("--version", action="version", version=f"%(prog)s v{__version__}")
+    parser.add_argument(
+        "-j",
+        "--jobs",
+        nargs="+",
+        help="pick a job from config file to run",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = get_command_line_args()
+    Runner().run(args)
+
+
+if __name__ == "__main__":
+    main()
