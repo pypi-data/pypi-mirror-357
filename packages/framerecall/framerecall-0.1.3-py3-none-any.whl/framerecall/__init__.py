917394e8cec6a92f3f2c9fce12a4543bb3b54610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:48 +0000

    Add package framerecall/0.1.3/framerecall-0.1.3-py3-none-any.whl

---
+++ packages/framerecall/framerecall-0.1.3-py3-none-any.whl/framerecall/__init__.py
@@ -0,0 +1,13 @@
+"""
+Framerecall - QR Code Video-Based AI Memory Library
+"""
+
+__version__ = "0.1.0"
+
+from .encoder import FramerecallEncoder
+from .retriever import FramerecallRetriever
+from .chat import FramerecallChat
+from .interactive import chat_with_memory, quick_chat
+from .llm_client import LLMClient, create_llm_client
+
+__all__ = ["FramerecallEncoder", "FramerecallRetriever", "FramerecallChat", "chat_with_memory", "quick_chat", "LLMClient", "create_llm_client"]
