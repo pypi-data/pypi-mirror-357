b4307ca46bd4ee1428fdfcad6462e57f477b4293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:38 +0000

    Add package xecm/24.4.2/xecm-24.4.2-py3-none-any.whl

---
+++ packages/xecm/xecm-24.4.2-py3-none-any.whl/xecm/__init__.py
@@ -0,0 +1,6 @@
+"""xecm - Simple Python Library to call OpenText Extended ECM REST API."""
+
+from .CSRestAPI import CSRestAPI
+from .CSRestAPI import LoginType
+
+__all__ = ["CSRestAPI", "LoginType"]
