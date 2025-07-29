efcb44db86ccf167fa10f68a9050660651fdf488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:38 +0000

    Add package telegram-notifier_mcp_ac/0.1.0/telegram_notifier_mcp_ac-0.1.0.tar.gz

---
+++ packages/telegram-notifier_mcp_ac/telegram_notifier_mcp_ac-0.1.0.tar.gz/telegram_notifier_mcp_ac-0.1.0/telegram_notifier_server.py
@@ -0,0 +1,33 @@
+import os
+import requests
+from dotenv import *
+from mcp.server.fastmcp import FastMCP
+from datetime import datetime
+
+
+load_dotenv() # Load environment variables from .env file
+
+TELEGRAM_BOT_TOKEN = os.environ.get("AC_TG_KEY")
+TELEGRAM_CHAT_ID = os.environ.get("AC_TG_CHATID")
+
+
+mcp = FastMCP("test_mcp")
+
+@mcp.tool()
+def get_current_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
+
+# @mcp.tool()
+# def send_message(message: str):
+#     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+#     payload = {
+#         "chat_id": TELEGRAM_CHAT_ID,
+#         "text": message
+#     }
+#     response = requests.post(url, data=payload)
+#     return response.json()
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
