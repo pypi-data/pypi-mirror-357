360b01d7b544542ce3ff6926fa8b866c8f1dd760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:05 +0000

    Add package epss-api/1.3.8/epss_api-1.3.8.tar.gz

---
+++ packages/epss-api/epss_api-1.3.8.tar.gz/epss_api-1.3.8/README.rst
@@ -0,0 +1,90 @@
+=================
+EPSS API Client
+=================
+
+.. image:: https://badge.fury.io/py/epss-api.svg
+    :target: https://badge.fury.io/py/epss-api
+
+.. image:: https://img.shields.io/pypi/dw/epss-api?style=flat
+    :target: https://pypistats.org/packages/epss-api
+
+.. image:: https://github.com/kannkyo/epss-api/actions/workflows/python-ci.yml/badge.svg
+    :target: https://github.com/kannkyo/epss-api/actions/workflows/python-ci.yml
+
+.. image:: https://codecov.io/gh/kannkyo/epss-api/branch/main/graph/badge.svg?token=R40FT0KITO 
+    :target: https://codecov.io/gh/kannkyo/epss-api
+
+.. image:: https://github.com/kannkyo/epss-api/actions/workflows/scorecards.yml/badge.svg
+    :target: https://github.com/kannkyo/epss-api/actions/workflows/scorecards.yml
+
+EPSS(Exploit Prediction Scoring System) API client.
+
+EPSS is the one of famous vulnerability score developed by FIRST (the Forum of Incident Response and Security Teams).
+
+EPSS's definition:
+
+    The Exploit Prediction Scoring System (EPSS) is an open, 
+    data-driven effort for estimating the likelihood (probability) that a software vulnerability will be exploited in the wild. 
+    Our goal is to assist network defenders to better prioritize vulnerability remediation efforts. 
+    While other industry standards have been useful for capturing innate characteristics of a vulnerability and provide measures of severity, 
+    they are limited in their ability to assess threat. 
+    EPSS fills that gap because it uses current threat information from CVE and real-world exploit data. 
+    The EPSS model produces a probability score between 0 and 1 (0 and 100%). 
+    The higher the score, the greater the probability that a vulnerability will be exploited.
+
+    https://www.first.org/epss/
+
+This package is most easiest and efficient EPSS api client.
+
+Usage
+=============
+
+EPSS has some methods.
+
+.. code-block:: bash
+
+    >>> from epss_api import EPSS
+    >>> 
+    >>> client = EPSS()
+
+    >>> print(client.scores()[0])
+    {'cve': 'CVE-1999-0013', 'epss': 0.00042, 'percentile': 0.05071}
+
+    >>> print(client.score('CVE-2024-0001'))
+    {'cve': 'CVE-2024-0001', 'epss': 0.00091, 'percentile': 0.4063}
+
+    >>> print(client.csv()[1])
+    cve,epss,percentile
+
+    >>> print(client.epss('CVE-2024-0001'))
+    0.00091
+
+    >>> print(client.epss_ge(0.50003)[0])
+    {'cve': 'CVE-2022-0651', 'epss': 0.50003, 'percentile': 0.97652}
+
+    >>> print(client.epss_gt(0.50003)[0])
+    {'cve': 'CVE-2018-0851', 'epss': 0.50036, 'percentile': 0.97653}
+
+    >>> print(client.epss_le(0.49982)[-1])
+    {'cve': 'CVE-2014-8074', 'epss': 0.49982, 'percentile': 0.97651}
+
+    >>> print(client.epss_lt(0.49982)[-1])
+    {'cve': 'CVE-2018-8011', 'epss': 0.49981, 'percentile': 0.97651}
+
+    >>> print(client.percentile('CVE-2024-0001'))
+    0.4063
+
+    >>> print(client.percentile_ge(0.5)[0])
+    {'cve': 'CVE-2019-5426', 'epss': 0.00137, 'percentile': 0.5}
+
+    >>> print(client.percentile_gt(0.5)[0])
+    {'cve': 'CVE-2021-43464', 'epss': 0.00137, 'percentile': 0.50004}
+
+    >>> print(client.percentile_le(0.5)[-1])
+    {'cve': 'CVE-2022-27777', 'epss': 0.00137, 'percentile': 0.5}
+
+    >>> print(client.percentile_lt(0.5)[-1])
+    {'cve': 'CVE-2021-1625', 'epss': 0.00137, 'percentile': 0.49999}
+
+If you call either one method, EPSS client cache all CVE's score in memory.
+After caching, you can get all data very fast.
---
+++ packages/epss-api/epss_api-1.3.8.tar.gz/epss_api-1.3.8/src/epss_api/epss.py
@@ -0,0 +1,240 @@
+from __future__ import annotations
+
+import csv
+from bisect import bisect_left, bisect_right
+from gzip import GzipFile
+from urllib.request import urlopen
+
+
+class Score(object):
+    """ EPSS Score Object"""
+
+    def __init__(self, cve: str, epss: str, percentile: str):
+        """Initialize EPSS Score Object
+
+        Args:
+            cve (str): CVE-yyyy-n
+            epss (str): [0-1]
+            percentile (str): [0-1]
+        """
+        self.cve = cve
+        self.epss = float(epss)
+        self.percentile = float(percentile)
+
+    def __str__(self):
+        return f"{{'cve': '{self.cve}', 'epss': {self.epss}, " \
+            + "'percentile': {self.percentile}}}"
+
+
+class EPSS(object):
+    def __init__(self) -> None:
+
+        url = 'https://epss.cyentia.com/epss_scores-current.csv.gz'
+
+        with urlopen(url) as res:
+            dec = GzipFile(fileobj=res)
+            epss_scores_str: str = dec.read().decode("utf-8")
+            epss_scores_list = epss_scores_str.split('\n')
+
+        self._download = epss_scores_list
+
+        scores = [row for row in csv.DictReader(self._download[1:])]
+
+        self._byCVE = {row['cve']:
+                       Score(row['cve'], row['epss'], row['percentile'])
+                       for row in scores}
+
+        self._sortedScoresByPercentile = sorted(self._byCVE.values(),
+                                                key=lambda x: x.percentile)
+
+        self._sortedScoresByEpss = sorted(self._byCVE.values(),
+                                          key=lambda x: x.epss)
+
+    def scores(self) -> list[Score]:
+        """Get all CVE's EPSS scores (downloaded data is cached in memory)
+
+        Example
+
+        [
+        {'cve': 'CVE-2022-39952', 'epss': '0.09029', 'percentile': '0.94031'},
+        {'cve': 'CVE-2023-0669', 'epss': '0.78437', 'percentile': '0.99452'},
+        ...
+        ]
+
+        Returns:
+            list[Score]: EPSS score's csv list
+        """
+        return list(self._sortedScoresByEpss)
+
+    def score(self, cve_id: str) -> Score:
+        """Get EPSS score and percentile
+
+        Example
+            {'cve': 'CVE-2022-39952', 'epss': 0.0095, 'percentile': 0.32069}
+
+        Args:
+            cve_id (str): CVE ID (CVE-nnnn)
+
+        Returns:
+            Score | None: EPSS score percentile
+        """
+
+        return self._byCVE.get(cve_id, None)
+
+    def epss(self, cve_id: str) -> float:
+        """Get EPSS score
+
+        Args:
+            cve_id (str): CVE ID (CVE-nnnn)
+
+        Returns:
+            float | None: EPSS score (0.0-1.0)
+        """
+
+        score = self._byCVE.get(cve_id, None)
+        if score is None:
+            return None
+        else:
+            return score.epss
+
+    def percentile(self, cve_id: str) -> float:
+        """Get EPSS percentile
+
+        Args:
+            cve_id (str): CVE ID (CVE-nnnn)
+
+        Returns:
+            float | None: EPSS percentile (0.0-1.0)
+        """
+        score = self._byCVE.get(cve_id, None)
+        if score is None:
+            return None
+        else:
+            return score.percentile
+
+    def epss_gt(self, min: float) -> list[Score]:
+        """Get CVEs with EPSS score greater than the parameter
+
+        Args:
+            min (float): limit of EPSS score
+
+        Returns:
+            list[Score] | None: EPSS score object list
+        """
+        i = bisect_right(self._sortedScoresByEpss, min,
+                         key=lambda x: x.epss)
+
+        return list(self._sortedScoresByEpss[i:])
+
+    def percentile_gt(self, min: float) -> list[Score]:
+        """Get CVEs with percentile greater than the parameter
+
+        Args:
+            min (float): limit of percentile
+
+        Returns:
+            list[Score] | None: EPSS score object list
+        """
+        i = bisect_right(self._sortedScoresByPercentile, min,
+                         key=lambda x: x.percentile)
+
+        return list(self._sortedScoresByPercentile[i:])
+
+    def epss_ge(self, min: float) -> list[Score]:
+        """Get CVEs with EPSS score greater than or equal to the parameter
+
+        Args:
+            min (float): limit of EPSS score
+
+        Returns:
+            list[Score] | None: EPSS score object list
+        """
+        i = bisect_left(self._sortedScoresByEpss, min,
+                        key=lambda x: x.epss)
+
+        return list(self._sortedScoresByEpss[i:])
+
+    def percentile_ge(self, min: float) -> list[Score]:
+        """Get CVEs with percentile greater than or equal to the parameter
+
+        Args:
+            min (float): limit of percentile
+
+        Returns:
+            list[Score] | None: EPSS score object list
+        """
+        i = bisect_left(self._sortedScoresByPercentile, min,
+                        key=lambda x: x.percentile)
+
+        return list(self._sortedScoresByPercentile[i:])
+
+    def epss_lt(self, max: float) -> list[Score]:
+        """Get CVEs with EPSS score lower than the parameter
+
+        Args:
+            max (float): limit of EPSS score
+
+        Returns:
+            list[Score] | []: EPSS score object list
+        """
+        i = bisect_left(self._sortedScoresByEpss, max,
+                        key=lambda x: x.epss)
+
+        return list(self._sortedScoresByEpss[:i])
+
+    def percentile_lt(self, max: float) -> list[Score]:
+        """Get CVEs with percentile less than the parameter
+
+        Args:
+            max (float): limit of percentile
+
+        Returns:
+            list[Score] | []: EPSS score object list
+        """
+        i = bisect_left(self._sortedScoresByPercentile, max,
+                        key=lambda x: x.percentile)
+
+        return list(self._sortedScoresByPercentile[:i])
+
+    def epss_le(self, max: float) -> list[Score]:
+        """Get CVEs with EPSS score less than or equal to the parameter
+
+        Args:
+            max (float): limit of EPSS score
+
+        Returns:
+            list[Score] | []: EPSS score object list
+        """
+        i = bisect_right(self._sortedScoresByEpss, max,
+                         key=lambda x: x.epss)
+
+        return list(self._sortedScoresByEpss[:i])
+
+    def percentile_le(self, max: float) -> list[Score]:
+        """Get CVEs with percentile less than or equal to the parameter
+
+        Args:
+            max (float): limit of percentile
+
+        Returns:
+            list[Score] | []: EPSS score object list
+        """
+        i = bisect_right(self._sortedScoresByPercentile, max,
+                         key=lambda x: x.percentile)
+
+        return list(self._sortedScoresByPercentile[:i])
+
+    def csv(self) -> list[str]:
+        """Get csv data containing all epss scores.
+
+        Example
+            #model_version:v2022.01.01,score_date:2023-03-03T00:00:00+0000
+            cve,epss,percentile
+            CVE-2014-6271,0.96235,0.99992
+            CVE-2014-7169,0.83508,0.99607
+            ...
+
+        Returns:
+            list[str]: csv data
+        """
+        return self._download
