625f67367d004c077ca9144e6a2c6f695d7f518e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:18 +0000

    Add package drdid/1.1.6/drdid-1.1.6.tar.gz

---
+++ packages/drdid/drdid-1.1.6.tar.gz/drdid-1.1.6/README.md
@@ -0,0 +1,9 @@
+# DRDID in python
+
+See the original [`R package`](https://github.com/pedrohcgs/DRDID/tree/master)
+
+## Installation
+
+```
+pip install git+https://github.com/d2cml-ai/DRDID
+```
---
+++ packages/drdid/drdid-1.1.6.tar.gz/drdid-1.1.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+from drdid._version_ import version
+
+setup(
+    name='drdid',
+    version=version,
+    description='Double Robust Difference in Difference y python',
+    url='https://github.com/d2cml-ai/drdid',
+    author='Jhon Flores',
+    license="MIT",
+    author_email='fr.jhonk@gmail.com',
+    packages=['drdid'],
+    install_requires=[
+        'pandas', 
+        'numpy',
+        'statsmodels'
+    ],
+    long_description='''
+    Implementation of drdid for Python, R-like syntax with return destructuring using optimized Python code.
+
+    See the original [R package](https://github.com/pedrohcgs/DRDID/tree/master)
+    '''
+)
