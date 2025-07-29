9dd6e38f814ecd02fd9fe398c64a8a57ad50af08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:17 +0000

    Add package jureap/0.7.2/jureap-0.7.2-py2.py3-none-any.whl

---
+++ packages/jureap/jureap-0.7.2-py2.py3-none-any.whl/jureap/__init__.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# --------------------------------------------------------------------------------------------------
+import jureap.metadata
+import jureap.manual
+import jureap.check
+import jureap.detail
+import jureap.log
+import jureap.report
