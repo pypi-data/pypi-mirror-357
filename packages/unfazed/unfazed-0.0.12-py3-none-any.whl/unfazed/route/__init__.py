c07fe4b6b774c144aab8262c9112a1e0aadeeb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:39 +0000

    Add package unfazed/0.0.12/unfazed-0.0.12-py3-none-any.whl

---
+++ packages/unfazed/unfazed-0.0.12-py3-none-any.whl/unfazed/route/__init__.py
@@ -0,0 +1,15 @@
+from starlette.convertors import Convertor, register_url_convertor
+
+from .base import include, path, static
+from .registry import parse_urlconf
+from .routing import Route
+
+__all__ = [
+    "Route",
+    "path",
+    "include",
+    "parse_urlconf",
+    "Convertor",
+    "register_url_convertor",
+    "static",
+]
