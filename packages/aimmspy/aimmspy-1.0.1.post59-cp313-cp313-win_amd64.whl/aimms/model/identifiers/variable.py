85346f87af0b00f4a6ff8791e2937ebdf9d69f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:28 +0000

    Add package aimmspy/1.0.1.post59/aimmspy-1.0.1.post59-cp313-cp313-win_amd64.whl

---
+++ packages/aimmspy/aimmspy-1.0.1.post59-cp313-cp313-win_amd64.whl/aimms/model/identifiers/variable.py
@@ -0,0 +1,22 @@
+
+from aimms.model.enums.me_identifier_types import IdentifierType
+from aimms.model.enums.me_attribute_types import AttributeType
+
+from aimms.model.identifiers.data_identifiers import DataIdentifier
+
+class Variable(DataIdentifier):
+    def __init__(self, parent_me_handle, name, index_domain, **kwargs):
+        
+        super().__init__(
+            name=name,
+            parent_me_handle=parent_me_handle,
+            identifier_type=IdentifierType.VARIABLE_NUMERIC.value,
+            index_domain=index_domain,
+            **kwargs
+        )
+    
+        self.range = self.set_attribute(self.me_handle, kwargs.get("range", None), AttributeType.RANGE.value)
+        self.definition = self.set_attribute(self.me_handle, kwargs.get("definition", None), AttributeType.DEFINITION.value)
+        
+    def __str__(self):
+        return self.name
