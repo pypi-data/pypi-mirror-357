d83c57b28c0c8c95b32c9dbf2569c3a5f7b843ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:26 +0000

    Add package avin_data/1.0.6/avin_data-1.0.6-py3-none-any.whl

---
+++ packages/avin_data/avin_data-1.0.6-py3-none-any.whl/avin_data/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env  python3
+# ============================================================================
+# URL:          http://arsvincere.com
+# AUTHOR:       Alex Avin
+# E-MAIL:       mr.alexavin@gmail.com
+# LICENSE:      MIT
+# ============================================================================
+
+from avin_data.manager import *
+from avin_data.utils import *
+
