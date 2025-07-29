c11d163c55c052849d59a894127228bdf9a1d58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:05 +0000

    Add package nandhubasicscalculator/0.0.1/nandhubasicscalculator-0.0.1.tar.gz

---
+++ packages/nandhubasicscalculator/nandhubasicscalculator-0.0.1.tar.gz/nandhubasicscalculator-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nandhubasicscalculator',
+    version='0.0.1',
+    description='A very basic calculator for simple arithmetic operations',
+    author='nandhu',
+    author_email='ggnandhinigg@gmail.com',
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here if needed
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.7',
+)
