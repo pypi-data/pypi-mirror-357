05ce709a83fabe5e26f6bd1f8643e6e459d58570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:36 +0000

    Add package core-extract-and-load/0.1/core_extract_and_load-0.1.tar.gz

---
+++ packages/core-extract-and-load/core_extract_and_load-0.1.tar.gz/core_extract_and_load-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='core_extract_and_load',
+    version='0.1',
+    packages=find_packages(),
+    description='core',
+    author='facundo vega',
+    author_email='facundo.vega1234@gmail.com',
+    zip_safe=False,
+    url='https://github.com/facuvegaingenieer/example-core', 
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
