5a952c26fdfe94a66330719c6eb7ec3425e79356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:49 +0000

    Add package mitol-django-mail/2025.6.23/mitol_django_mail-2025.6.23-py3-none-any.whl

---
+++ packages/mitol-django-mail/mitol_django_mail-2025.6.23-py3-none-any.whl/mitol/mail/apps.py
@@ -0,0 +1,16 @@
+"""Mail app AppConfigs"""
+
+import os
+
+from django.apps import AppConfig
+
+
+class MailApp(AppConfig):
+    """Default configuration for the mail app"""
+
+    name = "mitol.mail"
+    label = "mail"
+    verbose_name = "Mail"
+
+    # necessary because this is a namespaced app
+    path = os.path.dirname(os.path.abspath(__file__))  # noqa: PTH100, PTH120
