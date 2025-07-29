bfc268179a52ea1c0f2b71bcb424fe76a16da068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:03 +0000

    Add package vipwrap/0.1.4/vipwrap-0.1.4.tar.gz

---
+++ packages/vipwrap/vipwrap-0.1.4.tar.gz/vipwrap-0.1.4/tests/test_download.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.path.insert(0, "")
+
+from vipwrap import gdi
+
+
+def test_download_ftp():
+    # downloaded_files = gdi.download_files_from_gdi(
+    #     "ftp", "localhost", 10021, "myuser", "mypass", "/", "test", False
+    # )
+    # downloaded_files = gdi.download_files_from_gdi(
+    #     "sftp", "localhost", 10022, "foo", "pass", "/upload/", "test", False
+    # )
+    pass
+
+
+if __name__ == "__main__":
+    test_download_ftp()
