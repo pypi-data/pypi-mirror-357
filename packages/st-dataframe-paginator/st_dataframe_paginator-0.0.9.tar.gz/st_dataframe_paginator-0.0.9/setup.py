a12e1b83ec111bfc80c9130872fcd82c52897c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:27 +0000

    Add package st-dataframe-paginator/0.0.9/st_dataframe_paginator-0.0.9.tar.gz

---
+++ packages/st-dataframe-paginator/st_dataframe_paginator-0.0.9.tar.gz/st_dataframe_paginator-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="st_dataframe_paginator",
+    version="0.0.9",
+    author="",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.9",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
