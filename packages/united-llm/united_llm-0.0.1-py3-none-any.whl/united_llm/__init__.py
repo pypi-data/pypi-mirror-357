7a12985cd470f11d010ce26a2828bdf87164fa98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:42 +0000

    Add package united-llm/0.0.1/united_llm-0.0.1-py3-none-any.whl

---
+++ packages/united-llm/united_llm-0.0.1-py3-none-any.whl/united_llm/__init__.py
@@ -0,0 +1,9 @@
+"""
+A minimal test package for checking PyPI name availability.
+"""
+
+__version__ = "0.0.1"
+
+def hello():
+    """A simple test function."""
+    return "Hello from your test package!"
