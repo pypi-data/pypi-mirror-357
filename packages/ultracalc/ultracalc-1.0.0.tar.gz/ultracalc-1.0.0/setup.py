d08f80aa2fad3446d281b2d5aefbf3bba5136960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:03 +0000

    Add package ultracalc/1.0.0/ultracalc-1.0.0.tar.gz

---
+++ packages/ultracalc/ultracalc-1.0.0.tar.gz/ultracalc-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="ultracalc",
+    version="1.0.0",
+    description="Infinite-precision calculator with math expression parsing",
+    author="kkyian",
+    packages=["ultracalc"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
