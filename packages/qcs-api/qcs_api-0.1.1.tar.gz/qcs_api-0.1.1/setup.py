8b3aa9e24d97cfa7fa32867ad859b9d1de740cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:29 +0000

    Add package qcs-api/0.1.1/qcs_api-0.1.1.tar.gz

---
+++ packages/qcs-api/qcs_api-0.1.1.tar.gz/qcs_api-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qcs_api', 
+    version='0.1.1', 
+    packages=find_packages(), 
+    install_requires=[
+        'requests',
+    ],
+    author='TUM QCS', 
+    classifiers=['Programming Language :: Python :: 3', ],
+)
