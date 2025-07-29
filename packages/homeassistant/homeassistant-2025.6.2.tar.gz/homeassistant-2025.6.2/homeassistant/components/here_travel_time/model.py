26bad575580a0ec0cee50bef28fa1267878cd22f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:12 +0000

    Add package homeassistant/2025.6.2/homeassistant-2025.6.2.tar.gz

---
+++ packages/homeassistant/homeassistant-2025.6.2.tar.gz/homeassistant-2025.6.2/homeassistant/components/here_travel_time/model.py
@@ -0,0 +1,32 @@
+"""Model Classes for here_travel_time."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import datetime
+from typing import TypedDict
+
+
+class HERETravelTimeData(TypedDict):
+    """Routing information."""
+
+    attribution: str | None
+    duration: float
+    duration_in_traffic: float
+    distance: float
+    origin: str
+    destination: str
+    origin_name: str | None
+    destination_name: str | None
+
+
+@dataclass
+class HERETravelTimeAPIParams:
+    """Configuration for polling the HERE API."""
+
+    destination: list[str]
+    origin: list[str]
+    travel_mode: str
+    route_mode: str
+    arrival: datetime | None
+    departure: datetime | None
