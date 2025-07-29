f0c39ae1e75045862096cf53e2e6db88477847d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:34 +0000

    Add package svo-client/1.1.3/svo_client-1.1.3.tar.gz

---
+++ packages/svo-client/svo_client-1.1.3.tar.gz/svo_client-1.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="svo-client",
+    version="1.1.3",
+    description="Async client for SVO semantic chunker microservice.",
+    author="Your Name",
+    author_email="your@email.com",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.8.0",
+        "pydantic>=2.0.0",
+        "chunk_metadata_adapter",
+        "python-dateutil"
+    ],
+    python_requires=">=3.8",
+    url="https://github.com/your_org/svo_client",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+) 
