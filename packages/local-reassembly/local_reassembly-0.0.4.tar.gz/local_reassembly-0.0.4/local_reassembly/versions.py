8919bc333233bd1861400e239b89fb2c29e52c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:08 +0000

    Add package local-reassembly/0.0.4/local_reassembly-0.0.4.tar.gz

---
+++ packages/local-reassembly/local_reassembly-0.0.4.tar.gz/local_reassembly-0.0.4/local_reassembly/versions.py
@@ -0,0 +1,30 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.4",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "0.0.3",
+        "features": [
+            "1. add genic reassembly function",
+        ],
+    },
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. add get_range_assembly function",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. init",
+        ],
+    },
+]
