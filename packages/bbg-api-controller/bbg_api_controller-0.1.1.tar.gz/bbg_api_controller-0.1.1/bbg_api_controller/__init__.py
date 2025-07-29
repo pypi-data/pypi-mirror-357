d1cb5f5ce0754fe9aaf6aa04668da2912cf5a8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:21 +0000

    Add package bbg-api-controller/0.1.1/bbg_api_controller-0.1.1.tar.gz

---
+++ packages/bbg-api-controller/bbg_api_controller-0.1.1.tar.gz/bbg_api_controller-0.1.1/bbg_api_controller/__init__.py
@@ -0,0 +1,6 @@
+from .connector import *
+from .utils import *
+from .consts import *
+from .composed import *
+from .basis import *
+from .application import *
---
+++ packages/bbg-api-controller/bbg_api_controller-0.1.1.tar.gz/bbg_api_controller-0.1.1/bbg_api_controller/consts/db_consts.py
@@ -0,0 +1,2 @@
+DATABASE_NAME_BBG = 'database-bbg'
+COLLECTION_NAME_PRICE = 'dataset-PX_LAST'
