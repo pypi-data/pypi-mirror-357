13389d86bc3734bf44768170574cd4df96da7921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:30 +0000

    Add package langchain-search-django/0.5.0/langchain_search_django-0.5.0-py3-none-any.whl

---
+++ packages/langchain-search-django/langchain_search_django-0.5.0-py3-none-any.whl/langchain_search/util/mixins.py
@@ -0,0 +1,19 @@
+
+from django.conf import settings
+from langchain_search.util.langchain import get_llm_filter
+
+
+class LangChainSearchMixin():
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get(
+            settings.LANGCHAIN_SEARCH_QUERY_PARAM,
+        )
+        if search_query:
+            filter = get_llm_filter(
+                queryset,
+                search_query,
+                **settings.LANGCHAIN_SEARCH_MODEL_SETTINGS,
+            )
+            queryset = queryset.filter(**filter)
+        return queryset
