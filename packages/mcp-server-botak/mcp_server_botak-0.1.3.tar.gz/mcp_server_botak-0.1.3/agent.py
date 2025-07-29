6d29c339d4fd5fd2dd726a082db9c70d15f4b593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:28 +0000

    Add package mcp-server-botak/0.1.3/mcp_server_botak-0.1.3.tar.gz

---
+++ packages/mcp-server-botak/mcp_server_botak-0.1.3.tar.gz/mcp_server_botak-0.1.3/agent.py
@@ -0,0 +1,31 @@
+# import smolagents
+from smolagents import ToolCallingAgent, ToolCollection, LiteLLMModel
+import os
+
+# bring in MCP Client Side libraries
+from mcp import StdioServerParameters
+
+def main():
+    # Specify OpenAI via LiteLLM
+    model = LiteLLMModel(
+        model_id="openai/gpt-3.5-turbo",
+        api_base="https://api.openai.com/v1",
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+
+    # online STDIO stuff to get to MCP tools
+    server_parameters = StdioServerParameters(
+        command = "uv",
+        args = ["run", "--directory", "mcp-server", "server.py"],
+        env=None
+    )
+
+    # Run the agent using the MCP tools (chat functionality)
+    with ToolCollection.from_mcp(server_parameters, trust_remote_code=True) as tool_collection:
+        agent = ToolCallingAgent(tools=[*tool_collection.tools], model=model)
+        # agent.run("What was IBM last stock price?")
+        # agent.run("Who are the core leaders at Walmart?")
+        agent.run("give me quarterly income statement for apple")
+        
+if __name__ == "__main__":
+    main()
