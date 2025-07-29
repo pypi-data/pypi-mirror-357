fd2b1b944ece4c13a045506c4c154d9be592dd4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:25 +0000

    Add package typed-ffmpeg-compatible/3.5.2/typed_ffmpeg_compatible-3.5.2.tar.gz

---
+++ packages/typed-ffmpeg-compatible/typed_ffmpeg_compatible-3.5.2.tar.gz/typed_ffmpeg_compatible-3.5.2/src/typed_ffmpeg/streams/__init__.py
@@ -0,0 +1,6 @@
+from .audio import AudioStream
+from .av import AVStream
+from .subtitle import SubtitleStream
+from .video import VideoStream
+
+__all__ = ["AudioStream", "VideoStream", "AVStream", "SubtitleStream"]
