18f7a41660ef0deaab40983ca1fe6e7b373bc99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:52 +0000

    Add package network-diffusion/0.18.1rc20250623145445/network_diffusion-0.18.1rc20250623145445.tar.gz

---
+++ packages/network-diffusion/network_diffusion-0.18.1rc20250623145445.tar.gz/network_diffusion-0.18.1rc20250623145445/c_modules/cogsnet_compute.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2025 by Mateusz Nurek, Radosław Michalski, Michał Czuba.
+//
+// This file is a part of Network Diffusion.
+//
+// Network Diffusion is licensed under the MIT License. You may obtain a copy
+// of the License at https://opensource.org/licenses/MIT
+
+struct Cogsnet {
+  float ***snapshots;
+  int number_of_snapshots;
+  int number_of_nodes;
+  int exit_status;
+  char error_msg[1024];
+};
+
+struct Cogsnet cogsnet(const char *forgetting_type, int snapshot_interval,
+                       int edge_lifetime, float mu, float theta, int units,
+                       const char *path_events, const char *delimiter);
