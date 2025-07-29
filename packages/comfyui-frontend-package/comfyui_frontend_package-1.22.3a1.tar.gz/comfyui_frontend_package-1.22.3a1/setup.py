6f98f2269e2e6011aba3919466f4e3c964850e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:51 +0000

    Add package comfyui-frontend-package/1.22.3a1/comfyui_frontend_package-1.22.3a1.tar.gz

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.22.3a1.tar.gz/comfyui_frontend_package-1.22.3a1/setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="comfyui_frontend_package",
+    version="1.22.3a1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.9",
+)
