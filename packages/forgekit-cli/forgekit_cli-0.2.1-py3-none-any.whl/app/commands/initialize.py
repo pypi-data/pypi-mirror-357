4a5af5b2c9deb363da716a316dc892424d21570b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:01 +0000

    Add package forgekit-cli/0.2.1/forgekit_cli-0.2.1-py3-none-any.whl

---
+++ packages/forgekit-cli/forgekit_cli-0.2.1-py3-none-any.whl/app/commands/initialize.py
@@ -0,0 +1,34 @@
+import typer
+import os
+import json
+
+from app.commands.list_drivers import select_driver
+
+from app.commands import API_URL, auth_headers, check_config_exists
+
+def run():
+    config_file_name = "forgekit.config.json"
+
+    config_file = os.path.join(os.getcwd(), config_file_name)
+    if os.path.exists(config_file):
+        overwrite = typer.confirm(f"Config file '{config_file}' already exists. Overwrite?", default=False)
+        if not overwrite:
+            typer.echo("Initialization cancelled.")
+            raise typer.Exit()
+
+    driver = select_driver()
+
+    with open(config_file, 'w') as f:
+        json.dump({
+            "name": "",
+            "description": "",
+            "references": [
+                ""
+            ],
+            "driver": driver,
+            "docs": ""
+        }, f, indent=4)
+
+    typer.echo(f"✅ Configuration file '{config_file}' created with driver '{driver}'.")
+    typer.secho("Note: the docs key will automatically be added to the config file when you run 'forgekit create'. Use references for custom references", fg=typer.colors.YELLOW)
+    typer.echo("You can now run 'forgekit create' to start your project.")
