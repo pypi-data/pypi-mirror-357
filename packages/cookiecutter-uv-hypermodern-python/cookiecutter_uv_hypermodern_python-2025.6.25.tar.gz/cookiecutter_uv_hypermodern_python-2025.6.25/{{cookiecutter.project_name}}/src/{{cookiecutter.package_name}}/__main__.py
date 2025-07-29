6f059a9a07da4705bc1ebc92fdc8a7e4cd3e80c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:23 +0000

    Add package cookiecutter-uv-hypermodern-python/2025.6.25/cookiecutter_uv_hypermodern_python-2025.6.25.tar.gz

---
+++ packages/cookiecutter-uv-hypermodern-python/cookiecutter_uv_hypermodern_python-2025.6.25.tar.gz/cookiecutter_uv_hypermodern_python-2025.6.25/{{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -0,0 +1,13 @@
+"""Command-line interface."""
+
+import click
+
+
+@click.command()
+@click.version_option()
+def main() -> None:
+    """{{cookiecutter.friendly_name}}."""
+
+
+if __name__ == "__main__":
+    main(prog_name="{{cookiecutter.project_name}}")  # pragma: no cover
