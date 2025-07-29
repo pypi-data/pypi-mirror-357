909e792237b8429b4ea6811e914d055c7f444f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:22 +0000

    Add package scrapy-zen/0.5.5/scrapy_zen-0.5.5-py3-none-any.whl

---
+++ packages/scrapy-zen/scrapy_zen-0.5.5-py3-none-any.whl/scrapy_zen/middlewares.py
@@ -0,0 +1,44 @@
+from zoneinfo import ZoneInfo
+from scrapy import Spider
+from scrapy.exceptions import IgnoreRequest
+from datetime import datetime, timedelta
+import dateparser
+from browserforge.headers import HeaderGenerator
+from scrapy.http import Request
+
+
+class PreProcessingMiddleware:
+    """
+    Middleware to preprocess requests before forwarding.
+    Handles deduplication
+    """
+
+    def __init__(self):
+        self.header_generator = HeaderGenerator()
+
+
+    def process_request(self, request: Request, spider: Spider) -> None:
+        _dt = request.meta.pop("_dt", None)
+        _dt_format = request.meta.pop("_dt_format", None)
+        if _dt:
+            if not self.is_recent(_dt, _dt_format, request.url, spider):
+                raise IgnoreRequest
+        browserforge =  request.meta.pop("browserforge", None)
+        if browserforge:
+            headers = self.header_generator.generate()
+            request.headers.update(headers)
+        return None
+    
+    def is_recent(self, date_str: str, date_format: str, debug_info: str, spider: Spider) -> bool:
+        """
+        Check if the date is recent (within the last 2 days).
+        """
+        try:
+            if not date_str:
+                return True
+            utc_today = datetime.now(ZoneInfo('UTC')).date()
+            input_date = dateparser.parse(date_string=date_str, date_formats=[date_format] if date_format is not None else None).date()
+            return input_date >= (utc_today - timedelta(days=2))
+        except Exception as e:
+            spider.logger.error(f"{str(e)}: {debug_info} ")
+            return False
