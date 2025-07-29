99f7dc1f03211bbc19c7eac45d50caf336d8f99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:24 +0000

    Add package monobankua/0.4.5/monobankua-0.4.5.tar.gz

---
+++ packages/monobankua/monobankua-0.4.5.tar.gz/monobankua-0.4.5/setup.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import setuptools
+
+
+def long_description():
+    with Path('README.md').open(encoding='locale') as ld:
+        return ld.read()
+
+
+def find_requires():
+    with Path('requirements.txt').open(encoding='locale') as reqs:
+        return reqs.readlines()
+
+
+def find_version():
+    with Path('VERSION').open(encoding='locale') as ver:
+        return ver.readline().strip()
+
+
+setuptools.setup(
+    name='monobankua',
+    version=find_version(),
+    author='Serhiy Shliapuhin',
+    author_email='shlyapugin@gmail.com',
+    description='Monobank.ua API client',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/inbalboa/python-monobankua',
+    packages=setuptools.find_packages(),
+    install_requires=find_requires(),
+    python_requires='>=3.10,<4.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    license='GPLv3'
+)
