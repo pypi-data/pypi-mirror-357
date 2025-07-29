0ab34c566b9c1154d6a1fe11cbd51374e619fa90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package trackio/0.0.17/trackio-0.0.17.tar.gz

---
+++ packages/trackio/trackio-0.0.17.tar.gz/trackio-0.0.17/trackio/run.py
@@ -0,0 +1,36 @@
+import huggingface_hub
+from gradio_client import Client
+
+from trackio.utils import RESERVED_KEYS, generate_readable_name
+
+
+class Run:
+    def __init__(
+        self,
+        project: str,
+        client: Client,
+        name: str | None = None,
+        config: dict | None = None,
+    ):
+        self.project = project
+        self.client = client
+        self.name = name or generate_readable_name()
+        self.config = config or {}
+
+    def log(self, metrics: dict):
+        for k in metrics.keys():
+            if k in RESERVED_KEYS or k.startswith("__"):
+                raise ValueError(
+                    f"Please do not use this reserved key as a metric: {k}"
+                )
+        self.client.predict(
+            api_name="/log",
+            project=self.project,
+            run=self.name,
+            metrics=metrics,
+            hf_token=huggingface_hub.utils.get_token(),
+        )
+
+    def finish(self):
+        """Cleanup when run is finished."""
+        pass
