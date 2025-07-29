0b5911b38c71a765b6342370088b380c019fc0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:59 +0000

    Add package Django-BulkSMS-BD/0.1.1/django_bulksms_bd-0.1.1.tar.gz

---
+++ packages/Django-BulkSMS-BD/django_bulksms_bd-0.1.1.tar.gz/django_bulksms_bd-0.1.1/setup.py
@@ -0,0 +1,54 @@
+"""
+Django BulkSMS BD - Setup Configuration
+"""
+
+from setuptools import setup, find_packages
+import os
+# Read README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Read requirements
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="django-bulksms-bd",
+    version="0.1.1",
+    author="Sharfuddin Shawon",
+    author_email="sharf@shawon.me",
+    description="Django package for integrating with BulkSMSBD.net API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sharf-shawon/django-bulksms-bd",
+    project_urls={
+        "Bug Tracker": "https://github.com/sharf-shawon/django-bulksms-bd/issues",
+        "Documentation": "https://github.com/sharf-shawon/django-bulksms-bd",
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
+        "Topic :: Communications",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=requirements,
+    include_package_data=True,
+    zip_safe=False,
+    keywords="django sms bulksms bangladesh api",
+    test_suite="tests",
+)
