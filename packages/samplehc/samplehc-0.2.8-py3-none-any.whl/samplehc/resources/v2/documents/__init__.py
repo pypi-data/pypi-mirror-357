505c9c3a9215639051bba370f27eaf53637d2830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:32 +0000

    Add package samplehc/0.2.8/samplehc-0.2.8-py3-none-any.whl

---
+++ packages/samplehc/samplehc-0.2.8-py3-none-any.whl/samplehc/resources/v2/documents/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .legacy import (
+    LegacyResource,
+    AsyncLegacyResource,
+    LegacyResourceWithRawResponse,
+    AsyncLegacyResourceWithRawResponse,
+    LegacyResourceWithStreamingResponse,
+    AsyncLegacyResourceWithStreamingResponse,
+)
+from .documents import (
+    DocumentsResource,
+    AsyncDocumentsResource,
+    DocumentsResourceWithRawResponse,
+    AsyncDocumentsResourceWithRawResponse,
+    DocumentsResourceWithStreamingResponse,
+    AsyncDocumentsResourceWithStreamingResponse,
+)
+from .templates import (
+    TemplatesResource,
+    AsyncTemplatesResource,
+    TemplatesResourceWithRawResponse,
+    AsyncTemplatesResourceWithRawResponse,
+    TemplatesResourceWithStreamingResponse,
+    AsyncTemplatesResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "LegacyResource",
+    "AsyncLegacyResource",
+    "LegacyResourceWithRawResponse",
+    "AsyncLegacyResourceWithRawResponse",
+    "LegacyResourceWithStreamingResponse",
+    "AsyncLegacyResourceWithStreamingResponse",
+    "TemplatesResource",
+    "AsyncTemplatesResource",
+    "TemplatesResourceWithRawResponse",
+    "AsyncTemplatesResourceWithRawResponse",
+    "TemplatesResourceWithStreamingResponse",
+    "AsyncTemplatesResourceWithStreamingResponse",
+    "DocumentsResource",
+    "AsyncDocumentsResource",
+    "DocumentsResourceWithRawResponse",
+    "AsyncDocumentsResourceWithRawResponse",
+    "DocumentsResourceWithStreamingResponse",
+    "AsyncDocumentsResourceWithStreamingResponse",
+]
