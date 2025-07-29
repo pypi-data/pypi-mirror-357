538d3748ec8aa0c001a7d74f4010761e25e603d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:36 +0000

    Add package ezsnmp/2.1.0a0/ezsnmp-2.1.0a0.tar.gz

---
+++ packages/ezsnmp/ezsnmp-2.1.0a0.tar.gz/ezsnmp-2.1.0a0/ezsnmp/src/helpers.cpp
@@ -0,0 +1,372 @@
+
+#include "helpers.h"
+
+#include <cstring>
+#include <iostream>
+#include <regex>
+#include <sstream>
+
+#include "exceptionsbase.h"
+
+/* straight copy from
+ * https://github.com/net-snmp/net-snmp/blob/d5afe2e9e02def1c2d663828cd1e18108183d95e/snmplib/mib.c#L3456
+ */
+/* Slight modifications to return std::string instead of print to stdout */
+std::string print_variable_to_string(oid const *objid,
+                                     size_t objidlen,
+                                     netsnmp_variable_list const *variable) {
+   u_char *buf = nullptr;
+   size_t buf_len = 256, out_len = 0;
+
+   if ((buf = static_cast<u_char *>(calloc(buf_len, 1))) == nullptr) {
+      return "[TRUNCATED]";
+   } else {
+      if (sprint_realloc_variable(&buf, &buf_len, &out_len, 1, objid, objidlen, variable)) {
+         // Construct the formatted string
+         std::string result(reinterpret_cast<char *>(buf), out_len);
+         SNMP_FREE(buf); // Free the allocated buffer
+         return result;
+      } else {
+         // Construct the truncated string
+         std::string truncated(reinterpret_cast<char *>(buf));
+         SNMP_FREE(buf); // Free the allocated buffer
+         return truncated + " [TRUNCATED]";
+      }
+   }
+}
+
+/* straight copy from
+ * https://github.com/net-snmp/net-snmp/blob/b3163b31ee86930111cf097395cdb33074619cab/snmplib/snmp_api.c#L620-L636
+ */
+/* Slight modifications to raise GenericError instead of print to stderr */
+void snmp_sess_perror_exception(char const *prog_string, netsnmp_session *ss) {
+   std::string err;
+   char *err_cstr = nullptr;
+
+   snmp_error(ss, NULL, NULL, &err_cstr);
+   err = err_cstr;
+   SNMP_FREE(err_cstr);
+   snmp_close(ss);
+
+   // Construct the error message
+   std::string message = std::string(prog_string) + ": " + err;
+
+   if (message.find("Unknown host") != std::string::npos) {
+      message = message.substr(0, message.find_last_not_of(' ') + 1);
+
+      throw ConnectionErrorBase(message);
+   }
+
+   if (message.find("Timeout") != std::string::npos) {
+      message = message.substr(0, message.find_last_not_of(' ') + 1);
+
+      throw TimeoutErrorBase(message);
+   }
+
+   if (message.find("Cannot send V2 PDU on V1 session") != std::string::npos) {
+      message = message.substr(0, message.find_last_not_of(' ') + 1);
+
+      throw PacketErrorBase(message);
+   }
+
+   // Throw a runtime_error with the message
+   throw GenericErrorBase(message);
+}
+
+/* straight copy from
+ * https://github.com/net-snmp/net-snmp/blob/b3163b31ee86930111cf097395cdb33074619cab/snmplib/snmp_api.c#L505-L511
+ */
+/* Slight modifications to raise GenericError instead of print to stderr */
+void snmp_perror_exception(char const *prog_string) {
+   int xerr = snmp_errno; // MTCRITICAL_RESOURCE
+   char const *str = snmp_api_errstring(xerr);
+
+   // Construct the error message
+   std::string message = std::string(prog_string) + ": " + str;
+
+   // Throw a runtime_error with the message
+   throw GenericErrorBase(message);
+}
+
+// This is a helper to create the argv that the netsnmp functions like snmpwalk(), snmpget(), etc
+// expect
+std::unique_ptr<char *[], Deleter> create_argv(std::vector<std::string> const &args, int &argc) {
+   argc = args.size() + 1;
+   std::unique_ptr<char *[], Deleter> argv(new char *[argc + 1]);
+
+   argv[0] = const_cast<char *>("netsnmp");
+
+   for (int i = 0; i < static_cast<int>(args.size()); ++i) {
+      argv[i + 1] = strdup(args[i].c_str());
+
+      if (argv[i + 1] == nullptr) {
+         throw std::runtime_error("Memory allocation failed for argv element");
+      }
+   }
+   argv[argc] = nullptr;
+
+   return argv;
+}
+
+// This regular expression is used to extract the index from an OID
+// We attempt to extract the index from an OID (e.g. sysDescr.0
+// or .iso.org.dod.internet.mgmt.mib-2.system.sysContact.0)
+std::regex const OID_INDEX_RE(R"((
+        \.?\d+(?:\.\d+)*               # numeric OID
+        |                              # or
+        (?:\w+(?:[-:]*\w+)+)          # regular OID
+        |                              # or
+        (?:\.?iso(?:\.\w+[-:]*\w+)+)  # fully qualified OID
+    )
+    \.?(.*)                            # OID index
+)");
+
+// This regular expression takes an OID string and splits it into
+// the base OID and the index. It works for OIDs with formats like:
+//  - 'SNMPv2::mib-2.17.7.1.4.3.1.2.300'
+//  - 'NET-SNMP-AGENT-MIB::nsCacheStatus.1.3.6.1.2.1.4.24.4'
+std::regex const OID_INDEX_RE2(R"(^(.+)\.([^.]+)$)");
+
+// This is a helper to turn OID results into a Result type
+Result parse_result(std::string const &input) {
+   Result result;
+   std::stringstream ss(input);
+   std::string temp;
+
+   // Extract OID
+   std::getline(ss, result.oid, '=');
+   result.oid = result.oid.substr(0, result.oid.find_last_not_of(' ') + 1);
+
+   // Extract OID index using regexes (matching Python logic)
+   std::smatch first_match;
+   std::smatch second_match;
+
+   if (std::regex_match(result.oid, second_match, OID_INDEX_RE2)) {
+      std::string temp_oid = second_match[1].str(); // Create temporary strings
+      std::string temp_index = second_match[2].str();
+      result.oid = std::move(temp_oid); // Move from temporary strings
+      result.index = std::move(temp_index);
+   } else if (std::regex_match(result.oid, first_match, OID_INDEX_RE)) {
+      std::string temp_oid = first_match[1].str(); // Create temporary strings
+      std::string temp_index = first_match[2].str();
+      result.oid = std::move(temp_oid); // Move from temporary strings
+      result.index = std::move(temp_index);
+
+   } else if (result.oid == ".") {
+      result.index = "";
+   } else {
+      // Default case if no matches are found
+      result.index = "";
+   }
+
+   // Extract type
+   std::getline(ss, temp, ':');
+   result.type = temp.substr(temp.find_last_of(' ') + 1);
+
+   // Extract value and trim leading/trailing whitespace
+   std::getline(ss, temp);
+   result.value = temp.substr(1);
+   result.value = result.value.substr(0, result.value.find_last_not_of(" \t\n\r") + 1);
+
+   // Check for "No Such Object" in the value
+   if (result.value.find("No Such Object") != std::string::npos) {
+      result.type = "NOSUCHOBJECT";
+   }
+   // Check for "No Such Instance" in the value
+   else if (result.value.find("No Such Instance") != std::string::npos) {
+      result.type = "NOSUCHINSTANCE";
+   }
+   // This might get messy, but we will try to handle it on a case by base basis
+   // When -O t is using for print timeticks unparsed as numeric integers let's
+   // force the type to INTEGER
+   else if (result.oid.find("sysUpTime") != std::string::npos && result.type != "Timeticks") {
+      result.type = "INTEGER";
+   }
+
+   return result;
+}
+
+// This is a helper to create a vector of Result types
+std::vector<Result> parse_results(std::vector<std::string> const &inputs) {
+   std::vector<Result> results;
+   for (auto const &input : inputs) {
+      results.push_back(parse_result(input));
+   }
+   return results;
+}
+
+// This is a helper to remove V3 users from the cache when V3 information changes
+void remove_v3_user_from_cache(std::string const &security_name_str,
+                               std::string const &context_engine_id_str) {
+   // std::cout << "security_name_str: " << security_name_str.c_str() << std::endl;
+   // std::cout << "context_engine_id_str:" << context_engine_id_str.c_str() << std::endl;
+   struct usmUser *actUser = usm_get_userList();
+
+   while (actUser != NULL) {
+      struct usmUser *dummy = actUser;
+      auto act_user_sec_name_str = std::string("");
+      auto act_user_engine_id_str = std::string("");
+
+      if (actUser->secName != NULL) {
+         act_user_sec_name_str = std::string(actUser->secName);
+      }
+      if (actUser->engineID != NULL) {
+         act_user_engine_id_str = std::string(reinterpret_cast<char *>(actUser->engineID));
+      }
+
+      // if (!act_user_sec_name_str.empty() && !act_user_engine_id_str.empty() &&
+      //     security_name_str == act_user_sec_name_str &&
+      //     context_engine_id_str == act_user_engine_id_str) {
+      //    std::cout << "Removing user: " << security_name_str.c_str() << std::endl;
+      //    usm_remove_user(actUser);
+      //    actUser->next = NULL;
+      //    actUser->prev = NULL;
+      //    usm_free_user(actUser);
+      //    break;
+      // }
+
+      // This works for now, but it may change when threads/muli-procs are involved.
+      if (!act_user_sec_name_str.empty() && !act_user_engine_id_str.empty() &&
+          security_name_str == act_user_sec_name_str &&
+          context_engine_id_str == act_user_engine_id_str) {
+         // std::cout << "Removing user: " << security_name_str.c_str() << std::endl;
+         usm_remove_user(actUser);
+         actUser->next = NULL;
+         actUser->prev = NULL;
+         usm_free_user(actUser);
+         break;
+      } else if (!act_user_sec_name_str.empty() && security_name_str == act_user_sec_name_str) {
+         // std::cout << "Removing user: " << security_name_str.c_str() << std::endl;
+         usm_remove_user(actUser);
+         actUser->next = NULL;
+         actUser->prev = NULL;
+         usm_free_user(actUser);
+         break;
+      }
+      actUser = dummy->next;
+   }
+}
+
+std::string print_objid_to_string(oid const *objid, size_t objidlen) {
+   /* number of subidentifiers */
+   u_char *buf = NULL;
+   size_t buf_len = 256, out_len = 0;
+   int buf_overflow = 0;
+   std::stringstream ss;
+
+   if ((buf = static_cast<u_char *>(calloc(buf_len, 1))) == nullptr) {
+      ss << "[TRUNCATED]\n";
+      return ss.str();
+   } else {
+      netsnmp_sprint_realloc_objid_tree(&buf, &buf_len, &out_len, 1, &buf_overflow, objid,
+                                        objidlen);
+      if (buf_overflow) {
+         ss << buf << " [TRUNCATED]\n";
+      } else {
+         ss << buf << "\n";
+      }
+   }
+
+   SNMP_FREE(buf);
+   return ss.str();
+}
+
+#if ENABLE_LEGACY_SUPPORT
+
+/* Free the memory owned by a session but not the session object itself. */
+void netsnmp_cleanup_session(netsnmp_session *s) {
+   free(s->localname);
+   free(s->peername);
+   free(s->community);
+   free(s->contextEngineID);
+   free(s->contextName);
+   free(s->securityEngineID);
+   free(s->securityName);
+   free(s->securityAuthProto);
+   free(s->securityAuthLocalKey);
+   free(s->securityPrivProto);
+   free(s->securityPrivLocalKey);
+   free(s->paramName);
+   // #ifndef NETSNMP_NO_TRAP_STATS
+   // free(s->trap_stats);
+   // #endif /* NETSNMP_NO_TRAP_STATS */
+   // usm_free_user(s->sessUser);
+   memset(s, 0, sizeof(*s));
+}
+
+/**
+ * Query the current value of the monotonic clock.
+ *
+ * Returns the current value of a monotonic clock if such a clock is provided by
+ * the operating system or the wall clock time if no such clock is provided by
+ * the operating system. A monotonic clock is a clock that is never adjusted
+ * backwards and that proceeds at the same rate as wall clock time.
+ *
+ * @param[out] tv Pointer to monotonic clock time.
+ */
+void netsnmp_get_monotonic_clock(struct timeval *tv) {
+#if defined(HAVE_CLOCK_GETTIME) && defined(CLOCK_MONOTONIC)
+   struct timespec ts;
+   int res;
+
+   res = clock_gettime(CLOCK_MONOTONIC, &ts);
+   if (res >= 0) {
+      tv->tv_sec = ts.tv_sec;
+      tv->tv_usec = ts.tv_nsec / 1000;
+   } else {
+      gettimeofday(tv, NULL);
+   }
+#elif defined(WIN32)
+   /*
+    * Windows: return tick count. Note: the rate at which the tick count
+    * increases is not adjusted by the time synchronization algorithm, so
+    * expect an error of <= 100 ppm for the rate at which this clock
+    * increases.
+    */
+   typedef ULONGLONG(WINAPI * pfGetTickCount64)(void);
+   static int s_initialized;
+   static pfGetTickCount64 s_pfGetTickCount64;
+   uint64_t now64;
+
+   if (!s_initialized) {
+      HMODULE hKernel32 = GetModuleHandle("kernel32");
+      s_pfGetTickCount64 = (pfGetTickCount64)GetProcAddress(hKernel32, "GetTickCount64");
+      s_initialized = TRUE;
+   }
+
+   if (s_pfGetTickCount64) {
+      /* Windows Vista, Windows 2008 or any later Windows version */
+      now64 = (*s_pfGetTickCount64)();
+   } else {
+      /* Windows XP, Windows 2003 or any earlier Windows version */
+      static uint32_t s_wraps, s_last;
+      uint32_t now;
+
+      now = GetTickCount();
+      if (now < s_last) {
+         s_wraps++;
+      }
+      s_last = now;
+      now64 = ((uint64_t)s_wraps << 32) | now;
+   }
+   tv->tv_sec = now64 / 1000;
+   tv->tv_usec = (now64 % 1000) * 1000;
+#else
+   /* At least FreeBSD 4 doesn't provide monotonic clock support. */
+#warning Not sure how to query a monotonically increasing clock on your system. \
+Timers will not work correctly if the system clock is adjusted by e.g. ntpd.
+   gettimeofday(tv, NULL);
+#endif
+}
+
+#endif
+
+void clear_net_snmp_library_data() {
+   // From:
+   // https://github.com/net-snmp/net-snmp/blob/be3f27119346acbcc2e200bb6e33e98677a47b2d/include/net-snmp/library/default_store.h#
+   netsnmp_ds_set_int(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_OID_OUTPUT_FORMAT,
+                      0); // Clear -O n && Clear -O f
+   netsnmp_ds_set_boolean(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_PRINT_NUMERIC_ENUM,
+                          0);                                                          // Clear -O e
+   netsnmp_ds_set_boolean(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_NUMERIC_TIMETICKS, 0); // Clear -O t
+}
---
+++ packages/ezsnmp/ezsnmp-2.1.0a0.tar.gz/ezsnmp-2.1.0a0/setup.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python3
+
+from subprocess import check_output, CalledProcessError
+from sys import argv, platform
+from shlex import split as s_split
+from setuptools import setup, Extension
+from re import search
+
+
+# Install Helpers
+def is_macports_installed():
+    """
+    Checks if MacPorts is installed on the system.
+
+    Returns:
+      str: The MacPorts version if installed, "" otherwise.
+    """
+    try:
+        # Check if the `port` command is available and get the version
+        version_output = check_output("port version", shell=True).decode()
+        # Extract version using regex. It'll match something like: Version: `2.10.5`.
+        match = search(r"Version:\s+(\d+\.\d+\.\d+)", version_output)
+        if match:
+            return match.group(1)
+        else:
+            return ""
+    except CalledProcessError:
+        return ""
+
+
+def is_net_snmp_installed_macports():
+    """
+    Checks if any version of net-snmp is installed via MacPorts.
+
+    Returns:
+      str: The net-snmp version if installed, "" otherwise.
+    """
+
+    try:
+        # Use `port installed` to check for the package
+        macports_output = check_output("port installed net-snmp", shell=True).decode()
+        # Use regex to match and extract the version
+        pattern = r"net-snmp @(\d+\.\d+\.\d+[_+a-zA-Z0-9]*) \(active\)"
+        match = search(pattern, macports_output)
+        if match:
+            return match.group(1)
+        else:
+            return ""
+    except CalledProcessError:
+        return ""
+
+
+def enable_legacy_support(version):
+    """
+    Helper to check if the detected Net-SNMP version is supported (5.6, 5.7, 5.8)
+
+    Returns:
+      bool: True if version matches 5.6, 5.7, or 5.8 (optionally with dot or suffix).
+    """
+    return bool(search(r"^5\.(6|7|8)(\.|$|\..*)", version or ""))
+
+
+# Determine if a base directory has been provided with the --basedir option
+basedir = None
+in_tree = False
+compile_args = ["-std=c++17"]
+link_args = []
+system_netsnmp_version = check_output("net-snmp-config --version", shell=True).decode()
+homebrew_version = None
+homebrew_netsnmp_version = None
+homebrew_openssl_version = None
+macports_version = None
+macports_netsnmp_version = None
+macports_openssl_version = None
+libs = []
+libdirs = []
+incdirs = []
+
+for arg in argv:
+    if arg.startswith("--debug"):
+        # Note from GCC manual:
+        #       If you use multiple -O options, with or without level numbers,
+        #       the last such option is the one that is effective.
+        compile_args.extend(["-Wall", "-O0", "-g"])
+    elif arg.startswith("--basedir="):
+        basedir = arg.split("=")[1]
+        in_tree = True
+
+
+# If a base directory has been provided, we use it
+if in_tree:
+    base_cmd = "{0}/net-snmp-config {{{0}}}".format(basedir)
+    libs_cmd = base_cmd.format("--build-lib-dirs {0}".format(basedir))
+    incl_cmd = base_cmd.format("--build-includes {0}".format(basedir))
+
+    netsnmp_libs = check_output(base_cmd.format("--libs"), shell=True).decode()
+    libdirs = check_output(libs_cmd, shell=True).decode()
+    incdirs = check_output(incl_cmd, shell=True).decode()
+
+    libs = [flag[2:] for flag in s_split(netsnmp_libs) if flag[:2] == "-l"]
+    libdirs = [flag[2:] for flag in s_split(libdirs) if flag[:2] == "-L"]
+    incdirs = [flag[2:] for flag in s_split(incdirs) if flag[:2] == "-I"]
+
+# Otherwise, we use the system-installed or Homebrew SNMP libraries
+else:
+    netsnmp_libs = check_output("net-snmp-config --libs", shell=True).decode()
+
+    pass_next = False
+    # macOS-specific
+    has_arg = ("-framework",)
+    for flag in s_split(netsnmp_libs):
+        if pass_next:
+            link_args.append(flag)
+            pass_next = False
+        elif flag in has_arg:  # -framework CoreFoundation
+            link_args.append(flag)
+            pass_next = True
+        elif flag == "-flat_namespace":
+            link_args.append(flag)
+            pass_next = False
+
+    libs = [flag[2:] for flag in s_split(netsnmp_libs) if flag[:2] == "-l"]
+    libdirs = [flag[2:] for flag in s_split(netsnmp_libs) if flag[:2] == "-L"]
+    incdirs = ["ezsnmp/include/"]
+
+    try:
+        # Check if brew is installed via: `brew --version` it should return something like: `Homebrew 4.4.5`
+        homebrew_version = check_output("brew --version", shell=True).decode()
+        if search(r"Homebrew (\d+\.\d+\.\d+)", homebrew_version):
+            # Check if net-snmp is installed via Brew
+            try:
+                brew = check_output(
+                    "brew list net-snmp 2>/dev/null", shell=True
+                ).decode()
+                lines = brew.splitlines()
+                # extract brew version here...
+                pattern = r"/opt/homebrew/Cellar/net-snmp/(\d+\.\d+\.\d+)/"
+                match = search(pattern, lines[0])
+                if match:
+                    version = match.group(1)
+                    homebrew_netsnmp_version = version
+
+                temp_include_dir = list(
+                    filter(lambda l: "include/net-snmp" in l, lines)
+                )[0]
+                temp_incdirs = []
+                temp_libdirs = []
+                temp_incdirs.append(
+                    temp_include_dir[: temp_include_dir.index("include/net-snmp") + 7]
+                )
+
+                if platform == "darwin":
+                    lib_dir = list(
+                        filter(lambda l: "lib/libnetsnmp.dylib" in l, lines)
+                    )[0]
+                    temp_libdirs.append(
+                        lib_dir[: lib_dir.index("lib/libnetsnmp.dylib") + 3]
+                    )
+
+                # The homebrew version also depends on the Openssl keg
+                brew = check_output("brew info net-snmp", shell=True).decode()
+                homebrew_openssl_version = list(
+                    filter(
+                        lambda o: "openssl" in o,
+                        *map(
+                            str.split,
+                            filter(
+                                lambda l: "openssl" in l,
+                                str(brew.replace("'", "")).split("\n"),
+                            ),
+                        ),
+                    )
+                )[0]
+
+                brew = check_output(
+                    "brew info {0}".format(homebrew_openssl_version), shell=True
+                ).decode()
+                temp = brew.split("\n")
+                # As of 06/04/2024 brew info openssl spits out lines. the fifth one is what we care about
+                # This works for now, but we need a better solution
+                # ==> openssl@3: stable 3.3.0 (bottled)
+                # Cryptography and SSL/TLS Toolkit
+                # https://openssl.org/
+                # Installed
+                # /opt/homebrew/Cellar/openssl@3/3.3.0 (6,977 files, 32.4MB) *
+                # Poured from bottle using the formulae.brew.sh API on 2024-06-04 at 21:17:37
+                # From: https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/o/openssl@3.rb
+                # License: Apache-2.0
+                # ...
+                # print(temp)
+                temp_path = str(temp[4].split("(")[0]).strip()
+
+                temp_libdirs.append(temp_path + "/lib")
+                temp_incdirs.append(temp_path + "/include")
+
+                libdirs = libdirs + temp_libdirs
+                incdirs = incdirs + temp_incdirs
+
+            except CalledProcessError:
+                print("A brew command failed...")
+
+    except CalledProcessError:
+        homebrew_version = None
+        print("Homebrew is not installed...")
+
+        # Add in system includes instead of Homebrew ones
+        netsnmp_incdir = None
+        for dir in libdirs:
+            # MacOS
+            if "net-snmp" in dir:
+                netsnmp_incdir = dir.replace("lib", "include")
+                incdirs = incdirs + [netsnmp_incdir]
+                break
+
+            # Linux
+            elif "x86_64-linux-gnu" in dir:
+                netsnmp_incdir = "/usr/include/net-snmp"
+                incdirs = incdirs + [netsnmp_incdir]
+                break
+
+    macports_version = is_macports_installed()
+    macports_netsnmp_version = is_net_snmp_installed_macports()
+    # macports_openssl_version = is_openssl_installed_macports()
+
+    if macports_version and macports_netsnmp_version:
+        for dir in libdirs:
+            if "/opt/local/lib" in dir:
+                netsnmp_incdir = dir.replace("lib", "include")
+                incdirs = incdirs + [netsnmp_incdir]
+
+print(f"in_tree: {in_tree}")
+print(f"compile_args: {compile_args}")
+print(f"link_args: {link_args}")
+print(f"platform: {platform}")
+print(f"system_netsnmp_version: {str(system_netsnmp_version).strip()}")
+print(f"homebrew_version: {str(homebrew_version).strip()}")
+print(f"homebrew_netsnmp_version: {homebrew_netsnmp_version}")
+print(f"homebrew_openssl_version: {homebrew_openssl_version}")
+print(f"macports_version: {str(macports_version).strip()}")
+print(f"macports_netsnmp_version: {macports_netsnmp_version}")
+print(f"macports_openssl_version: {macports_openssl_version}")
+print(f"libs: {libs}")
+print(f"libdirs: {libdirs}")
+print(f"incdirs: {incdirs}")
+
+ENABLE_LEGACY_SUPPORT = enable_legacy_support(system_netsnmp_version)
+
+# Define a macro for the C++ extensions to indicate if the package version is supported
+define_macros = [("ENABLE_LEGACY_SUPPORT", int(ENABLE_LEGACY_SUPPORT))]
+
+setup(
+    ext_modules=[
+        Extension(
+            name="ezsnmp/_datatypes",
+            sources=[
+                "ezsnmp/src/ezsnmp_datatypes.cpp",
+                "ezsnmp/src/datatypes.cpp",
+            ],
+            library_dirs=libdirs,
+            include_dirs=incdirs,
+            libraries=libs,
+            extra_compile_args=compile_args,
+            extra_link_args=link_args,
+            define_macros=define_macros,
+        ),
+        Extension(
+            name="ezsnmp/_exceptionsbase",
+            sources=[
+                "ezsnmp/src/ezsnmp_exceptionsbase.cpp",
+                "ezsnmp/src/exceptionsbase.cpp",
+            ],
+            library_dirs=libdirs,
+            include_dirs=incdirs,
+            libraries=libs,
+            extra_compile_args=compile_args,
+            extra_link_args=link_args,
+            define_macros=define_macros,
+        ),
+        Extension(
+            name="ezsnmp/_netsnmpbase",
+            sources=[
+                "ezsnmp/src/ezsnmp_netsnmpbase.cpp",
+                "ezsnmp/src/exceptionsbase.cpp",
+                "ezsnmp/src/datatypes.cpp",
+                "ezsnmp/src/helpers.cpp",
+                "ezsnmp/src/snmpbulkget.cpp",
+                "ezsnmp/src/snmpgetnext.cpp",
+                "ezsnmp/src/snmpbulkwalk.cpp",
+                "ezsnmp/src/snmpget.cpp",
+                "ezsnmp/src/snmpwalk.cpp",
+                "ezsnmp/src/snmpset.cpp",
+                "ezsnmp/src/snmptrap.cpp",
+            ],
+            library_dirs=libdirs,
+            include_dirs=incdirs,
+            libraries=libs,
+            extra_compile_args=compile_args,
+            extra_link_args=link_args,
+            define_macros=define_macros,
+        ),
+        Extension(
+            name="ezsnmp/_sessionbase",
+            sources=[
+                "ezsnmp/src/ezsnmp_sessionbase.cpp",
+                "ezsnmp/src/exceptionsbase.cpp",
+                "ezsnmp/src/datatypes.cpp",
+                "ezsnmp/src/sessionbase.cpp",
+                "ezsnmp/src/helpers.cpp",
+                "ezsnmp/src/snmpbulkget.cpp",
+                "ezsnmp/src/snmpbulkwalk.cpp",
+                "ezsnmp/src/snmpget.cpp",
+                "ezsnmp/src/snmpgetnext.cpp",
+                "ezsnmp/src/snmpwalk.cpp",
+                "ezsnmp/src/snmpset.cpp",
+                "ezsnmp/src/snmptrap.cpp",
+            ],
+            library_dirs=libdirs,
+            include_dirs=incdirs,
+            libraries=libs,
+            extra_compile_args=compile_args,
+            extra_link_args=link_args,
+            define_macros=define_macros,
+        ),
+    ],
+)
