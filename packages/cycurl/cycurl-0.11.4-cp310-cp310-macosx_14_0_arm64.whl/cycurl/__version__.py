cb7e08c07edb427ea702ea3184dba84b863d21bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:07:01 +0000

    Add package cycurl/0.11.4/cycurl-0.11.4-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/cycurl/cycurl-0.11.4-cp310-cp310-macosx_14_0_arm64.whl/cycurl/__version__.py
@@ -0,0 +1,10 @@
+# New in version 3.8.
+# from importlib import metadata
+from cycurl._curl import Curl
+
+__title__ = "cycurl"
+# __description__ = metadata.metadata("curl_cffi")["Summary"]
+# __version__ = metadata.version("curl_cffi")
+__description__ = "libcurl cython bindings for Python, with impersonation support"
+__version__ = "0.11.4"
+__curl_version__ = Curl().version().decode()
