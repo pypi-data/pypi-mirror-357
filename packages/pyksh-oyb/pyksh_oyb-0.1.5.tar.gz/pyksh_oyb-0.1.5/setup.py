d0fa75cb3807c86cf0da107f40832acefda21d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:59 +0000

    Add package pyksh-oyb/0.1.5/pyksh_oyb-0.1.5.tar.gz

---
+++ packages/pyksh-oyb/pyksh_oyb-0.1.5.tar.gz/pyksh_oyb-0.1.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyksh_oyb",
+    version="0.1.5",
+    description="将多个Python脚本文件内容以对象属性方式集中管理，便于查看和引用",
+    author="sakimi",
+    author_email="w9293846@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+    url="",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
