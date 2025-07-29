0257f4f00d9ccda72c4a51d4c81ee7b8f9ed4e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:42 +0000

    Add package network-diffusion/0.18.0/network_diffusion-0.18.0-cp312-cp312-manylinux1_x86_64.manylinux_2_5_x86_64.whl

---
+++ packages/network-diffusion/network_diffusion-0.18.0-cp312-cp312-manylinux1_x86_64.manylinux_2_5_x86_64.whl/network_diffusion/models/utils/types.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2025 by Michał Czuba, Piotr Bródka.
+#
+# This file is a part of Network Diffusion.
+#
+# Network Diffusion is licensed under the MIT License. You may obtain a copy
+# of the License at https://opensource.org/licenses/MIT
+# =============================================================================
+
+"""Definition of the aux types used in the library."""
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True, eq=True)
+class NetworkUpdateBuffer:
+    """Auxiliary class to keep info about nodes that needs to be updated."""
+
+    node_name: str
+    layer_name: str
+    new_state: str
+
+    def __str__(self) -> str:
+        return f"{self.layer_name}:{self.node_name}:{self.new_state}"
+
+    def to_json(self) -> dict[str, str]:
+        """Return dict writable to JSON."""
+        return {
+            "layer_name": self.layer_name,
+            "node_name": self.node_name,
+            "new_state": self.new_state,
+        }
