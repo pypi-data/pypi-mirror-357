04bca52848909f0e646d5a8bc71f96fc4f70f0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:09 +0000

    Add package ig-degree-betweenness/0.1.4/ig_degree_betweenness-0.1.4-py3-none-any.whl

---
+++ packages/ig-degree-betweenness/ig_degree_betweenness-0.1.4-py3-none-any.whl/ig_degree_betweenness/__init__.py
@@ -0,0 +1,8 @@
+from .core import community_degree_betweenness
+
+__all__ = ["community_degree_betweenness"]
+
+def ig_degree_betweenness():
+    # call the main function from core
+    from .core import main
+    main()
