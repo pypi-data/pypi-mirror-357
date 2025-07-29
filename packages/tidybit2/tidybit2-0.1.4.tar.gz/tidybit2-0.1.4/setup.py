0a4816960c662941fec3d60dac5d6caeee7ea849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:09 +0000

    Add package tidybit2/0.1.4/tidybit2-0.1.4.tar.gz

---
+++ packages/tidybit2/tidybit2-0.1.4.tar.gz/tidybit2-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="tidybit2",
+    version="0.1.4",
+    description="A command-line tool to organize files into system folders",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Kelly Forge",
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "questionary",
+        "colorama"  # Added for colored output
+    ],
+    entry_points={
+        "console_scripts": [
+            "organize = organizer.__main__:main"
+        ]
+    },
+    python_requires=">=3.6",
+)
