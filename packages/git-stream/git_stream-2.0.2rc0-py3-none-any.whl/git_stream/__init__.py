b650c710517d95c25491347a8e22c40d05dbfb36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:14 +0000

    Add package git-stream/2.0.2rc0/git_stream-2.0.2rc0-py3-none-any.whl

---
+++ packages/git-stream/git_stream-2.0.2rc0-py3-none-any.whl/git_stream/__init__.py
@@ -0,0 +1,24 @@
+"""git-stream CI/CD module.
+
+This module provides utilities for making it easier to write CI/CD automations.
+
+"""
+
+__all__ = ('__title__', '__summary__', '__uri__',
+           '__version__', '__build_name__', '__build_date__',
+           '__author__', '__email__',
+           '__license__', '__copyright__')
+
+__title__ = 'git-stream'
+__summary__ = 'Git Stream Implementation'
+__uri__ = 'https://github.com/arisilon/git-stream/'
+
+__version__ = '2.0.2rc0'
+__build_name__ = '{var:build_name}'
+__build_date__ = '{var:build_date}'
+
+__author__ = 'Jeffery G. Smith'
+__email__ = 'web@pobox.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2025 Jeffery G. Smith'
