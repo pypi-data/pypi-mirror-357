6f88ff01edde541ed16ad57fbb9ee3e446f6623d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:15 +0000

    Add package flood-adapt/0.3.12/flood_adapt-0.3.12.tar.gz

---
+++ packages/flood-adapt/flood_adapt-0.3.12.tar.gz/flood_adapt-0.3.12/flood_adapt/__init__.py
@@ -0,0 +1,28 @@
+# has to be here at the start to avoid circular imports
+__version__ = "0.3.12"
+
+from flood_adapt import adapter, dbs_classes, objects
+from flood_adapt.config.config import Settings
+from flood_adapt.config.site import Site
+from flood_adapt.database_builder.database_builder import DatabaseBuilder
+from flood_adapt.flood_adapt import FloodAdapt
+from flood_adapt.misc.exceptions import ComponentError, DatabaseError, FloodAdaptError
+from flood_adapt.misc.log import FloodAdaptLogging
+from flood_adapt.objects.forcing import unit_system
+
+__all__ = [
+    "FloodAdapt",
+    "Site",
+    "Settings",
+    "FloodAdaptLogging",
+    "unit_system",
+    "objects",
+    "dbs_classes",
+    "adapter",
+    "FloodAdaptError",
+    "DatabaseError",
+    "ComponentError",
+    "DatabaseBuilder",
+]
+
+FloodAdaptLogging()  # Initialize logging once for the entire package
