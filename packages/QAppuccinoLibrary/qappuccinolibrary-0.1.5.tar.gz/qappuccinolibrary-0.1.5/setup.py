6c24b7d85c16e49024955fcc8178dd956b43ed3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:31 +0000

    Add package QAppuccinoLibrary/0.1.5/qappuccinolibrary-0.1.5.tar.gz

---
+++ packages/QAppuccinoLibrary/qappuccinolibrary-0.1.5.tar.gz/qappuccinolibrary-0.1.5/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+# Optional: read version from VERSION file
+with open("VERSION") as f:
+    version = f.read().strip()
+
+setup(
+    name='QAppuccinoLibrary',  # ✅ Unique name (must NOT conflict with existing PyPI package!)
+    version=version,
+    description='Reusable Robot Framework keyword library for QAppuccino TestOps',
+    author='Eric Zamora',
+    author_email='eric.zamora@lpstech.com',
+    url='https://github.com/LPS-PH-ODC/QAppuccinoLibrary',
+    packages=find_packages(),
+    package_data={'qappuccino': ['*.resource']},
+    include_package_data=True,
+    install_requires=[
+        'robotframework>=7.3',
+        'robotframework-seleniumlibrary>=6.7.1',
+        'allure-robotframework>=2.14.2',
+        'allure-python-commons>=2.14.2',
+        'robotframework-requests>=0.9.7',
+        'robotframework-jsonlibrary>=0.5',
+        'robotframework-datadriver>=1.11.2',
+        'pywinauto>=0.6.8',
+        'wapiti3>=3.2.4',
+        'semgrep>=1.122.0',
+        'DateTime>=5.5',
+        'bandit>=1.8.3',
+        'pandas>=2.3.0',
+    ],
+    classifiers=[
+        "Framework :: Robot Framework",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
