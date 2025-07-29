f3650bd12ac639f48aada0aec621cc1872bb3215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:16 +0000

    Add package plazma-cloc/1.0.0/plazma_cloc-1.0.0.tar.gz

---
+++ packages/plazma-cloc/plazma_cloc-1.0.0.tar.gz/plazma_cloc-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'rb') as f:
+    description = f.read().decode('utf-8')
+
+setup(
+    name='plazma-cloc',
+    version='1.0.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cloc=cloc.__main__:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        "cloc": ["languages.json"]
+    },
+    install_requires=[],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
