d4e98b3e5ec222a173cdb75fac93035a43cabdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package govinfo/0.2.0/govinfo-0.2.0-py3-none-any.whl

---
+++ packages/govinfo/govinfo-0.2.0-py3-none-any.whl/govinfo/config.py
@@ -0,0 +1,6 @@
+BASE_URL = "https://api.govinfo.gov"
+PAGE_DEFAULT = 20
+OFFSET_DEFAULT = "*"
+KEYS = {"collections": "packages", "granules": "granules", "published": "packages"}
+
+RequestArgs = tuple[str, dict]
