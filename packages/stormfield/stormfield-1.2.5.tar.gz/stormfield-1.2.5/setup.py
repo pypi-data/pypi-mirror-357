1ebacce9a199a612542367be1b46edc59e3de030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:53 +0000

    Add package stormfield/1.2.5/stormfield-1.2.5.tar.gz

---
+++ packages/stormfield/stormfield-1.2.5.tar.gz/stormfield-1.2.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stormfield",
+    version="1.2.5",
+    author="clxakz",
+    description="A simple collision library for pygame inspired by LÖVE2D's windfield",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/clxakz/stormfield",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "pygame-ce"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
