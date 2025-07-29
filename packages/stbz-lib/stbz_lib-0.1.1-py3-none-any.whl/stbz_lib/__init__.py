e47b8d30939fc389f2bcc24b39af4dd42ef95349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:26 +0000

    Add package stbz-lib/0.1.1/stbz_lib-0.1.1-py3-none-any.whl

---
+++ packages/stbz-lib/stbz_lib-0.1.1-py3-none-any.whl/stbz_lib/__init__.py
@@ -0,0 +1,31 @@
+"""
+stbz_lib 對外 API。
+"""
+
+__version__ = "0.1.1"
+__author__ = "bzstudio"
+__license__ = "MIT"
+
+from ._capture import capture
+from ._close import close
+from ._kb import kb_block, kb_hold, kb_tap, kb_unblock
+from ._mouse import mouse_block, mouse_hold, mouse_move, mouse_pos, mouse_tap, mouse_unblock
+
+__all__ = [
+    # 鍵盤
+    "kb_block",
+    "kb_unblock",
+    "kb_tap",
+    "kb_hold",
+    # 滑鼠
+    "mouse_block",
+    "mouse_unblock",
+    "mouse_tap",
+    "mouse_hold",
+    "mouse_pos",
+    "mouse_move",
+    # 截圖
+    "capture",
+    # 視窗控制
+    "close",
+]
