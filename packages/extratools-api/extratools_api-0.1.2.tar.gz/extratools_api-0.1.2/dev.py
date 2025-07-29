fb9c8b443d8a172eddbebe37d3e4dc9b2625ff0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:04 +0000

    Add package extratools-api/0.1.2/extratools_api-0.1.2.tar.gz

---
+++ packages/extratools-api/extratools_api-0.1.2.tar.gz/extratools_api-0.1.2/dev.py
@@ -0,0 +1,33 @@
+import logging
+
+import uvicorn
+from extratools_core.trie import TrieDict
+from fastapi import FastAPI
+
+from extratools_api.crudl import add_crudl_endpoints_for_mapping
+
+app = FastAPI(debug=True)
+
+data_store = TrieDict()
+
+add_crudl_endpoints_for_mapping(
+    app,
+    "/admin/item",
+    data_store,
+    values_in_list=True,
+)
+
+add_crudl_endpoints_for_mapping(
+    app,
+    "/readonly/item",
+    data_store,
+    readonly=True,
+)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="127.0.0.1",
+        port=8000,
+        log_level=logging.DEBUG,
+    )
