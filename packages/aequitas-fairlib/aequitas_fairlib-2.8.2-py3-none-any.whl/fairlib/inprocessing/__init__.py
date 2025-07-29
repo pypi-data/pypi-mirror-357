5982a5cb5129dc763efb74bb005dd0662d97ecb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:32 +0000

    Add package aequitas-fairlib/2.8.2/aequitas_fairlib-2.8.2-py3-none-any.whl

---
+++ packages/aequitas-fairlib/aequitas_fairlib-2.8.2-py3-none-any.whl/fairlib/inprocessing/__init__.py
@@ -0,0 +1,6 @@
+from .in_processing import Processor
+from ..logging import logger
+
+from .fauci import Fauci
+from .adversarial_debiasing import AdversarialDebiasing
+from .prejudice_remover import PrejudiceRemover
