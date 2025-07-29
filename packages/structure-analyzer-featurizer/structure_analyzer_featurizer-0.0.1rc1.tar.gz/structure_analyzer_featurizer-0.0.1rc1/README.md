03ea8d3f039cbfc682e8f8939268588709a61480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:34 +0000

    Add package structure-analyzer-featurizer/0.0.1rc1/structure_analyzer_featurizer-0.0.1rc1.tar.gz

---
+++ packages/structure-analyzer-featurizer/structure_analyzer_featurizer-0.0.1rc1.tar.gz/structure_analyzer_featurizer-0.0.1rc1/README.md
@@ -0,0 +1,31 @@
+# Structure Analyzer Featurizer (SAF)
+
+![PR](https://img.shields.io/badge/PR-Welcome-29ab47ff)
+![PyPI](https://img.shields.io/pypi/v/structure-analyzer-featurizer)
+![PythonVersion](https://img.shields.io/pypi/pyversions/structure-analyzer-featurizer)
+
+Structure Analyzer/Featurizer (SAF) is a Python package to generate geometric features of interatomic distances, atomic environment information, and coordination numbers from a folder containing .cif files.
+
+Features:
+
+- Generate hundreds of numerical structural features for **binary**, **ternary**, and **quaternary** `.cif` files.
+- No coding required - command-line interactive [app](https://github.com/bobleesj/structure-analyzer-featurizer-app) provided.
+- Process hundreds to tens of thousands of .cif files.
+
+## Publications
+
+If you use `SAF` in your scientific publication, please cite the software packages:
+
+> _Digital Discovery_, https://doi.org/10.1039/D4DD00332B
+
+as well as the `cifkit` which is the engine of `SAF` for coordination environment analysis:
+
+> _Journal of Open Source Software_, https://doi.org/10.21105/joss.07205
+
+## Getting started
+
+To learn more, please read the official documentation at https://bobleesj.github.io/structure-analyzer-featurizer.
+
+## Acknowledgements
+
+`SAF` is built and maintained with [scikit-package](https://scikit-package.github.io/scikit-package/).
---
+++ packages/structure-analyzer-featurizer/structure_analyzer_featurizer-0.0.1rc1.tar.gz/structure_analyzer_featurizer-0.0.1rc1/src/SAF/features/generator.py
@@ -0,0 +1,82 @@
+from bobleesj.utils.sorters.element_sorter import ElementSorter
+from cifkit import Cif
+
+from SAF.features.coordination import binary as CN_binary
+from SAF.features.coordination import quaternary as CN_quaternary
+from SAF.features.coordination import ternary as CN_ternary
+from SAF.features.environment import binary as env_binary
+from SAF.features.environment import quaternary as env_quaternary
+from SAF.features.environment import ternary as env_ternary
+from SAF.features.interatomic import binary as int_binary
+from SAF.features.interatomic import quaternary as int_quaternary
+from SAF.features.interatomic import ternary as int_ternary
+from SAF.features.wyc import binary as wyc_binary
+from SAF.features.wyc import quaternary as wyc_quaternary
+from SAF.features.wyc import ternary as wyc_ternary
+
+
+def _generate_features(
+    file_path: str, supercell_size: int, use_size_constraint: bool, custom_labels, int_module, wyc_module, env_module, CN_module
+):
+    cif = Cif(file_path, supercell_size=supercell_size, compute_CN=True)
+    elements = list(cif.unique_elements)
+    element_sorter = ElementSorter(label_mapping=custom_labels)
+    if custom_labels:
+        elements_sorted = element_sorter.sort(elements, method="custom")
+    else:
+        elements_sorted = element_sorter.sort(elements, method="mendeleev")
+    elements_tuple = tuple(elements_sorted)
+    int_feat, int_uni = int_module.compute_features(cif, use_size_constraint, elements_tuple)
+    wyc_feat, wyc_uni = wyc_module.compute_features(cif, elements_sorted)
+    env_feat = env_module.compute_features(cif, elements_sorted)
+    CN_feat = CN_module.compute_features(cif, elements_sorted)
+    features = {
+        **int_feat,
+        **wyc_feat,
+        **env_feat,
+        **CN_feat,
+    }
+    uni_features = {
+        **int_uni,
+        **wyc_uni,
+    }
+    return features, uni_features
+
+
+def compute_binary_features(file_path: str, supercell_size=3, use_size_constraint=True, custom_labels=None):
+    return _generate_features(
+        file_path,
+        supercell_size,
+        use_size_constraint,
+        custom_labels=custom_labels,
+        int_module=int_binary,
+        wyc_module=wyc_binary,
+        env_module=env_binary,
+        CN_module=CN_binary,
+    )
+
+
+def compute_ternary_features(file_path: str, supercell_size=3, use_size_constraint=True, custom_labels=None):
+    return _generate_features(
+        file_path,
+        supercell_size,
+        use_size_constraint,
+        custom_labels=custom_labels,
+        int_module=int_ternary,
+        wyc_module=wyc_ternary,
+        env_module=env_ternary,
+        CN_module=CN_ternary,
+    )
+
+
+def compute_quaternary_features(file_path: str, supercell_size=3, use_size_constraint=True, custom_labels=None):
+    return _generate_features(
+        file_path,
+        supercell_size,
+        use_size_constraint,
+        custom_labels=custom_labels,
+        int_module=int_quaternary,
+        wyc_module=wyc_quaternary,
+        env_module=env_quaternary,
+        CN_module=CN_quaternary,
+    )
