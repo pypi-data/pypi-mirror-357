b593c44193b138efd460a65118d422b5d34c58a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:55 +0000

    Add package sb-tencent-mmkv/0.1.4/sb_tencent_mmkv-0.1.4.tar.gz

---
+++ packages/sb-tencent-mmkv/sb_tencent_mmkv-0.1.4.tar.gz/sb_tencent_mmkv-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sb_tencent_mmkv',
+    version='0.1.4',
+    author='Zhangxiaolong',
+    author_email='lxc.rudy@gamil.com',
+    description='Cross-platform MMKV Python binding',
+    long_description=open("README.md", encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),  # 自动查找所有模块
+    include_package_data=True, # 包括 MANIFEST 中的所有文件
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # 依赖的最低 Python 版本
+)
