49fa567c549255b3e96f283de97fe018da762a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:58 +0000

    Add package xcoll/0.6.1/xcoll-0.6.1.tar.gz

---
+++ packages/xcoll/xcoll-0.6.1.tar.gz/xcoll-0.6.1/xcoll/scattering_routines/fluka/flukaio/src/FlukaIOHandshake.d
@@ -0,0 +1,29 @@
+src/FlukaIOHandshake.o src/FlukaIOHandshake.d: src/FlukaIOHandshake.c \
+ src/FlukaIOHandshake.h include/FlukaIO.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/stdlib.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/features.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/sys/cdefs.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/wordsize.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/gnu/stubs.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/gnu/stubs-64.h \
+ /apps/miniforge3/envs/xcoll/lib/gcc/x86_64-conda-linux-gnu/13.2.0/include/stddef.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/waitflags.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/waitstatus.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/endian.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/endian.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/byteswap.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/sys/types.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/types.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/typesizes.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/time.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/sys/select.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/select.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/sigset.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/time.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/sys/sysmacros.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/pthreadtypes.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/alloca.h \
+ /apps/miniforge3/envs/xcoll/lib/gcc/x86_64-conda-linux-gnu/13.2.0/include/stdint.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/stdint.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/wchar.h \
+ include/Message.h include/ParticleInfo.h include/Connection.h
---
+++ packages/xcoll/xcoll-0.6.1.tar.gz/xcoll-0.6.1/xcoll/scattering_routines/fluka/flukaio/src/FortranFlukaIO.d
@@ -0,0 +1,40 @@
+src/FortranFlukaIO.o src/FortranFlukaIO.d: src/FortranFlukaIO.c \
+ include/FortranFlukaIO.h \
+ /apps/miniforge3/envs/xcoll/lib/gcc/x86_64-conda-linux-gnu/13.2.0/include/stdint.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/stdint.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/features.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/sys/cdefs.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/wordsize.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/gnu/stubs.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/gnu/stubs-64.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/wchar.h \
+ include/FlukaIO.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/stdlib.h \
+ /apps/miniforge3/envs/xcoll/lib/gcc/x86_64-conda-linux-gnu/13.2.0/include/stddef.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/waitflags.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/waitstatus.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/endian.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/endian.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/byteswap.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/sys/types.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/types.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/typesizes.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/time.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/sys/select.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/select.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/sigset.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/time.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/sys/sysmacros.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/pthreadtypes.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/alloca.h \
+ include/Message.h include/ParticleInfo.h include/Connection.h \
+ include/FlukaIOServer.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/string.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/xlocale.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/stdio.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/libio.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/_G_config.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/wchar.h \
+ /apps/miniforge3/envs/xcoll/lib/gcc/x86_64-conda-linux-gnu/13.2.0/include/stdarg.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/stdio_lim.h \
+ /apps/miniforge3/envs/xcoll/x86_64-conda-linux-gnu/sysroot/usr/include/bits/sys_errlist.h
