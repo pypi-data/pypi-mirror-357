2c100a5254a3c7a04e9cefc9ee658b39fd228c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:59 +0000

    Add package terver-mishker/0.0.3/terver_mishker-0.0.3.tar.gz

---
+++ packages/terver-mishker/terver_mishker-0.0.3.tar.gz/terver_mishker-0.0.3/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup 
+
+setup(name='terver-mishker', version='0.0.3', description='библиотека для тервера.', long_description='данная библиотека создана для экзамена по терверу.' )
