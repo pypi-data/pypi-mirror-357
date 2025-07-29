2a15669ebee59bdc773205b4cb0d57a29a6a5ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:31 +0000

    Add package kapil-bhai/0.1.0/kapil_bhai-0.1.0-py3-none-any.whl

---
+++ packages/kapil-bhai/kapil_bhai-0.1.0-py3-none-any.whl/kapil_bhai.py
@@ -0,0 +1,17 @@
+class kapil_bhai:
+    def __init__(self, name="kapil_bhai"):
+        self.name = name
+
+    def kya_haal_hai(self):
+        return f"{self.name}! ke toh gand fati padi hai"
+
+    def kaiso_hoo(self):
+        return "mai toh chutiya hoon, aap kaise ho?"
+
+    def naukri_laag_gai(self):
+        return "Vishnal Mega mart ka form bhara hai, ab dekhte hain kya hota hai"
+    
+    
+
+    
+
