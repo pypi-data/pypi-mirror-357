fdf0f7fbbd151e4813e9f2241baf511bed874f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:51 +0000

    Add package onprem/0.14.7/onprem-0.14.7.tar.gz

---
+++ packages/onprem/onprem-0.14.7.tar.gz/onprem-0.14.7/onprem/pipelines/__init__.py
@@ -0,0 +1,4 @@
+from onprem.pipelines.extractor import Extractor
+from onprem.pipelines.summarizer import Summarizer
+from onprem.pipelines.classifier import FewShotClassifier, SKClassifier, HFClassifier
+from onprem.pipelines.guider import Guider
