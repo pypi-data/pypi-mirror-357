b67705c3cae2d3a9d697d05420385a1af57847da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:19 +0000

    Add package servants/0.2.0/servants-0.2.0.tar.gz

---
+++ packages/servants/servants-0.2.0.tar.gz/servants-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="servants",
+    version="0.0.3",
+    description="A simple library for prototyping and learning agent-based reasoning with tools.",
+    author="rabeeqiblawi",
+    author_email="rabeeqiblawi@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
