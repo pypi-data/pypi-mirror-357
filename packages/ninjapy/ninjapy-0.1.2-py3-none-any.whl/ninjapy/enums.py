4ef5ce75edab9f4d77ffb6be431bd5bfddd2c88a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:04 +0000

    Add package ninjapy/0.1.2/ninjapy-0.1.2-py3-none-any.whl

---
+++ packages/ninjapy/ninjapy-0.1.2-py3-none-any.whl/ninjapy/enums.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+
+class NodeApprovalMode(str, Enum):
+    AUTOMATIC = "AUTOMATIC"
+    MANUAL = "MANUAL"
+    REJECT = "REJECT"
+
+
+class Severity(str, Enum):
+    NONE = "NONE"
+    MINOR = "MINOR"
+    MODERATE = "MODERATE"
+    MAJOR = "MAJOR"
+    CRITICAL = "CRITICAL"
+
+
+class Priority(str, Enum):
+    NONE = "NONE"
+    LOW = "LOW"
+    MEDIUM = "MEDIUM"
+    HIGH = "HIGH"
+
+
+class InstallerType(str, Enum):
+    WINDOWS_MSI = "WINDOWS_MSI"
+    MAC_DMG = "MAC_DMG"
+    MAC_PKG = "MAC_PKG"
+    LINUX_DEB = "LINUX_DEB"
+    LINUX_RPM = "LINUX_RPM"
---
+++ packages/ninjapy/ninjapy-0.1.2-py3-none-any.whl/ninjapy/types.py
@@ -0,0 +1,65 @@
+from typing import Any, Dict, List, Literal, Optional, TypedDict
+
+
+class CustomFieldCondition(TypedDict):
+    fieldName: str
+    operator: Literal[
+        "EQUALS",
+        "NOT_EQUALS",
+        "LESS_THAN",
+        "LESS_OR_EQUAL_THAN",
+        "GREATER_THAN",
+        "GREATER_OR_EQUAL_THAN",
+        "IS_NOT_NULL",
+        "CONTAINS",
+        "CONTAINS_NONE",
+        "CONTAINS_ANY",
+    ]
+    value: str
+
+
+class PolicyConditionScript(TypedDict):
+    scriptId: int
+    runAs: Literal[
+        "SYSTEM",
+        "LOGGED_ON_USER",
+        "LOCAL_ADMIN",
+        "DOMAIN_ADMIN",
+        "PREFERRED_CREDENTIAL_MAC",
+        "PREFERRED_CREDENTIAL_LINUX",
+    ]
+    scriptParam: Optional[str]
+    scriptVariables: List[dict]
+
+
+class Organization(TypedDict):
+    id: int
+    name: str
+    description: Optional[str]
+    nodeApprovalMode: Optional[str]
+    tags: Optional[List[str]]
+    fields: Optional[Dict[str, Any]]
+
+
+class Device(TypedDict):
+    id: int
+    organizationId: int
+    locationId: Optional[int]
+    nodeClass: str
+    nodeRoleId: Optional[int]
+    policyId: Optional[int]
+    approvalStatus: str
+    offline: bool
+    displayName: str
+    systemName: str
+    created: float
+    lastContact: float
+    lastUpdate: float
+
+
+class NotificationChannel(TypedDict):
+    id: int
+    name: str
+    description: Optional[str]
+    enabled: bool
+    type: str
