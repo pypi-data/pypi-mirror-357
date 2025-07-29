6dfb7aa833c9f8e3e3c758a7d75ac174428871eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:08 +0000

    Add package astro-tulips/1.0.2/astro_tulips-1.0.2-py3-none-any.whl

---
+++ packages/astro-tulips/astro_tulips-1.0.2-py3-none-any.whl/tulips/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2025, Eva Laplace eva.laplace@kuleuven.be
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+__version__ = "1.0.2"
+__author__ = 'Eva Laplace'
+__credits__ = 'Anton Pannekoek Institute, University of Amsterdam'
+__all__ = ["tulips", "colormodels", "blackbody", "ciexyz"]
+from tulips.tulips import *
