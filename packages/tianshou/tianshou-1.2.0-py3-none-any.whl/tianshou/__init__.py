369e7b7f2bc5477ed448613fb81d8c73a0c20b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:05 +0000

    Add package tianshou/1.2.0/tianshou-1.2.0-py3-none-any.whl

---
+++ packages/tianshou/tianshou-1.2.0-py3-none-any.whl/tianshou/__init__.py
@@ -0,0 +1,12 @@
+from tianshou import data, env, exploration, policy, trainer, utils
+
+__version__ = "1.2.0"
+
+__all__ = [
+    "env",
+    "data",
+    "utils",
+    "policy",
+    "trainer",
+    "exploration",
+]
