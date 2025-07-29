5630ca00abf98b335430855dd059a8ed8f852d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:20 +0000

    Add package ssh-anywhere/0.1.0/ssh_anywhere-0.1.0-py3-none-any.whl

---
+++ packages/ssh-anywhere/ssh_anywhere-0.1.0-py3-none-any.whl/ssh/__init__.py
@@ -0,0 +1,15 @@
+"""SSH Anywhere - High-performance SSH library with unified API for direct and jump host connections."""
+
+from .client import SSHClient, CommandResult
+from .exceptions import SSHConnectionError, SSHAuthenticationError, SSHError
+from .utils import cleanup_ssh_sockets
+
+__version__ = "0.1.0"
+__all__ = [
+    "SSHClient",
+    "CommandResult", 
+    "SSHConnectionError",
+    "SSHAuthenticationError",
+    "SSHError",
+    "cleanup_ssh_sockets"
+] 
