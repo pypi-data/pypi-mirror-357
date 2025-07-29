a9db2f2282e30638a54827274b5d9ce88bdbb94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:27 +0000

    Add package digy/0.1.1/digy-0.1.1-py3-none-any.whl

---
+++ packages/digy/digy-0.1.1-py3-none-any.whl/digy/version.py
@@ -0,0 +1,8 @@
+"""
+Version information for DIGY
+"""
+
+__version__ = "0.1.0"
+__author__ = "Tom Sapletta"
+__email__ = "info@softreck.dev"
+__description__ = "Dynamic Interactive Git deploY - Deploy Python applications from Git repositories"
