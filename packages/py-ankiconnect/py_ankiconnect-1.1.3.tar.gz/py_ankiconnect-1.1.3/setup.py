a6342ef7bb2ed7d51b26a2862adf3778844a991f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:06 +0000

    Add package py-ankiconnect/1.1.3/py_ankiconnect-1.1.3.tar.gz

---
+++ packages/py-ankiconnect/py_ankiconnect-1.1.3.tar.gz/py_ankiconnect-1.1.3/setup.py
@@ -0,0 +1,40 @@
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="py-ankiconnect",
+    version="1.1.3",
+    description="Minimal wrapper to simplify the usage of the awesome ankiconnect anki addon.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thiswillbeyourgithub/py_ankiconnect",
+    packages=find_packages(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    license="GPLv3",
+    keywords=["anki", "flashcards", "ankiconnect", "learning", "cli", "tool", "spaced", "repetition", "ebbinghaus", "addon"],
+    python_requires=">=3.9",
+
+    entry_points={
+        'console_scripts': [
+            'py_ankiconnect=py_ankiconnect.__init__:cli_launcher',
+        ],
+    },
+
+    install_requires=[
+        "fire >= 0.6.0",
+        "aiohttp",
+    ],
+
+    extras_require={
+        'rich': ['rich'],
+    },
+)
