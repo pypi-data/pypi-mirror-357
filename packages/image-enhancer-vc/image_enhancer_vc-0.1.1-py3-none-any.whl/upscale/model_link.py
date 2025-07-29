1334f1e70aa2b1f7680fa41a461b77830d5472aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:40 +0000

    Add package image-enhancer-vc/0.1.1/image_enhancer_vc-0.1.1-py3-none-any.whl

---
+++ packages/image-enhancer-vc/image_enhancer_vc-0.1.1-py3-none-any.whl/upscale/model_link.py
@@ -0,0 +1,4 @@
+MODEL_URLS = {
+    "realesrgan": "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth",
+    "gfpgan": "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth"
+}
