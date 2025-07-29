e2d75011ad86a952d7d91c1ed0ed54b360689120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:37 +0000

    Add package kos-Htools/0.1.5.post2/kos_htools-0.1.5.post2.tar.gz

---
+++ packages/kos-Htools/kos_htools-0.1.5.post2.tar.gz/kos_htools-0.1.5.post2/kos_Htools/__init__.py
@@ -0,0 +1,10 @@
+"""
+kos_Htools - Библиотека инструментов для работы с Telegram и Redis
+"""
+from .telethon_core.clients import MultiAccountManager
+from .telethon_core.settings import TelegramAPI
+from .redis_core.redisetup import RedisBase
+from .sql.sql_alchemy import BaseDAO, Update_date
+
+__version__ = '0.1.5.post2'
+__all__ = ["MultiAccountManager", "TelegramAPI", "RedisBase", "BaseDAO", "Update_date"]
