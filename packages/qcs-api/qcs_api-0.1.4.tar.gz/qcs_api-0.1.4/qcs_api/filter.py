52f606e41aec133f9cd4a3d9b701633872ac841f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:36 +0000

    Add package qcs-api/0.1.4/qcs_api-0.1.4.tar.gz

---
+++ packages/qcs-api/qcs_api-0.1.4.tar.gz/qcs_api-0.1.4/qcs_api/filter.py
@@ -0,0 +1,22 @@
+import sqlite3
+import os
+import pandas as pd
+
+FILENAME = "hbn_defects_structure.db"
+DATA_PATH = os.path.join(os.getcwd(), FILENAME)
+
+def get_filtered_data(properties: str, value):
+    if not os.path.exists(DATA_PATH):
+        raise FileNotFoundError("Database not found. Run get_all_data first.")
+
+    conn = sqlite3.connect(DATA_PATH)
+
+    query = f"""
+    SELECT "Defect", "Defect name", "{properties}"
+    FROM your_table_name
+    WHERE "{properties}" = ?
+    """
+    df = pd.read_sql_query(query, conn, params=(value,))
+    conn.close()
+
+    return df
