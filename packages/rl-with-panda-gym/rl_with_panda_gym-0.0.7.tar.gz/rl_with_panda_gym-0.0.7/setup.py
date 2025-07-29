a76813346246226fe9d65824b2ee3033846f226b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:11 +0000

    Add package rl-with-panda-gym/0.0.7/rl_with_panda_gym-0.0.7.tar.gz

---
+++ packages/rl-with-panda-gym/rl_with_panda_gym-0.0.7.tar.gz/rl_with_panda_gym-0.0.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rl_with_panda_gym',
+    packages=find_packages(),
+    version='0.0.7',
+    description='Testing multiple RL algorithms on panda-gym tasks',
+    author='Dasr Lab',
+    license='MIT',
+)
