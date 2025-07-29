d90f287286f5d9aa1bbe0ebbebf23b8e91a191fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:10 +0000

    Add package odoo-addon-sale-stock-available-to-promise-release-block/16.0.1.1.1/odoo_addon_sale_stock_available_to_promise_release_block-16.0.1.1.1-py3-none-any.whl

---
+++ packages/odoo-addon-sale-stock-available-to-promise-release-block/odoo_addon_sale_stock_available_to_promise_release_block-16.0.1.1.1-py3-none-any.whl/odoo/addons/sale_stock_available_to_promise_release_block/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2023 ACSONE SA/NV
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Available to Promise Release - Block from Sales",
+    "summary": """Block release of deliveries from sales orders.""",
+    "version": "16.0.1.1.1",
+    "license": "AGPL-3",
+    "author": "Camptcamp, ACSONE SA/NV, BCIM, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/wms",
+    "depends": [
+        # OCA/wms
+        "sale_stock_available_to_promise_release",
+        "stock_available_to_promise_release_block",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_order.xml",
+        "views/sale_order_line.xml",
+        "views/stock_move.xml",
+        "wizards/unblock_release.xml",
+    ],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+}
