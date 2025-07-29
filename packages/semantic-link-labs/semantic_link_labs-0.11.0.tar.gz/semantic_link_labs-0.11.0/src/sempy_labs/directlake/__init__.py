6b9b2d78719be74280fbf83a380d63b878508087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:20 +0000

    Add package semantic-link-labs/0.11.0/semantic_link_labs-0.11.0.tar.gz

---
+++ packages/semantic-link-labs/semantic_link_labs-0.11.0.tar.gz/semantic_link_labs-0.11.0/src/sempy_labs/directlake/__init__.py
@@ -0,0 +1,55 @@
+from sempy_labs.directlake._generate_shared_expression import generate_shared_expression
+from sempy_labs.directlake._directlake_schema_compare import direct_lake_schema_compare
+from sempy_labs.directlake._directlake_schema_sync import direct_lake_schema_sync
+from sempy_labs.directlake._dl_helper import (
+    check_fallback_reason,
+    generate_direct_lake_semantic_model,
+    get_direct_lake_source,
+)
+from sempy_labs.directlake._get_directlake_lakehouse import get_direct_lake_lakehouse
+from sempy_labs.directlake._get_shared_expression import get_shared_expression
+from sempy_labs.directlake._guardrails import (
+    get_direct_lake_guardrails,
+    get_sku_size,
+    get_directlake_guardrails_for_sku,
+)
+from sempy_labs.directlake._list_directlake_model_calc_tables import (
+    list_direct_lake_model_calc_tables,
+)
+from sempy_labs.directlake._show_unsupported_directlake_objects import (
+    show_unsupported_direct_lake_objects,
+)
+from sempy_labs.directlake._update_directlake_model_lakehouse_connection import (
+    update_direct_lake_model_lakehouse_connection,
+    update_direct_lake_model_connection,
+)
+from sempy_labs.directlake._update_directlake_partition_entity import (
+    update_direct_lake_partition_entity,
+    add_table_to_direct_lake_semantic_model,
+)
+from sempy_labs.directlake._warm_cache import (
+    warm_direct_lake_cache_isresident,
+    warm_direct_lake_cache_perspective,
+)
+
+__all__ = [
+    "generate_shared_expression",
+    "direct_lake_schema_compare",
+    "direct_lake_schema_sync",
+    "check_fallback_reason",
+    "get_direct_lake_lakehouse",
+    "get_shared_expression",
+    "get_direct_lake_guardrails",
+    "get_sku_size",
+    "get_directlake_guardrails_for_sku",
+    "list_direct_lake_model_calc_tables",
+    "show_unsupported_direct_lake_objects",
+    "update_direct_lake_model_lakehouse_connection",
+    "update_direct_lake_partition_entity",
+    "warm_direct_lake_cache_isresident",
+    "warm_direct_lake_cache_perspective",
+    "add_table_to_direct_lake_semantic_model",
+    "generate_direct_lake_semantic_model",
+    "get_direct_lake_source",
+    "update_direct_lake_model_connection",
+]
