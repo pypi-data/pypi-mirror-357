72076608bd816a1300801a8cc42ee50094339fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:25 +0000

    Add package load/1.0.12/load-1.0.12-py2.py3-none-any.whl

---
+++ packages/load/load-1.0.12-py2.py3-none-any.whl/load/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Configuration variables for Load
+"""
+
+# Cache modułów w pamięci
+_module_cache = {}
+
+# Konfiguracja auto-print
+AUTO_PRINT = True
+PRINT_LIMIT = 1000
+PRINT_TYPES = (str, int, float, list, dict, tuple)
