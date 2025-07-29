0702d59b42c0357a2d7e26de221fc196ef5befbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:21 +0000

    Add package torchwrench/0.7.0/torchwrench-0.7.0.tar.gz

---
+++ packages/torchwrench/torchwrench-0.7.0.tar.gz/torchwrench-0.7.0/src/torchwrench/serialization/pickle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from pathlib import Path
+from typing import Any, Union
+
+from pythonwrench.pickle import dump_pickle as _dump_pickle_base
+from pythonwrench.pickle import load_pickle  # noqa: F401
+
+from .common import as_builtin
+
+
+def dump_pickle(
+    obj: Any,
+    fpath: Union[str, Path, os.PathLike, None],
+    *,
+    overwrite: bool = True,
+    make_parents: bool = True,
+    to_builtins: bool = False,
+) -> bytes:
+    """Dump content to PICKLE format into a bytes and/or file."""
+    if to_builtins:
+        obj = as_builtin(obj)
+    return _dump_pickle_base(obj, fpath, overwrite=overwrite, make_parents=make_parents)
---
+++ packages/torchwrench/torchwrench-0.7.0.tar.gz/torchwrench-0.7.0/src/torchwrench/serialization/yaml.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from torchwrench.extras.yaml import (  # noqa: F401
+    IgnoreTagLoader,
+    SafeLoader,
+    dump_yaml,
+    load_yaml,
+)
