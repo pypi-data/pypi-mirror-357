eb3eb9663645038db2d089e76f732167d05bf121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:19 +0000

    Add package MoConfig/1.0.8/moconfig-1.0.8.tar.gz

---
+++ packages/MoConfig/moconfig-1.0.8.tar.gz/moconfig-1.0.8/MoConfig/config.py
@@ -0,0 +1,32 @@
+# @Time  : 2025/06/16
+# @Author: DriftCloud
+# @File  : config.py
+
+import json
+import os
+
+__all__ = ['Config']
+
+class Config:
+    _config_file_path = None
+    _configs = None
+
+    def __init__(self, config_file_path='config.json'):
+        self._config_file_path = config_file_path
+        if os.path.exists(config_file_path):
+            self._configs = json.load(open(self._config_file_path, 'r', encoding='utf-8'))
+        else:
+            self._configs = {}
+
+    def get(self, key, default=None):
+        return self._configs.get(key, default)
+
+    def set(self, key, value):
+        self._configs[key] = value
+
+    def save(self):
+        with open(self._config_file_path, 'w', encoding='utf-8') as f:
+            json.dump(self._configs, f, ensure_ascii=False, indent=4)
+
+    def __del__(self):
+        self.save()
