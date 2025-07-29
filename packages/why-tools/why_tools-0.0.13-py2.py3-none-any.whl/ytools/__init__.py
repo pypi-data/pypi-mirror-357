5ac5a875934580d49250eff9f6c20aee856a0fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:14 +0000

    Add package why-tools/0.0.13/why_tools-0.0.13-py2.py3-none-any.whl

---
+++ packages/why-tools/why_tools-0.0.13-py2.py3-none-any.whl/ytools/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : __init__.py.py
+@Date    : 2024/4/15 下午7:28
+@Author  : yintian
+@Desc    : 
+"""
+
+__version__ = '0.0.13'
+
+from ytools.log import logger
+
+if __name__ == '__main__':
+    pass
