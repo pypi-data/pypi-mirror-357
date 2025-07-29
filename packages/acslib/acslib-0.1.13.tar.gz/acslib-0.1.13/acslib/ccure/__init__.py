fb00244beb3551c96dc1d3015f75afa71f13eda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:06 +0000

    Add package acslib/0.1.13/acslib-0.1.13.tar.gz

---
+++ packages/acslib/acslib-0.1.13.tar.gz/acslib-0.1.13/acslib/ccure/__init__.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from acslib.base import ACSRequestResponse
+from acslib.base.connection import ACSRequestData, ACSRequestMethod
+from acslib.ccure.actions import CcureAction
+from acslib.ccure.base import CcureACS
+from acslib.ccure.connection import CcureConnection
+from acslib.ccure.crud import (
+    CcurePersonnel,
+    CcureClearance,
+    CcureCredential,
+    CcureClearanceItem,
+)
+from acslib.ccure.filters import ClearanceFilter, PersonnelFilter, CredentialFilter
+
+
+class CcureAPI:
+    def __init__(self, connection: Optional[CcureConnection] = None):
+        self.connection = connection or CcureConnection()
+        self.personnel = CcurePersonnel(self.connection)
+        self.clearance = CcureClearance(self.connection)
+        self.credential = CcureCredential(self.connection)
+        self.clearance_item = CcureClearanceItem(self.connection)
+        self.action = CcureAction(self.connection)
+        self.ccure_object = CcureACS(self.connection)
