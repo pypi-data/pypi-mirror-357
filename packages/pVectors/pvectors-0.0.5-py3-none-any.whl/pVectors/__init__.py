e3e0df48f90b0ab05b2af6d964f8ef895a43cd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:28 +0000

    Add package pVectors/0.0.5/pvectors-0.0.5-py3-none-any.whl

---
+++ packages/pVectors/pvectors-0.0.5-py3-none-any.whl/pVectors/__init__.py
@@ -0,0 +1,6 @@
+"""pVector is a python module designed to help handle vectors, 
+and calculations around them in a simple way. 
+It is written in python and at the moment only supports 2D vectors.
+This project bases vactors off of the c# implementation of them."""
+
+from pVectors.vector2 import *
