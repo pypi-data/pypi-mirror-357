c3b7851d0ae496d4b4185ffa121345326f53489b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:39 +0000

    Add package vacancycalculator/0.1.0/vacancycalculator-0.1.0.tar.gz

---
+++ packages/vacancycalculator/vacancycalculator-0.1.0.tar.gz/vacancycalculator-0.1.0/src/__init__.py
@@ -0,0 +1,8 @@
+from vfscript.cluster_processing import *
+from vfscript.runner import *
+from vfscript.utils import *
+from vfscript.surface_processor import *
+from vfscript.training import *
+import vfscript.main
+import vfscript.core
+
