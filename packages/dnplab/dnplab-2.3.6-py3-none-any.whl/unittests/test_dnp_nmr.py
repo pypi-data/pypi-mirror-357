6ab973ad1da7cb4b543c6df06dd070bd7e23b784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:31 +0000

    Add package dnplab/2.3.6/dnplab-2.3.6-py3-none-any.whl

---
+++ packages/dnplab/dnplab-2.3.6-py3-none-any.whl/unittests/test_dnp_nmr.py
@@ -0,0 +1,58 @@
+import unittest
+from numpy.testing import assert_array_equal
+import dnplab as dnp
+import numpy as np
+import logging
+
+# logging.basicConfig(filename='phase_debug.log', encoding='utf-8', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class dnpNMR_tester(unittest.TestCase):
+    def setUp(self):
+        pts = 1024
+        omega = 50 * dnp.pi
+        tau = 0.1
+        t2 = np.r_[0 : 1 : 1j * pts]
+        y = np.exp(1j * t2 * omega) * np.exp(-1 * t2 / tau)
+        self.data = dnp.DNPData(y, ["t2"], [t2])
+        self.data.dnplab_attrs["frequency"] = 400e6
+
+    def test_basic_nmr_processing(self):
+        data = dnp.remove_background(self.data)
+
+        data = dnp.left_shift(data)
+
+        data = dnp.apodize(data, lw=1)
+
+        data = dnp.fourier_transform(data)
+
+        data = dnp.autophase(data)
+
+    def test_calc_enhancement(self):
+        pass
+
+
+class dnpNMR_tester_sim(unittest.TestCase):
+    def setUp(self):
+        p1 = np.array([0, 0, 0, 1, 2, 3, 4, 3, 2, 1, 0, 0, 0])
+        p2 = np.array([0, 1, 2, 3, 4, 3, 2, 1, 0, 0, 0, 0, 0])
+        p3 = np.array([0, 0, 0, 0, 0, 1, 2, 3, 4, 3, 2, 1, 0])
+        self.data = dnp.DNPData(
+            np.array([p1, p2, p3]).T,
+            ["x", "t2"],
+            [np.arange(0, len(p1)), np.arange(0, 3)],
+        )
+
+    def test_align(self):
+        self.aligned_data = dnp.ndalign(self.data, dim="x")
+        assert_array_equal(
+            self.aligned_data.values,
+            np.array(
+                [
+                    [0, 0, 0, 1, 2, 3, 4, 3, 2, 1, 0, 0, 0],
+                    [0, 0, 0, 1, 2, 3, 4, 3, 2, 1, 0, 0, 0],
+                    [0, 0, 0, 1, 2, 3, 4, 3, 2, 1, 0, 0, 0],
+                ]
+            ).T,
+        )
