59c7e6761b961005fa1e2749e914a4a0cfac28e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:46 +0000

    Add package aws-app-framework-for-github-apps-on-aws-ssdk/0.0.1a1/aws_app_framework_for_github_apps_on_aws_ssdk-0.0.1a1.tar.gz

---
+++ packages/aws-app-framework-for-github-apps-on-aws-ssdk/aws_app_framework_for_github_apps_on_aws_ssdk-0.0.1a1.tar.gz/aws_app_framework_for_github_apps_on_aws_ssdk-0.0.1a1/setup.py
@@ -0,0 +1,14 @@
+import sys
+from setuptools import setup
+
+args = ' '.join(sys.argv).strip()
+if not any(args.endswith(suffix) for suffix in ['setup.py check -r -s', 'setup.py sdist']):
+    raise ImportError('This package is temporarily parked to protect against typosquating. Coming soon.')
+
+setup(
+    classifiers=['Development Status :: 7 - Inactive'],
+    description='This package is temporarily parked to protect against typosquating. Coming soon.',
+    long_description='This package is temporarily parked to protect against typosquating. Coming soon.',
+    name='aws-app-framework-for-github-apps-on-aws-ssdk',
+    version='0.0.1a1'
+)
