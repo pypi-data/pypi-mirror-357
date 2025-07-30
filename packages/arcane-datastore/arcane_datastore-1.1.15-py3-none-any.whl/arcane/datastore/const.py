a043d8f058f2e34c47ec39ff413d8f5198fb3a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:55 +0000

    Add package arcane-datastore/1.1.15/arcane_datastore-1.1.15-py3-none-any.whl

---
+++ packages/arcane-datastore/arcane_datastore-1.1.15-py3-none-any.whl/arcane/datastore/const.py
@@ -0,0 +1,34 @@
+## FLOW KINDS ##
+PRODUCT_FLOW_KIND = 'smart-feeds-product-flow'
+DATA_EXPORT_KIND = 'smart-feeds-data-exports'
+DATA_INGESTION_KIND = 'smart-feeds-data-ingestions'
+DATA_PROCESSING_KIND = 'smart-feeds-data-processings'
+PROTECTION_KIND = 'protection-file-size'
+ALERT_KIND = 'alert-file-change'
+PERFORMANCE_DASHBOARD_KIND = 'performance-dashboard'
+# To maintain compatibility with the old version of package
+PEFORMANCE_DASHBOARD_KIND = PERFORMANCE_DASHBOARD_KIND
+GENERAL_DASHBOARD_KIND = 'general-dashboard'
+OPTIMIZATION_KIND = 'smart-feeds-optimization'
+OPTIMIZATION_CONFIG_KIND = 'smart-feeds-optimization-config'
+OPTIMIZATION_DETAILS_KIND = 'smart-feeds-optimization-details'
+OPTIMIZATION_EXAMPLES_KIND = 'smart-feeds-optimization-examples'
+OPTIMIZATION_EXECUTION_INFO_KIND = 'smart-feeds-optimization-execution-info'
+OPTIMIZATION_MODEL_COUNTER_LOCK_KIND = 'smart-feeds-optimization-model-lock'
+OPTIMIZATION_WAITING_QUEUE_KIND = 'smart-feeds-optimization-waiting-queue'
+
+## CLIENTS SERVICES ##
+CLIENT_KIND = 'base-clients'
+CLIENT_DETAILS_KIND = 'clients-details'
+MCT_ACCOUNT_KIND = 'mct-accounts'
+BING_ACCOUNT_KIND = 'bing-accounts'
+CRITEO_ACCOUNT_KIND = 'criteo-accounts'
+FACEBOOK_ACCOUNT_KIND = 'facebook-accounts'
+FACEBOOK_CAMPAIGNS_KIND = 'facebook-campaigns'
+GOOGLE_ADS_ACCOUNT_KIND = 'google-ads-accounts'
+GOOGLE_ANALYTICS_V4_KIND = "google-analytics-v4-accounts"
+PINTEREST_ACCOUNT_KIND = 'pinterest-accounts'
+SCOPE_KIND = 'accounts-scopes'
+
+## UTILS SERVICES ##
+ORGANISATION_KIND = 'organisation'
