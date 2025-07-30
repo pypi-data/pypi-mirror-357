63b88bfa27810d8a996a4d6aed760c1d6a410076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:46 +0000

    Add package langgraph/0.4.9/langgraph-0.4.9.tar.gz

---
+++ packages/langgraph/langgraph-0.4.9.tar.gz/langgraph-0.4.9/tests/test_io.py
@@ -0,0 +1,18 @@
+from collections.abc import Iterator
+
+from langgraph.pregel.io import single
+
+
+def test_single() -> None:
+    closed = False
+
+    def myiter() -> Iterator[int]:
+        try:
+            yield 1
+            yield 2
+        finally:
+            nonlocal closed
+            closed = True
+
+    assert single(myiter()) == 1
+    assert closed
