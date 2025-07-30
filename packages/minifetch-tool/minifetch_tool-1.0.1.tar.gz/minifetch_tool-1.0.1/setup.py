e63759e2514cb1507d5c08e7caf13e929b774685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:34 +0000

    Add package minifetch-tool/1.0.1/minifetch_tool-1.0.1.tar.gz

---
+++ packages/minifetch-tool/minifetch_tool-1.0.1.tar.gz/minifetch_tool-1.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="minifetch-tool",
+    version="1.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['ascii/*.txt', 'custom.txt'],
+    },
+    install_requires=[
+        'colorama',
+        'psutil',
+        'distro; platform_system=="Linux"',
+    ],
+    entry_points={
+        'console_scripts': [
+            'minifetch=minifetch:entry_point',
+        ],
+    },
+    author="wdkq",
+    author_email="koc2b93o0@mozmail.com",
+    description="A mini system information fetch tool.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="system info fetch",
+    url="https://github.com/widikov/minifetch",
+)
