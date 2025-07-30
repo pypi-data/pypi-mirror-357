196ab1a5e55501a1fa89176a82daac726aa4ebf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:49 +0000

    Add package akari-bot-webrender/0.1.3/akari_bot_webrender-0.1.3-py3-none-any.whl

---
+++ packages/akari-bot-webrender/akari_bot_webrender-0.1.3-py3-none-any.whl/akari_bot_webrender/functions/options.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+
+from ..constants import base_width, base_height
+
+
+class LegacyScreenshotOptions(BaseModel):
+    content: str = None
+    width: int = base_width
+    height: int = base_height
+    mw: bool = False
+    tracing: bool = False
+    counttime: bool = True
+
+class PageScreenshotOptions(BaseModel):
+    url: str = None
+    css: str = None
+
+class ElementScreenshotOptions(BaseModel):
+    element: str | list = None
+    content: str = None
+    url: str = None
+    css: str = None
+    width: int = base_width
+    height: int = base_height
+    counttime: bool = True
+    tracing: bool = False
+
+
+class SectionScreenshotOptions(BaseModel):
+    section: str | list = None
+    content: str = None
+    url: str = None
+    css: str = None
+    width: int = base_width
+    height: int = base_height
+    counttime: bool = True
+    tracing: bool = False
+
+class SourceOptions(BaseModel):
+    url: str = None
