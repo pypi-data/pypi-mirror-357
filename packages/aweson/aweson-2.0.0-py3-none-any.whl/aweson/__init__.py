d6e54fb0dd8f04097fadb80849d4ca7824cdda88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:38 +0000

    Add package aweson/2.0.0/aweson-2.0.0-py3-none-any.whl

---
+++ packages/aweson/aweson-2.0.0-py3-none-any.whl/aweson/__init__.py
@@ -0,0 +1,6 @@
+"""
+Iterate or find items in a hierarchical data structure (e.g. loaded from JSON content) based
+on JSON Path-like Pythonic expressions.
+"""
+
+from .aweson import JP, find_all, find_next
