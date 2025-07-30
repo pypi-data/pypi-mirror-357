2a9adddf6d5a4bfde4839885edfdf5b808dbc56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:46 +0000

    Add package jupyterlab-pkm/0.1.12/jupyterlab_pkm-0.1.12-py3-none-any.whl

---
+++ packages/jupyterlab-pkm/jupyterlab_pkm-0.1.12-py3-none-any.whl/jupyterlab_pkm/__init__.py
@@ -0,0 +1,9 @@
+"""
+JupyterLab PKM Extension
+
+A JupyterLab extension for personal knowledge management with wikilinks and backlinks.
+"""
+
+from ._version import __version__
+
+__all__ = ["__version__"]
