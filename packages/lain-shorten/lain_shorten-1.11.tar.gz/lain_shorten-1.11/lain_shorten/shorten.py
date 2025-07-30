442e3f48c9b1a9e6a82c001699559c3bae6c6cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:40 +0000

    Add package lain-shorten/1.11/lain_shorten-1.11.tar.gz

---
+++ packages/lain-shorten/lain_shorten-1.11.tar.gz/lain_shorten-1.11/lain_shorten/shorten.py
@@ -0,0 +1,35 @@
+import requests
+import re
+
+
+def url_check(url):
+    pattern = (
+        r"^https?:\/\/(?:www\.)?"
+        r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\."
+        r"[a-zA-Z0-9()]{1,6}\b"
+        r"(?:[-a-zA-Z0-9()@:%_\+.~#?&/=]*)$"
+    )
+    return re.match(pattern, url) is not None
+
+
+def shorten_url(url):
+    # checking if the url is valid
+    is_valid_url = url_check(url)
+
+    # adding https:// to the start of the string if url is not valid
+    url = f"https://{url}" if not is_valid_url else url
+
+    # checking if the url is valid again
+    is_valid_url = url_check(url)
+
+    if not is_valid_url:
+        return "Not a valid url."
+
+    data = f"url={url}"
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    response = requests.post("https://s.lain.la", headers=headers, data=data)
+    return response.text
