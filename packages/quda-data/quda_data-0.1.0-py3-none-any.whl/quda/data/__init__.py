38ec637cfb5ee0df9f2d717d443e1c4132c08fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:13 +0000

    Add package quda-data/0.1.0/quda_data-0.1.0-py3-none-any.whl

---
+++ packages/quda-data/quda_data-0.1.0-py3-none-any.whl/quda/data/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------------
+Copyright (c) 2025 ZhangYundi
+Licensed under the MIT License. 
+Created on 2025/6/24 22:07
+Email: yundi.xxii@outlook.com
+Description: 
+---------------------------------------------
+"""
+
+from .loader import DataLoader
+from .updater import DataUpdater, submit, do
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "DataLoader",
+    "DataUpdater",
+    "submit",
+    "do"
+]
