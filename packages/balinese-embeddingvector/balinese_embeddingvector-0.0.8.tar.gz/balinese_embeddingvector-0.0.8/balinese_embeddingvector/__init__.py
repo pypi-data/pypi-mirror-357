ea20fd41c7fce94c0a6fd5cc3b49dcdff4727e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:03 +0000

    Add package balinese-embeddingvector/0.0.8/balinese_embeddingvector-0.0.8.tar.gz

---
+++ packages/balinese-embeddingvector/balinese_embeddingvector-0.0.8.tar.gz/balinese_embeddingvector-0.0.8/balinese_embeddingvector/__init__.py
@@ -0,0 +1,2 @@
+from .model import Glove
+from .pretrained import BaliGlove, BaliBERT, BaliFastText, BaliWord2Vec
---
+++ packages/balinese-embeddingvector/balinese_embeddingvector-0.0.8.tar.gz/balinese_embeddingvector-0.0.8/balinese_embeddingvector/pretrained/BaliBERT.py
@@ -0,0 +1,7 @@
+from .BasePretrained import BasePretrained
+from gensim.models import Word2Vec
+from huggingface_hub import hf_hub_download
+
+
+class BaliBERT(BasePretrained):
+    pass
