58df17600723e85a4f0ead73432b5b768d6d9930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:43 +0000

    Add package kaena/0.1.0/kaena-0.1.0-cp39-cp39-musllinux_1_2_x86_64.whl

---
+++ packages/kaena/kaena-0.1.0-cp39-cp39-musllinux_1_2_x86_64.whl/kaena/tape.py
@@ -0,0 +1,14 @@
+from . import ctape as _c
+
+read = _c.read
+append = _c.append
+replace = _c.replace
+create = _c.create
+open = _c.open
+save_index = _c.save_index
+close = _c.close
+read_batch = _c.read_batch
+append_batch = _c.append_batch
+KaenaBinaryFile = _c.KaenaBinaryFile
+read_raw = _c.read_raw
+reindex = _c.reindex
