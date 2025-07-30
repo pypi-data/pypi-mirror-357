337c6332938fa5a2478d9f51d5d148d1e858e3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:09 +0000

    Add package aipyapp/0.2.1b1/aipyapp-0.2.1b1.tar.gz

---
+++ packages/aipyapp/aipyapp-0.2.1b1.tar.gz/aipyapp-0.2.1b1/aipyapp/cli/command/cmd_info.py
@@ -0,0 +1,40 @@
+import sys
+
+from ... import T
+from .base import BaseCommand
+from .utils import print_table
+
+class InfoCommand(BaseCommand):
+    name = 'info'
+    description = 'Display system information'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--verbose', action='store_true', help='Show detailed info')
+        parser.add_argument('--format', choices=['json', 'text'], default='text', help='Output format')
+
+    def execute(self, args):
+        tm = self.manager.tm
+        settings = tm.settings
+
+        info = [
+            (T('Current configuration directory'), str(settings.config_dir)),
+            (T('Current working directory'), str(tm.workdir)),
+            (T('Current LLM'), repr(tm.client_manager.current)),
+            (T('Current role'), '-' if settings.get('system_prompt') else tm.tips_manager.current_tips.name),
+            ('Python', sys.executable),
+            (T('Python version'), sys.version),
+            (T('Python base prefix'), sys.base_prefix),
+        ]
+
+        print_table(info, title=T("System information"), headers=[T("Parameter"), T("Value")])
+
+class MCPCommand(BaseCommand):
+    name = 'mcp'
+    description = 'Display MCP information'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--list', action='store_true', help='List MCPs')
+
+    def execute(self, args):
+        if args.list:
+            self.cmd_list(args)
