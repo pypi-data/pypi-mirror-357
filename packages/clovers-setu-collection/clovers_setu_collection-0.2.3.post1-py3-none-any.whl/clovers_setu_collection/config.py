471b0a7504e7106c16bac81c07790894ba8f9894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:22 +0000

    Add package clovers-setu-collection/0.2.3.post1/clovers_setu_collection-0.2.3.post1-py3-none-any.whl

---
+++ packages/clovers-setu-collection/clovers_setu_collection-0.2.3.post1-py3-none-any.whl/clovers_setu_collection/config.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from pathlib import Path
+from typing import TypedDict, Any
+
+
+class HttpxConfig(TypedDict, total=False):
+    """httpx配置"""
+
+    LoliconAPI: dict[str, Any]
+    MirlKoiAPI: dict[str, Any]
+    AnosuAPI: dict[str, Any]
+
+
+class Config(BaseModel):
+    save_image: bool = False
+    """是否保存从api获取的图片"""
+    path: str = Path("./data/setu_collection").as_posix()
+    """主路径"""
+    private_setu_limit: bool = False
+    """私聊图片限制"""
+    private_setu_api: str = "Lolicon API"
+    """私聊使用的图片api"""
+    public_setu_limit: bool = True
+    """群聊图片限制（别关）"""
+    public_setu_api: str = "Anosu API"
+    """群聊使用的图片api"""
+    httpx_config: HttpxConfig = {
+        "LoliconAPI": {},
+        "MirlKoiAPI": {},
+        "AnosuAPI": {},
+    }
