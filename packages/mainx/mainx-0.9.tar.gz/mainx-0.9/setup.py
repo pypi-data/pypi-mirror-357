d18b310d4014d3b2584fbb1f6ad09c88bf097caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:08 +0000

    Add package mainx/0.9/mainx-0.9.tar.gz

---
+++ packages/mainx/mainx-0.9.tar.gz/mainx-0.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mainx",
+    version="0.9",  # 🔁 Bump version every time!
+    py_modules=["main1", "runner", "safe_runner"],
+    entry_points={
+        "console_scripts": [
+            "mainx = runner:main",
+            "mainx2 = safe_runner:main",
+        ],
+    },
+    author="Your Name",
+    description="Educational bug simulation tool with network, encryption, and system info features",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "discord.py>=2.0.0",
+        "pycryptodome",
+        "cryptography",
+        'pywin32; platform_system=="Windows"',
+    ],
+)
