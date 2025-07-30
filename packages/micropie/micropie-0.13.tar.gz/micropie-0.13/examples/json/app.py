5983fbbeb74e11f5a943ecd384e62e8da6cc9810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:35 +0000

    Add package micropie/0.13/micropie-0.13.tar.gz

---
+++ packages/micropie/micropie-0.13.tar.gz/micropie-0.13/examples/json/app.py
@@ -0,0 +1,11 @@
+from MicroPie import App
+
+
+class Root(App):
+
+    async def new_post(self):
+        data = self.request.get_json
+        return {'status': 'ok', 'data': data}
+
+
+app = Root()
