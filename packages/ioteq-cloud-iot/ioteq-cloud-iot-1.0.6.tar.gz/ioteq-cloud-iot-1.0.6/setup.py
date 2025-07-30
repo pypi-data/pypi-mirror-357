262ccbaa2521ee3eae9e13bf4a53ed7b228d34fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:05 +0000

    Add package ioteq-cloud-iot/1.0.6/ioteq-cloud-iot-1.0.6.tar.gz

---
+++ packages/ioteq-cloud-iot/ioteq-cloud-iot-1.0.6.tar.gz/ioteq-cloud-iot-1.0.6/setup.py
@@ -0,0 +1,73 @@
+# Copyright 2025 IO-TEQ, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import io
+import os
+
+import setuptools
+
+name = "ioteq-cloud-iot"
+description = "Cloud IoT API client library"
+version = "1.0.6"
+release_status = "Development Status :: 5 - Production/Stable"
+dependencies = ["httpx", "proto-plus"]
+
+package_root = os.path.abspath(os.path.dirname(__file__))
+
+readme_filename = os.path.join(package_root, "README.md")
+with io.open(readme_filename, encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+packages = [
+    package
+    for package in setuptools.PEP420PackageFinder.find()
+    if package.startswith("ioteq")
+]
+
+setuptools.setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Angel Oberto",
+    author_email="angel.oberto@io-teq.com",
+    license="Apache 2.0",
+    url="https://github.com/aeoberto/python-iot",
+    project_urls={
+        "Bug Reports": "https://github.com/aeoberto/python-iot/issues",
+        "Source": "https://github.com/aeoberto/python-iot",
+        "Documentation": "https://github.com/aeoberto/python-iot#readme",
+    },
+    classifiers=[
+        release_status,
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+    ],
+    platforms="Posix; MacOS X; Windows",
+    packages=packages,
+    install_requires=dependencies,
+    python_requires=">=3.8",
+    include_package_data=True,
+    zip_safe=False,
+)
