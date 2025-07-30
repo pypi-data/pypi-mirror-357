a6a6591c3b5ba8d5b790d72d212a717350af7fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:34 +0000

    Add package unveil-python/0.0.2/unveil_python-0.0.2.tar.gz

---
+++ packages/unveil-python/unveil_python-0.0.2.tar.gz/unveil_python-0.0.2/README.md
@@ -0,0 +1,4 @@
+# UNVEIL
+A simple tractography viewer implemented in python.
+
+![image](https://github.com/user-attachments/assets/4fbefa7a-3fc2-446a-8835-2844266b14fa)
---
+++ packages/unveil-python/unveil_python-0.0.2.tar.gz/unveil_python-0.0.2/src/unveil/__init__.py
@@ -0,0 +1,5 @@
+"""
+This package contains an UNVEIL, a lightweight tractography viewer implemented in python."""
+
+__version__ = "0.0.2"
+__author__ = 'Nicolas Delinte'
