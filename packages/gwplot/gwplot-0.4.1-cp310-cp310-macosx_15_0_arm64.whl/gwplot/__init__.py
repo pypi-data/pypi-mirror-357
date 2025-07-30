89982505750964d5b5908f098bcfb151a988cf0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:13 +0000

    Add package gwplot/0.4.1/gwplot-0.4.1-cp310-cp310-macosx_15_0_arm64.whl

---
+++ packages/gwplot/gwplot-0.4.1-cp310-cp310-macosx_15_0_arm64.whl/gwplot/__init__.py
@@ -0,0 +1,11 @@
+
+from gwplot.glfw_interface import GLFW
+
+from gwplot.interface import (
+    Gw,
+    GwPalette
+)
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("gwplot")
