cacf15a5966ac6a9024886c270c2ab6c353640e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:31 +0000

    Add package winaccent/2.1.0/winaccent-2.1.0.tar.gz

---
+++ packages/winaccent/winaccent-2.1.0.tar.gz/winaccent-2.1.0/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+
+description = open("README.md", "r", encoding = "utf8").read()
+
+setup(
+    name = "winaccent",
+    version = "2.1.0",
+    license = "MIT",
+    author = "Valer100",
+    maintainer = "Valer100",
+    description = "A simple and lightweight Python module for easily retrieving Windows' accent color, including shades, specific window chrome colors such as active/inactive titlebar and window borders and theme. Supports Windows 8.x, 10 and 11 and doesn't require additional dependencies.",
+    url = "https://github.com/Valer100/winaccent",
+    python_requires = ">=3.6",
+    project_urls = {
+        "Source": "https://github.com/Valer100/winaccent",
+        "Documentation": "https://valer100.github.io/winaccent",
+        "Changelog": "https://valer100.github.io/winaccent/about/changelog",
+        "Issues": "https://github.com/Valer100/winaccent/issues",
+    },
+    packages = find_packages(),
+    long_description = description,
+    long_description_content_type = "text/markdown",
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Microsoft :: Windows :: Windows 8",
+        "Operating System :: Microsoft :: Windows :: Windows 8.1",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Microsoft :: Windows :: Windows 11",
+    ],
+    keywords = [
+        "accent",
+        "accent-light",
+        "accent-dark",
+        "accent-color"
+        "accent-palette",
+        "dark-mode",
+        "light-mode",
+        "system-theme",
+        "color",
+        "theme",
+        "modern",
+        "gui",
+        "win32",
+        "fluent",
+        "sun-valley",
+        "windows-11",
+        "windows-10",
+        "windows-8",
+        "winui",
+        "winaccent"
+    ],
+)
---
+++ packages/winaccent/winaccent-2.1.0.tar.gz/winaccent-2.1.0/winaccent/_utils.py
@@ -0,0 +1,77 @@
+'''
+A winaccent submodule containing functions used internaly.
+'''
+
+import winreg, colorsys
+from typing import Any, Literal
+
+def get_registry_value(hkey: int, key_path: str, value_name: str, expected_regtype: int = None) -> Any :
+    key = winreg.OpenKey(hkey, key_path, 0, winreg.KEY_READ)
+    value, regtype = winreg.QueryValueEx(key, value_name)
+    winreg.CloseKey(key)
+    
+    if expected_regtype == regtype or expected_regtype == None: return value
+    else: raise ValueError(f"Invalid registry value: expected {expected_regtype} but got {regtype}")
+
+def get_color_from_registry_rgb(hkey: int, key_path: str, value_name: str, from_: Literal["abgr", "argb"]) -> str:
+    list = f"{get_registry_value(hkey, key_path, value_name):08X}"
+    color = "#"
+
+    if from_ == "abgr": color = "#" + list[6] + list[7] + list[4] + list[5] + list[2] + list[3]
+    elif from_ == "argb": color = "#" + list[2] + list[3] + list[4] + list[5] + list[6] + list[7]
+
+    if list[0] + list[1] + color.replace("#", "") == "00000000": 
+        return "0"
+    else:
+        # Check if the color is valid
+        if len(color) != 7: return "#FF00FF"
+
+        try: color_test = int(color[1] + color[2], base = 16) + int(color[3] + color[4], base = 16) + int(color[5] + color[6], base = 16)
+        except: return "#FF00FF"
+        
+        return color
+
+def blend_colors(color_1: str, color_2: str, intensity: int) -> str:
+    intensity = intensity * 255 / 100
+
+    color_1_red = int(color_1[1] + color_1[2], base = 16)
+    color_1_green = int(color_1[3] + color_1[4], base = 16)
+    color_1_blue = int(color_1[5] + color_1[6], base = 16)
+
+    color_2_red = int(color_2[1] + color_2[2], base = 16)
+    color_2_green = int(color_2[3] + color_2[4], base = 16)
+    color_2_blue = int(color_2[5] + color_2[6], base = 16)
+
+    red = (((color_1_red * intensity)) + (color_2_red * (255 - intensity))) / 255
+    green = (((color_1_green * intensity)) + (color_2_green * (255 - intensity))) / 255
+    blue = (((color_1_blue * intensity)) + (color_2_blue * (255 - intensity))) / 255
+
+    return f"#{round(red):02X}{round(green):02X}{round(blue):02X}"
+
+def increase_saturation(color: str, factor: float) -> str:
+    red = int(color[1] + color[2], base = 16) / 255
+    green = int(color[3] + color[4], base = 16) / 255
+    blue = int(color[5] + color[6], base = 16) / 255
+    
+    hue, lightness, saturation = colorsys.rgb_to_hls(red, green, blue)
+    saturation = min(1, saturation * factor)
+    red, green, blue = colorsys.hls_to_rgb(hue, lightness, saturation)
+    
+    return f"#{round(red * 255):02X}{round(green * 255):02X}{round(blue * 255):02X}"
+
+def white_text_on_color(color: str) -> bool:
+    red = int(color[1] + color[2], base = 16)
+    green = int(color[3] + color[4], base = 16)
+    blue = int(color[5] + color[6], base = 16)
+
+    return (5 * green + 2 * red + blue) <= 8 * 128
+
+def generate_color_palette(color: str) -> list:
+    return [
+        increase_saturation(blend_colors("#FFFFFF", color, 75), 2),
+        increase_saturation(blend_colors("#FFFFFF", color, 50), 2),
+        increase_saturation(blend_colors("#FFFFFF", color, 25), 2),
+        increase_saturation(blend_colors("#000000", color, 25), 2),
+        increase_saturation(blend_colors("#000000", color, 50), 2),
+        increase_saturation(blend_colors("#000000", color, 75), 2),
+    ]
