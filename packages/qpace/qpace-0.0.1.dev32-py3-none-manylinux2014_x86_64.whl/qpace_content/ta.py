e0881ccae83541183ba80e7c35b883177bb5eafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:35 +0000

    Add package qpace/0.0.1.dev32/qpace-0.0.1.dev32-py3-none-manylinux2014_x86_64.whl

---
+++ packages/qpace/qpace-0.0.1.dev32-py3-none-manylinux2014_x86_64.whl/qpace_content/ta.py
@@ -0,0 +1,66 @@
+
+  
+from dataclasses import dataclass
+from typing import Optional, Union, Literal, TypedDict, Any, Tuple, List
+from qpace import Ctx, Backtest
+from qpace_content import _lib
+  
+  
+def accdist(ctx: Ctx) -> List[float]:
+    """
+`accdist`
+    """
+
+    return _lib.Incr_fn_accdist_a4c94e(ctx).collect()
+
+class AccdistLocals:
+    def __init__(self, inner):
+        self.__inner = inner
+
+    
+
+      
+
+class Accdist:
+    """
+`accdist`
+    """
+    
+    def __init__(self, ctx: Ctx):
+        self.ctx = ctx
+        self.inner = _lib.Incr_fn_accdist_a4c94e(ctx)
+        self.locals = AccdistLocals(self.inner)
+
+    def next(self, ) -> Optional[float]:
+        return self.inner.next()
+    
+
+def rsi(ctx: Ctx, src: List[float], length: Optional[int]) -> List[float]:
+    """
+`rsi`
+    """
+
+    return _lib.Incr_fn_rsi_4b05dd(ctx).collect(_9682_src=src, _9683_length=length)
+
+class RsiLocals:
+    def __init__(self, inner):
+        self.__inner = inner
+
+    
+
+      
+
+class Rsi:
+    """
+`rsi`
+    """
+    
+    def __init__(self, ctx: Ctx):
+        self.ctx = ctx
+        self.inner = _lib.Incr_fn_rsi_4b05dd(ctx)
+        self.locals = RsiLocals(self.inner)
+
+    def next(self, src: float, length: Optional[int]) -> Optional[float]:
+        return self.inner.next(_9682_src=src, _9683_length=length)
+    
+          
