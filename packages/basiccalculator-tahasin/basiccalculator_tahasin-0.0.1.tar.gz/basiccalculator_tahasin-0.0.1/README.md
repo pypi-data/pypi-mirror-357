8f5446a962be83bfafafd22f9bdf20b7c4f160b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:29 +0000

    Add package basiccalculator-tahasin/0.0.1/basiccalculator_tahasin-0.0.1.tar.gz

---
+++ packages/basiccalculator-tahasin/basiccalculator_tahasin-0.0.1.tar.gz/basiccalculator_tahasin-0.0.1/README.md
@@ -0,0 +1,1 @@
+## This an test on pypi package building 
---
+++ packages/basiccalculator-tahasin/basiccalculator_tahasin-0.0.1.tar.gz/basiccalculator_tahasin-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='basiccalculator-tahasin',
+  version='0.0.1',
+  description='A very basic calculator',
+  packages=find_packages(),
+  install_requires=[] 
+)
