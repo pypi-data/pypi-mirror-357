3a3c9e58b8239237812d1388a17ad17b5ce2cc85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:33 +0000

    Add package icbc-captcha-recognizer/0.1.0/icbc_captcha_recognizer-0.1.0.tar.gz

---
+++ packages/icbc-captcha-recognizer/icbc_captcha_recognizer-0.1.0.tar.gz/icbc_captcha_recognizer-0.1.0/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="icbc-captcha-recognizer",
+    version="0.1.0",
+    description="ICBC CAPTCHA recognizer with pretrained model",
+    author="lk",
+    author_email="",
+    packages=find_packages(),
+    package_data={
+        "captcha_recognizer": [
+            "config/*.json",
+            "config/*.txt",
+            "output/custom_captcha_model/model.pth",
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        "torch",
+        "torchvision",
+        "opencv-python",
+        "numpy"
+    ],
+    entry_points={
+        "console_scripts": [
+            "captcha-predict=captcha_recognizer.cli:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.10",
+)
