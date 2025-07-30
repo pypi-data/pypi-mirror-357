ebc2e2b347620d1ef2d3d55b8a3441e4cdc580ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:45 +0000

    Add package talentro-definitions/0.7.1/talentro_definitions-0.7.1-py3-none-any.whl

---
+++ packages/talentro-definitions/talentro_definitions-0.7.1-py3-none-any.whl/talentro/iam/types.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Organization:
+    id: str
+    name: str
+    displayName: str
+    attributes: dict
+    roles: List[str]
+    url: str
