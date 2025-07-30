de98254295ff942da4f2dd670477d67d5aa003d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:38 +0000

    Add package thehive4py/2.0.0b12/thehive4py-2.0.0b12-py3-none-any.whl

---
+++ packages/thehive4py/thehive4py-2.0.0b12-py3-none-any.whl/thehive4py/types/custom_field.py
@@ -0,0 +1,59 @@
+from typing import Any, TypedDict
+
+
+class InputCustomFieldValueRequired(TypedDict):
+    name: str
+
+
+class InputCustomFieldValue(InputCustomFieldValueRequired, total=False):
+    value: Any
+    order: int
+
+
+class OutputCustomFieldValue(TypedDict):
+    _id: str
+    name: str
+    type: str
+    value: Any
+    order: int
+
+
+class InputCustomFieldRequired(TypedDict):
+    name: str
+    group: str
+    description: str
+    type: str
+
+
+class InputCustomField(InputCustomFieldRequired, total=False):
+    displayName: str
+    mandatory: bool
+    options: list
+
+
+class OutputCustomFieldRequired(TypedDict):
+    _id: str
+    _type: str
+    _createdBy: str
+    _createdAt: int
+    name: str
+    displayName: str
+    group: str
+    description: str
+    type: str
+    mandatory: bool
+
+
+class OutputCustomField(OutputCustomFieldRequired, total=False):
+    _updatedBy: str
+    _updatedAt: int
+    options: list
+
+
+class InputUpdateCustomField(TypedDict, total=False):
+    displayName: str
+    group: str
+    description: str
+    type: str
+    options: list
+    mandatory: bool
