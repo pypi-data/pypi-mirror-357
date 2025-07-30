b696179e43ac57afcac22ffbdb04040f0244165e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:36 +0000

    Add package camb-sdk/1.0.3/camb_sdk-1.0.3.tar.gz

---
+++ packages/camb-sdk/camb_sdk-1.0.3.tar.gz/camb_sdk-1.0.3/cambai/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from cambai.api.apis_api import CambAI
+from cambai.api.audio_separation_api import AudioSeparationApi
+from cambai.api.dictionaries_api import DictionariesApi
+from cambai.api.dub_api import DubApi
+from cambai.api.stories_api import StoriesApi
+from cambai.api.text_to_audio_api import TextToAudioApi
+from cambai.api.text_to_speech_api import TextToSpeechApi
+from cambai.api.text_to_voice_api import TextToVoiceApi
+
