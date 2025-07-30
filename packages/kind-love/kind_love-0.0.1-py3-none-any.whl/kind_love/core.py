52755b30c1ec317d2ac114f4eb08c0934bd4993c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:42 +0000

    Add package kind-love/0.0.1/kind_love-0.0.1-py3-none-any.whl

---
+++ packages/kind-love/kind_love-0.0.1-py3-none-any.whl/kind_love/core.py
@@ -0,0 +1,9 @@
+
+
+def say_love(name: str=None) -> None:
+    if name is None:
+        print("Who you should love most is yourself!!!")
+    elif name == "ybz":
+        print("Thank you for loving me! You are doing a great job!!!")
+    else:
+        print(f"I gusss {name} is a kind people!!!")
