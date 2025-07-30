0a87dcb3b792e8133472d7221806e9eec1490591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:39 +0000

    Add package looptime/0.3/looptime-0.3-py3-none-any.whl

---
+++ packages/looptime/looptime-0.3-py3-none-any.whl/looptime/__init__.py
@@ -0,0 +1,17 @@
+from .chronometers import Chronometer
+from .loops import IdleTimeoutError, LoopTimeEventLoop, LoopTimeoutError, TimeWarning
+from .patchers import make_event_loop_class, new_event_loop, patch_event_loop, reset_caches
+from .timeproxies import LoopTimeProxy
+
+__all__ = [
+    'Chronometer',
+    'TimeWarning',
+    'LoopTimeProxy',
+    'IdleTimeoutError',
+    'LoopTimeoutError',
+    'LoopTimeEventLoop',
+    'reset_caches',
+    'new_event_loop',
+    'patch_event_loop',
+    'make_event_loop_class',
+]
