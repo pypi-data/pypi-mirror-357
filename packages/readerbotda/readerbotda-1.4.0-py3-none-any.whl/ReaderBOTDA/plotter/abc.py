96e38788d581fbab630bf96618565a7b22f6d381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:25 +0000

    Add package readerbotda/1.4.0/readerbotda-1.4.0-py3-none-any.whl

---
+++ packages/readerbotda/readerbotda-1.4.0-py3-none-any.whl/ReaderBOTDA/plotter/abc.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+class Plotter(ABC):
+
+    @abstractmethod
+    def single_plot(self,position, profile, title: str = None):
+        pass
+
+    @abstractmethod
+    def multiple_plot(self,position, profiles, timestamps, title: str = ''):
+        pass
+
+    @abstractmethod
+    def statistics(self,position,mean,std,title:str = ''):
+        pass
+
+    @abstractmethod
+    def raw2d_plot(self,position, frequency, BGS, title: str = None):
+        pass
+    
+    @abstractmethod
+    def rawBGS_plot(self,frequency, BGS, index: int = None, title: str = None):
+        pass
+
+    @abstractmethod
+    def raw3d_plot(self,position, frequency, BGS, title: str = None):
+        pass
+
+    @abstractmethod
+    def max_plot(self,position,max,title: str = None):
+        pass
+
+    @abstractmethod
+    def max_stat_plot(self,max_mean,max_std, title:str = ''):
+        pass
