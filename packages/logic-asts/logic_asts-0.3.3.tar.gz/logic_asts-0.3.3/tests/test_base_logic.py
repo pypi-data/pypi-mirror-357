0981aa945c4be1f59566b5dcf481c0acaee59aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:39 +0000

    Add package logic-asts/0.3.3/logic_asts-0.3.3.tar.gz

---
+++ packages/logic-asts/logic_asts-0.3.3.tar.gz/logic_asts-0.3.3/tests/test_base_logic.py
@@ -0,0 +1,27 @@
+import operator
+from functools import reduce
+
+import pytest
+
+import logic_asts
+from logic_asts.base import Equiv, Expr, Variable
+
+
+def test_base_logic() -> None:
+    expr = "(x1 <-> x2) | x3"
+    expected = Equiv(Variable("x1"), Variable("x2")) | Variable("x3")
+    parsed = logic_asts.parse_expr(expr, syntax="base")
+    assert parsed == expected, (parsed, expected)
+    assert parsed.horizon() == expected.horizon() == 0
+
+
+@pytest.mark.parametrize(
+    "n",
+    [3, 5, 10, 20, 30, 40, 80, 100],
+)
+def test_parse_large_expr(n: int) -> None:
+    expr = " & ".join((f"(x{i} <-> y{i})" for i in range(n)))
+    expected: Expr = reduce(operator.__and__, (Equiv(Variable(f"x{i}"), Variable(f"y{i}")) for i in range(n)))
+    parsed = logic_asts.parse_expr(expr, syntax="base")
+    assert parsed == expected
+    assert parsed.horizon() == expected.horizon() == 0
