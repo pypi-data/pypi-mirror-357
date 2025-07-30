1f98769c0d6ed4d9eb32df82e1a4be4638715996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:49 +0000

    Add package cyrecon/1.0.3/cyrecon-1.0.3.tar.gz

---
+++ packages/cyrecon/cyrecon-1.0.3.tar.gz/cyrecon-1.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cyrecon",
+    version="1.0.3",
+    author="Nikhil Bhor",
+    author_email="nikhilbhor201@gmail.com",
+    description="Automated AI-powered recon toolkit for subdomains, ports, directories and CVE detection",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrcoder420/cyrecon",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "tqdm",
+        "selenium",
+        "fpdf",
+        "colorama"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'cyrecon=main:main_menu',  # <-- THIS enables the CLI command "cyrecon"
+        ],
+    },
+)
