92fd86aebd68d2221a3d16469f0ebf3d43067932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:30 +0000

    Add package Flask-PluginKit/3.10.1/flask_pluginkit-3.10.1-py3-none-any.whl

---
+++ packages/Flask-PluginKit/flask_pluginkit-3.10.1-py3-none-any.whl/flask_pluginkit/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+flask_pluginkit
+~~~~~~~~~~~~~~~
+
+Load and run plugins for your Flask application
+
+:copyright: (c) 2019 by staugur.
+:license: BSD 3-Clause, see LICENSE for more details.
+"""
+
+from .pluginkit import PluginManager, push_dcp
+from .utils import (
+    LocalStorage,
+    RedisStorage,
+    JsonResponse,
+    ExpiredLocalStorage,
+)
+from .version import __version__
+from ._installer import PluginInstaller
+from ._web import blueprint
+
+
+__author__ = "Hiroshi.tao <me@tcw.im>"
+
+__all__ = [
+    "PluginManager",
+    "LocalStorage",
+    "RedisStorage",
+    "JsonResponse",
+    "PluginInstaller",
+    "push_dcp",
+    "blueprint",
+    "ExpiredLocalStorage",
+]
