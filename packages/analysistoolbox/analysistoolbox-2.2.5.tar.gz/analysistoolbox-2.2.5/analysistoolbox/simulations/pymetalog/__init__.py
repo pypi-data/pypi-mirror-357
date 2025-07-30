cc1d6a91f015a8b29931fc301ca7ec6fdf732afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:09 +0000

    Add package analysistoolbox/2.2.5/analysistoolbox-2.2.5.tar.gz

---
+++ packages/analysistoolbox/analysistoolbox-2.2.5.tar.gz/analysistoolbox-2.2.5/analysistoolbox/simulations/pymetalog/__init__.py
@@ -0,0 +1,15 @@
+from .metalog import metalog
+from .class_method import rmetalog, plot, qmetalog, pmetalog, dmetalog, summary, update
+
+# Create a pymetalog module that can be imported
+class pymetalog:
+    def __init__(self):
+        self.metalog = metalog()
+        self.rmetalog = rmetalog()
+        self.plot = plot()
+        self.qmetalog = qmetalog()
+        self.pmetalog = pmetalog()
+        self.dmetalog = dmetalog()
+        self.summary = summary()
+        self.update = update()
+
