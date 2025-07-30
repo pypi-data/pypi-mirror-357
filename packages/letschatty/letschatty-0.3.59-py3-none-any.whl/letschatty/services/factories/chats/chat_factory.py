adeb2f386582b4fc5f5c045fafb87d3cb10c41c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:50 +0000

    Add package letschatty/0.3.59/letschatty-0.3.59-py3-none-any.whl

---
+++ packages/letschatty/letschatty-0.3.59-py3-none-any.whl/letschatty/services/factories/chats/chat_factory.py
@@ -0,0 +1,24 @@
+from letschatty.models.chat.chat import Chat
+from letschatty.models.chat.client import Client
+from letschatty.models.company.empresa import EmpresaModel
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from ..messages.chatty_message_factory import from_message_json
+
+class ChatFactory:
+    @staticmethod
+    def from_json(chat_json: dict) -> Chat:
+        chat_json["messages"] = [from_message_json(message) for message in chat_json["messages"]]
+        if not chat_json.get("chatty_ai_agent"):
+            chat_json["chatty_ai_agent"] = None
+        return Chat(**chat_json)
+
+    @staticmethod
+    def from_client(client: Client, empresa: EmpresaModel, channel_id: str) -> Chat:
+        return Chat(
+            client=client,
+            channel_id=channel_id,
+            company_id=empresa.id,
+            created_at=datetime.now(ZoneInfo("UTC")),
+            updated_at=datetime.now(ZoneInfo("UTC"))
+        )
