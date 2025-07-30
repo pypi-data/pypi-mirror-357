29c91c0158ba0e66aa4715a602bbc5b216966807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:09 +0000

    Add package hitesh-fastapi-starter/0.1.0/hitesh_fastapi_starter-0.1.0.tar.gz

---
+++ packages/hitesh-fastapi-starter/hitesh_fastapi_starter-0.1.0.tar.gz/hitesh_fastapi_starter-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hitesh-fastapi-starter",
+    version="0.1.0",
+    author="Hitesh Ladumor",
+    author_email="your_email@example.com",
+    description="Generate a FastAPI starter project structure",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_github/fastapi-starter",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+    ],
+    entry_points={
+        "console_scripts": [
+            "fastapi-starter=fastapi_starter.generator:create_fastapi_project_cli"
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
