201f035bd33cd8fd3fa8a87f80deb82f855593c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:26 +0000

    Add package svc-user-auth-zxw/3.0.2/svc_user_auth_zxw-3.0.2-py3-none-any.whl

---
+++ packages/svc-user-auth-zxw/svc_user_auth_zxw-3.0.2-py3-none-any.whl/svc_user_auth_zxw/tools/error_code.py
@@ -0,0 +1,17 @@
+"""
+# File       : error_code.py
+# Time       ：2025/6/24 09:41
+# Author     ：xuewei zhang
+# Email      ：shuiheyangguang@gmail.com
+# version    ：python 3.12
+# Description：
+"""
+
+
+class ErrorCode:
+    """错误码"""
+    存在关联数据 = 19001
+    系统错误 = 19002
+    系统异常 = 19003
+    参数错误 = 19004
+    权限不足 = 19005
