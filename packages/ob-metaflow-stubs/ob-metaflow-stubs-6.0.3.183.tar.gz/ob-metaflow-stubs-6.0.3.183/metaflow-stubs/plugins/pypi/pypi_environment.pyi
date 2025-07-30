edbc5a5bb6ca230dd4b7758dd308733cd986c3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:49 +0000

    Add package ob-metaflow-stubs/6.0.3.183/ob-metaflow-stubs-6.0.3.183.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.183.tar.gz/ob-metaflow-stubs-6.0.3.183/metaflow-stubs/plugins/pypi/pypi_environment.pyi
@@ -0,0 +1,18 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-25T00:26:31.551464                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.plugins.pypi.conda_environment
+
+from .conda_environment import CondaEnvironment as CondaEnvironment
+
+class PyPIEnvironment(metaflow.plugins.pypi.conda_environment.CondaEnvironment, metaclass=type):
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.183.tar.gz/ob-metaflow-stubs-6.0.3.183/metaflow-stubs/plugins/secrets/inline_secrets_provider.pyi
@@ -0,0 +1,25 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-25T00:26:31.552623                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import abc
+import typing
+if typing.TYPE_CHECKING:
+    import abc
+    import metaflow.plugins.secrets
+
+from . import SecretsProvider as SecretsProvider
+
+class InlineSecretsProvider(metaflow.plugins.secrets.SecretsProvider, metaclass=abc.ABCMeta):
+    def get_secret_as_dict(self, secret_id, options = {}, role = None):
+        """
+        Intended to be used for testing purposes only.
+        """
+        ...
+    ...
+
