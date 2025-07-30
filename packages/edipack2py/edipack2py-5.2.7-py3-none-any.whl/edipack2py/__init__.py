f62cb54a66ae48e67264a0e59a072fab87e62432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:23 +0000

    Add package edipack2py/5.2.7/edipack2py-5.2.7-py3-none-any.whl

---
+++ packages/edipack2py/edipack2py-5.2.7-py3-none-any.whl/edipack2py/__init__.py
@@ -0,0 +1,7 @@
+from edipack2py import class_creator
+from ctypes import *
+import numpy as np
+
+
+# this ed class contains all the global variables and methods
+global_env = class_creator.global_env
