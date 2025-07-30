7a30116d49e795106b99087d730c8f4031e7c91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:13 +0000

    Add package deluge-web-client/1.0.6/deluge_web_client-1.0.6-py3-none-any.whl

---
+++ packages/deluge-web-client/deluge_web_client-1.0.6-py3-none-any.whl/deluge_web_client/__init__.py
@@ -0,0 +1,6 @@
+from deluge_web_client.client import DelugeWebClient
+from deluge_web_client.exceptions import DelugeWebClientError
+from deluge_web_client.response import Response
+from deluge_web_client.state import TorrentState
+
+__all__ = ["DelugeWebClient", "DelugeWebClientError", "Response", "TorrentState"]
