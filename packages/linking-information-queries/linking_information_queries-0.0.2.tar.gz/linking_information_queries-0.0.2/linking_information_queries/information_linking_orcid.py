63af6e7e0e409e2f6e5e5cfd8701838772cab371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:26 +0000

    Add package linking-information-queries/0.0.2/linking_information_queries-0.0.2.tar.gz

---
+++ packages/linking-information-queries/linking_information_queries-0.0.2.tar.gz/linking_information_queries-0.0.2/linking_information_queries/information_linking_orcid.py
@@ -0,0 +1,11 @@
+from .orcid import retrieveORCIDinfo
+
+def information_linking_orcid(first_name, last_name, orcid_api_key):
+    if not orcid_api_key:
+        raise ValueError("\nPlease provide a valid ORCID API key: You can obtain a key from here: 'https://info.orcid.org/what-is-orcid/services/public-api/'")
+    if not first_name or not last_name:
+        raise ValueError("\nPlease provide a first name and a last name")
+    else:
+        orcid = retrieveORCIDinfo(first_name, last_name, orcid_api_key)
+
+    return orcid
