7a0452c96fe5c7450175545276d724c524aafbc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:38 +0000

    Add package aratree/0.1.0/aratree-0.1.0.tar.gz

---
+++ packages/aratree/aratree-0.1.0.tar.gz/aratree-0.1.0/aratree/__init__.py
@@ -0,0 +1,4 @@
+from .metrics import MSE, RMSE, MAE, MAPE, SSE
+from .regressors import RandomForestRegressor, DecisionTreeRegressor
+from .utils import rolling_mean
+from .preprocessing import prep_data
