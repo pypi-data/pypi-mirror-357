17835175750734da6924159b9b148b124f805269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:50 +0000

    Add package helper-package/0.1.0/helper_package-0.1.0.tar.gz

---
+++ packages/helper-package/helper_package-0.1.0.tar.gz/helper_package-0.1.0/helper_package/__init__.py
@@ -0,0 +1,1 @@
+from .helper_module import helper_module
---
+++ packages/helper-package/helper_package-0.1.0.tar.gz/helper_package-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="helper_module",
+    version="0.1.0",
+    description="A simple module to find symbol positions in a grid.",
+    author="Your Name",
+    author_email="you@example.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
