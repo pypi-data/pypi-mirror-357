ec25b6ca12f9b978e9c84ae671f04e4a110039cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:08 +0000

    Add package dinari-api-sdk/0.2.0/dinari_api_sdk-0.2.0-py3-none-any.whl

---
+++ packages/dinari-api-sdk/dinari_api_sdk-0.2.0-py3-none-any.whl/dinari_api_sdk/types/v2/accounts/order_request_create_limit_sell_params.py
@@ -0,0 +1,24 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, TypedDict
+
+__all__ = ["OrderRequestCreateLimitSellParams"]
+
+
+class OrderRequestCreateLimitSellParams(TypedDict, total=False):
+    asset_quantity: Required[int]
+    """Quantity of shares to trade. Must be a positive integer."""
+
+    limit_price: Required[float]
+    """Price at which to execute the order.
+
+    Must be a positive number with a precision of up to 2 decimal places.
+    """
+
+    stock_id: Required[str]
+    """ID of `Stock`."""
+
+    recipient_account_id: str
+    """ID of `Account` to receive the `Order`."""
---
+++ packages/dinari-api-sdk/dinari_api_sdk-0.2.0-py3-none-any.whl/dinari_api_sdk/types/v2/market_data/__init__.py
@@ -0,0 +1,16 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .stock_list_params import StockListParams as StockListParams
+from .stock_list_response import StockListResponse as StockListResponse
+from .stock_retrieve_news_params import StockRetrieveNewsParams as StockRetrieveNewsParams
+from .stock_retrieve_news_response import StockRetrieveNewsResponse as StockRetrieveNewsResponse
+from .stock_retrieve_quote_response import StockRetrieveQuoteResponse as StockRetrieveQuoteResponse
+from .stock_retrieve_dividends_response import StockRetrieveDividendsResponse as StockRetrieveDividendsResponse
+from .stock_retrieve_historical_prices_params import (
+    StockRetrieveHistoricalPricesParams as StockRetrieveHistoricalPricesParams,
+)
+from .stock_retrieve_historical_prices_response import (
+    StockRetrieveHistoricalPricesResponse as StockRetrieveHistoricalPricesResponse,
+)
