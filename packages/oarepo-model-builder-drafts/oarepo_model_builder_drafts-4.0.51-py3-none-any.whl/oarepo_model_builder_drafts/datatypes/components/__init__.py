7dc075f3b374da8ebfe94fe302a134ea87a6a316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:36 +0000

    Add package oarepo-model-builder-drafts/4.0.51/oarepo_model_builder_drafts-4.0.51-py3-none-any.whl

---
+++ packages/oarepo-model-builder-drafts/oarepo_model_builder_drafts-4.0.51-py3-none-any.whl/oarepo_model_builder_drafts/datatypes/components/__init__.py
@@ -0,0 +1,33 @@
+from .draft import DraftComponent
+from .draft_model import (
+    DraftDefaultsModelComponent,
+    DraftJSONSchemaModelComponent,
+    DraftMappingModelComponent,
+    DraftParentComponent,
+    DraftPIDModelComponent,
+    DraftRecordMetadataModelComponent,
+    DraftRecordModelComponent,
+    DraftResourceModelComponent,
+    DraftServiceModelComponent,
+    DraftsRecordDumperModelComponent,
+    ParentMarshmallowComponent,
+    DraftSearchOptionsModelComponent,
+)
+from .draft_tests import DraftModelTestComponent
+
+DRAFT_COMPONENTS = [
+    DraftParentComponent,
+    DraftComponent,
+    DraftRecordModelComponent,
+    DraftRecordMetadataModelComponent,
+    DraftPIDModelComponent,
+    DraftDefaultsModelComponent,
+    DraftJSONSchemaModelComponent,
+    DraftModelTestComponent,
+    DraftMappingModelComponent,
+    DraftResourceModelComponent,
+    DraftServiceModelComponent,
+    ParentMarshmallowComponent,
+    DraftsRecordDumperModelComponent,
+    DraftSearchOptionsModelComponent,
+]
