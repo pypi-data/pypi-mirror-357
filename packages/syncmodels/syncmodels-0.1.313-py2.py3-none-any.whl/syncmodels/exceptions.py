84d7f0b6534fdcbc0c3b8fffa27e8aac23ff9ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:03 +0000

    Add package syncmodels/0.1.313/syncmodels-0.1.313-py2.py3-none-any.whl

---
+++ packages/syncmodels/syncmodels-0.1.313-py2.py3-none-any.whl/syncmodels/exceptions.py
@@ -0,0 +1,27 @@
+# =========================================================
+# syncmodels
+# =========================================================
+
+
+class SyncModelException(Exception):
+    "base for all SyncModel Exceptions"
+
+
+class Recoverable(SyncModelException):
+    """the request can be recovered."""
+
+
+class NonRecoverable(SyncModelException):
+    """the request can't be recovered.
+    we need to advance the request.
+    """
+
+
+class NonRecoverableAuth(NonRecoverable):
+    """the request can't be recovered.
+    we need to advance the request.
+    """
+
+
+class BadData(SyncModelException):
+    """Data is not properly formatted."""
