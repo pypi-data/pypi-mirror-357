ac0270df1d14198e2ec6418d85dfeeec553c0154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:58 +0000

    Add package fusionlib/0.2/fusionlib-0.2-py3-none-any.whl

---
+++ packages/fusionlib/fusionlib-0.2-py3-none-any.whl/fusionlib/__init__.py
@@ -0,0 +1,51 @@
+# fusionlib: A one-stop import for common libraries
+
+# Numeric and Data Handling
+import numpy as np
+import pandas as pd
+import math
+import json
+
+# Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# NLP
+from textblob import TextBlob
+import nltk
+
+# DateTime
+import datetime as dt
+
+# Web/API
+import requests
+from bs4 import BeautifulSoup
+
+# ML / DS
+import scipy as sp
+import sklearn as skl
+
+# Image
+from PIL import Image
+
+# Logging
+import logging
+
+# Aliases for convenience
+np = np
+pd = pd
+plt = plt
+sns = sns
+math = math
+json = json
+dt = dt
+TextBlob = TextBlob
+nltk = nltk
+requests = requests
+BeautifulSoup = BeautifulSoup
+sp = sp
+skl = skl
+Image = Image
+logging = logging
+
+print("FusionLib Loaded: numpy, pandas, matplotlib, seaborn, scipy, sklearn, textblob, nltk, json, requests, BeautifulSoup, PIL, math, datetime, logging")
