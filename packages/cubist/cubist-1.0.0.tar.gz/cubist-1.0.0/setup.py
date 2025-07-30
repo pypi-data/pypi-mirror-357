f3fdddc38aa383cbfb2bfc8a9392930d0fb472aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:24 +0000

    Add package cubist/1.0.0/cubist-1.0.0.tar.gz

---
+++ packages/cubist/cubist-1.0.0.tar.gz/cubist-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import glob
+
+from Cython.Build import cythonize
+from setuptools import setup, Extension
+import numpy as np
+
+
+extensions = [
+    Extension(
+        name="_cubist",
+        sources=["cubist/_cubist.pyx"] + glob.glob("cubist/src/*.c"),
+        include_dirs=["cubist/src", np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    )
+]
+
+setup(
+    ext_modules=cythonize(
+        extensions, compiler_directives={"language_level": 3, "profile": False}
+    ),
+)
