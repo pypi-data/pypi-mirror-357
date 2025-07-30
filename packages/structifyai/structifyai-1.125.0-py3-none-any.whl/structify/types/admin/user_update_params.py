e0055abc95c1acc59ec086437c182a6ad85bb869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:45 +0000

    Add package structifyai/1.125.0/structifyai-1.125.0-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.125.0-py3-none-any.whl/structify/types/admin/user_update_params.py
@@ -0,0 +1,29 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["UserUpdateParams"]
+
+
+class UserUpdateParams(TypedDict, total=False):
+    current_email: Required[str]
+
+    new_email: Optional[str]
+
+    new_feature_flags: Optional[
+        List[
+            Literal[
+                "functional_test",
+                "pdf_parsing",
+                "boredm_construction_model",
+                "generic_suspicious_queue",
+                "new_use_case_preview",
+                "none",
+            ]
+        ]
+    ]
+
+    new_permissions: Optional[List[Literal["labeler", "qa_labeler", "debug", "human_llm", "none"]]]
