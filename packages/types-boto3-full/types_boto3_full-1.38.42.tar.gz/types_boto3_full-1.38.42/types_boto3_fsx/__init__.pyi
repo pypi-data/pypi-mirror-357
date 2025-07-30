87774f6d4c2328404ee59d1840f6e68d37b55cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:19 +0000

    Add package types-boto3-full/1.38.42/types_boto3_full-1.38.42.tar.gz

---
+++ packages/types-boto3-full/types_boto3_full-1.38.42.tar.gz/types_boto3_full-1.38.42/types_boto3_fsx/__init__.pyi
@@ -0,0 +1,52 @@
+"""
+Main interface for fsx service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_fsx/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_fsx import (
+        Client,
+        DescribeBackupsPaginator,
+        DescribeFileSystemsPaginator,
+        DescribeStorageVirtualMachinesPaginator,
+        DescribeVolumesPaginator,
+        FSxClient,
+        ListTagsForResourcePaginator,
+    )
+
+    session = Session()
+    client: FSxClient = session.client("fsx")
+
+    describe_backups_paginator: DescribeBackupsPaginator = client.get_paginator("describe_backups")
+    describe_file_systems_paginator: DescribeFileSystemsPaginator = client.get_paginator("describe_file_systems")
+    describe_storage_virtual_machines_paginator: DescribeStorageVirtualMachinesPaginator = client.get_paginator("describe_storage_virtual_machines")
+    describe_volumes_paginator: DescribeVolumesPaginator = client.get_paginator("describe_volumes")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    ```
+"""
+
+from .client import FSxClient
+from .paginator import (
+    DescribeBackupsPaginator,
+    DescribeFileSystemsPaginator,
+    DescribeStorageVirtualMachinesPaginator,
+    DescribeVolumesPaginator,
+    ListTagsForResourcePaginator,
+)
+
+Client = FSxClient
+
+__all__ = (
+    "Client",
+    "DescribeBackupsPaginator",
+    "DescribeFileSystemsPaginator",
+    "DescribeStorageVirtualMachinesPaginator",
+    "DescribeVolumesPaginator",
+    "FSxClient",
+    "ListTagsForResourcePaginator",
+)
