684eda7b4064634700050de42f686b0369a5ed94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:45 +0000

    Add package ddns/4.0.2/ddns-4.0.2-py2.py3-none-any.whl

---
+++ packages/ddns/ddns-4.0.2-py2.py3-none-any.whl/ddns/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+"""
+ddns Package
+"""
+
+__description__ = "automatically update DNS records to my IP [域名自动指向本机IP]"
+
+# 编译时，版本会被替换
+__version__ = "4.0.2"
+
+# 时间也会被替换掉
+build_date = "2025-06-25T01:07:58Z"
+
+__doc__ = """
+ddns [v{}@{}]
+(i) homepage or docs [文档主页]: https://ddns.newfuture.cc/
+(?) issues or bugs [问题和反馈]: https://github.com/NewFuture/DDNS/issues
+Copyright (c) New Future (MIT License)
+""".format(__version__, build_date)
