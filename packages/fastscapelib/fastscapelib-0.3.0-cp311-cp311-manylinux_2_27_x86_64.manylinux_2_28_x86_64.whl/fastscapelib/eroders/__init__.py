d2bc9053377987f1207f5a9d44227d2aa545db6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:04 +0000

    Add package fastscapelib/0.3.0/fastscapelib-0.3.0-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/fastscapelib/fastscapelib-0.3.0-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/fastscapelib/eroders/__init__.py
@@ -0,0 +1,8 @@
+from _fastscapelib_py.eroders import (  # type: ignore[import]
+    DiffusionADIEroder,
+    SPLEroder,
+)
+
+from fastscapelib.eroders.flow_kernel_eroder import FlowKernelEroder
+
+__all__ = ["DiffusionADIEroder", "FlowKernelEroder", "SPLEroder"]
