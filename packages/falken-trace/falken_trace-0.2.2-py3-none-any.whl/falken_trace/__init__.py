53933654521a5d8b9c0531556489e62a0e78874a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:14 +0000

    Add package falken-trace/0.2.2/falken_trace-0.2.2-py3-none-any.whl

---
+++ packages/falken-trace/falken_trace-0.2.2-py3-none-any.whl/falken_trace/__init__.py
@@ -0,0 +1,15 @@
+import wrapt
+
+from falken_trace.common import config
+from falken_trace.tracing.datadog import wrap_dd_span
+from falken_trace.web.fastapi import wrap_fastapi_entrypoint_span
+
+if config.env_vars_config.falken_trace_enabled:
+    if config.env_vars_config.dd_trace_enabled:
+        try:
+            wrapt.wrap_function_wrapper("ddtrace.trace", "Tracer.start_span", wrap_dd_span)
+        except AttributeError:
+            # fallback to v2 import
+            wrapt.wrap_function_wrapper("ddtrace", "Tracer.start_span", wrap_dd_span)
+    if config.env_vars_config.fastapi_installed:
+        wrapt.wrap_function_wrapper("fastapi.routing", "run_endpoint_function", wrap_fastapi_entrypoint_span)
