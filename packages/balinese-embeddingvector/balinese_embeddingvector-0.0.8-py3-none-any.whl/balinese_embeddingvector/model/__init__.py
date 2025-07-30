b9318b6ffe70daa901c20e568d698b6425c88938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:03 +0000

    Add package balinese-embeddingvector/0.0.8/balinese_embeddingvector-0.0.8-py3-none-any.whl

---
+++ packages/balinese-embeddingvector/balinese_embeddingvector-0.0.8-py3-none-any.whl/balinese_embeddingvector/model/__init__.py
@@ -0,0 +1,1 @@
+from .Glove import Glove
---
+++ packages/balinese-embeddingvector/balinese_embeddingvector-0.0.8-py3-none-any.whl/balinese_embeddingvector/pretrained/__init__.py
@@ -0,0 +1,4 @@
+from .BaliWord2Vec import BaliWord2Vec
+from .BaliFastText import BaliFastText
+from .BaliGlove import BaliGlove
+from .BaliBERT import BaliBERT
