b93d4ca7b12515880d1117c1efd8bb8fbb729d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:18 +0000

    Add package Aluora/1.0.4/aluora-1.0.4.tar.gz

---
+++ packages/Aluora/aluora-1.0.4.tar.gz/aluora-1.0.4/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '1.0.4' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
+PACKAGE_NAME = 'Aluora' #Debe coincidir con el nombre de la carpeta
+AUTHOR = 'Pedro Sarmiento' #Modificar con vuestros datos
+AUTHOR_EMAIL = 'pedro.sarmiento102@alu.ulpgc.es' #Modificar con vuestros datos
+URL = 'https://github.com/Pedro-Sarmiento' #Modificar con vuestros datos
+
+LICENSE = 'MIT' #Tipo de licencia
+DESCRIPTION = 'Librería para detectar alucinaciones' #Descripción corta
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8') #Referencia al documento README con una descripción más elaborada
+LONG_DESC_TYPE = "text/markdown"
+
+
+#Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
+INSTALL_REQUIRES = [
+      ]
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
