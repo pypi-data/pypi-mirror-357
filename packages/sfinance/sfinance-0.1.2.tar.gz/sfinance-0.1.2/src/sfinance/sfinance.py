6f4ec1b93bc6df2fa408929f82a1b4994ee878db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:26 +0000

    Add package sfinance/0.1.2/sfinance-0.1.2.tar.gz

---
+++ packages/sfinance/sfinance-0.1.2.tar.gz/sfinance-0.1.2/src/sfinance/sfinance.py
@@ -0,0 +1,12 @@
+from .fetcher import BaseFetcher
+from .ticker import Ticker
+
+class SFinance:
+    def __init__(self, base_url):
+        self.fetcher = BaseFetcher(base_url)
+
+    def ticker(self, symbol: str):
+        return Ticker(symbol, self.fetcher)
+
+    def close(self):
+        self.fetcher.close()
