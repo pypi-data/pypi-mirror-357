0df895528b4482b230f88500b0a6426fb5489d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:57 +0000

    Add package AutoGitSemVer/0.8.3/autogitsemver-0.8.3.tar.gz

---
+++ packages/AutoGitSemVer/autogitsemver-0.8.3.tar.gz/autogitsemver-0.8.3/src/AutoGitSemVer/__init__.py
@@ -0,0 +1,14 @@
+# pylint: disable=missing-module-docstring,invalid-name
+
+# Note that this value will be overwritten by calls to `python ../../Build.py update_version` based
+# on changes observed in the git repository. The default value below will be used until the value
+# here is explicitly updated by the Continuous Integration system.
+__version__ = "0.8.3"
+
+from .Lib import GenerateStyle, GetSemanticVersion, GetSemanticVersionResult
+
+__all__ = [
+    "GenerateStyle",
+    "GetSemanticVersion",
+    "GetSemanticVersionResult",
+]
