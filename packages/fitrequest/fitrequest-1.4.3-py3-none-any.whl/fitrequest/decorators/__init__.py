ca896bc4df3ca07bb2ac63039a7b0631504a3a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:39 +0000

    Add package fitrequest/1.4.3/fitrequest-1.4.3-py3-none-any.whl

---
+++ packages/fitrequest/fitrequest-1.4.3-py3-none-any.whl/fitrequest/decorators/__init__.py
@@ -0,0 +1,14 @@
+from fitrequest.decorators.cli_method import cli_method
+from fitrequest.decorators.fit import delete, fit, get, patch, post, put
+from fitrequest.decorators.retry import retry
+
+__all__ = [
+    'cli_method',
+    'delete',
+    'fit',
+    'get',
+    'patch',
+    'post',
+    'put',
+    'retry',
+]
