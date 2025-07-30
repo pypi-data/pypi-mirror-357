a5278d68cf30c45975e9b50fda1519f18e0e9b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:50 +0000

    Add package schematized-jwt/0.1.0/schematized_jwt-0.1.0-py3-none-any.whl

---
+++ packages/schematized-jwt/schematized_jwt-0.1.0-py3-none-any.whl/schematized_jwt/exceptions.py
@@ -0,0 +1,39 @@
+class SchematizedJWTError(Exception):
+    """Any errors while working with JWT."""
+
+
+# Decode
+
+
+class JWTDecodeInvalidError(SchematizedJWTError):
+    """Token is invalid."""
+
+
+# Encode
+
+
+class JWTInvalidPayloadError(SchematizedJWTError):
+    """Specified schema for JWT and payload does not match."""
+
+
+# Decoded validation
+
+
+class JWTValidationError(SchematizedJWTError):
+    """Any errors while validate JWT payload."""
+
+
+class JWTValidationInvalidPayload(JWTValidationError):
+    """Decoded payload from token is invalid."""
+
+
+class JWTValidationExpiredError(JWTValidationError):
+    """TTL of JWT has been expired."""
+
+
+class JWTValidationMismatchTokenTypeError(JWTValidationError):
+    """Token type (access, refresh) in payload mismatch."""
+
+
+# class JWTInvalidTokenType(JWTValidationError):
+#     """The type of JWT in the payload does not match."""
