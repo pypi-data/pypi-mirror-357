a6de7dab235b6ad004d31fb8fdddf82a4b9baf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:01 +0000

    Add package mseep-BigGo-MCP-Server/1.0.1/mseep_biggo_mcp_server-1.0.1.tar.gz

---
+++ packages/mseep-BigGo-MCP-Server/mseep_biggo_mcp_server-1.0.1.tar.gz/mseep_biggo_mcp_server-1.0.1/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-BigGo-MCP-Server",
+    version="1.0.1",
+    description="A Model Context Protocol (MCP) server that provides product search, price history tracking, and specification search capabilities.",
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    maintainer="mseep",
+    maintainer_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['aiohttp>=3.11.11', 'async-lru>=2.0.4', 'async-timeout>=5.0.1', 'elasticsearch8[async]>=8.17.1', 'mcp[cli]>=1.2.1', 'typing-extensions>=4.12.2'],
+    keywords=["mseep"] + [],
+)
