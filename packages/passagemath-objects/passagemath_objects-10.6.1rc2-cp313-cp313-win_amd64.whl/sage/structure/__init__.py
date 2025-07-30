08489176ec3b797d7b07be28fc32bdf747ec8684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:45 +0000

    Add package passagemath-objects/10.6.1rc2/passagemath_objects-10.6.1rc2-cp313-cp313-win_amd64.whl

---
+++ packages/passagemath-objects/passagemath_objects-10.6.1rc2-cp313-cp313-win_amd64.whl/sage/structure/__init__.py
@@ -0,0 +1,16 @@
+# sage_setup: distribution = sagemath-objects
+# Resolve a cyclic import
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_10_1():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'passagemath_objects.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_10_1()
+del _delvewheel_patch_1_10_1
+# end delvewheel patch
+
+import sage.structure.element
