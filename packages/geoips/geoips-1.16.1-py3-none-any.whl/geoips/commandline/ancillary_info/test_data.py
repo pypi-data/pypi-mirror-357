85914cbbf99b49d795d843110a6fa243d89e8202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:45 +0000

    Add package geoips/1.16.1/geoips-1.16.1-py3-none-any.whl

---
+++ packages/geoips/geoips-1.16.1-py3-none-any.whl/geoips/commandline/ancillary_info/test_data.py
@@ -0,0 +1,21 @@
+# # # This source code is subject to the license referenced at
+# # # https://github.com/NRLMMD-GEOIPS.
+
+"""Ancillary module containing test dataset information.
+
+Dictionary mapping of GeoIPS Test Datasets.
+
+Mapping goes {"test_dataset_name": "test_dataset_url"}
+"""
+
+from os import environ
+
+import yaml
+
+interface = None  # denotes that this is not a plugin module
+
+urls = yaml.safe_load(
+    open(f"{environ['GEOIPS_PACKAGES_DIR']}/geoips/setup/test-data-urls.yaml", "r")
+)
+
+test_dataset_dict = urls["test_data_urls"]
