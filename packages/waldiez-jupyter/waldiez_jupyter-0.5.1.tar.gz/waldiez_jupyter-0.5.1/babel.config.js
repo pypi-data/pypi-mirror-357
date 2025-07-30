f828b630ab471c83ace435aadf766878a6910025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:05 +0000

    Add package waldiez-jupyter/0.5.1/waldiez_jupyter-0.5.1.tar.gz

---
+++ packages/waldiez-jupyter/waldiez_jupyter-0.5.1.tar.gz/waldiez_jupyter-0.5.1/babel.config.js
@@ -0,0 +1,9 @@
+ 
+const babelConfig = require("@jupyterlab/testutils/lib/babel.config");
+module.exports = {
+    env: {
+        test: {
+            plugins: ["@babel/plugin-transform-modules-commonjs"],
+        },
+    },
+};
