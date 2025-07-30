38721168a503d58b92c21c2a73f8191232e272b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:23 +0000

    Add package freva-deployment/2505.0.0/freva_deployment-2505.0.0.tar.gz

---
+++ packages/freva-deployment/freva_deployment-2505.0.0.tar.gz/freva_deployment-2505.0.0/docs/conf.py
@@ -0,0 +1,164 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import io
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+from contextlib import redirect_stderr, redirect_stdout
+from datetime import date
+
+# from recommonmark.parser import CommonMarkParser
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../../src"))
+
+from freva_deployment import __version__
+from freva_deployment.cli import main_cli
+
+
+def get_cli_output(sub_cmd=""):
+    if sub_cmd:
+        cmd = [sub_cmd, "--help"]
+    else:
+        cmd = ["--help"]
+    command = f"deploy-freva {sub_cmd} --help"
+    buf = io.StringIO()
+    try:
+        with redirect_stderr(buf), redirect_stdout(buf):
+            main_cli(cmd)
+    except SystemExit:
+        pass
+    output = buf.getvalue()
+    return f"```console\n{command}\n{output}```"
+
+
+cli_tui = get_cli_output()
+cli_cmd = get_cli_output("cmd")
+cli_mig = get_cli_output("migrate")
+# -- Project information -----------------------------------------------------
+
+project = "freva-deployment"
+copyright = f"{date.today().year}, DKRZ - CLINT"
+author = "Climate Informatics and Technology"
+
+# The full version, including alpha/beta/rc tags
+release = __version__
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx_copybutton",
+    "sphinx_togglebutton",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "pydata_sphinx_theme"
+html_logo = "_static/freva_owl.svg"
+html_favicon = "_static/freva_owl.svg"
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_sidebars = {"pagename": []}
+html_static_path = ["_static"]
+html_context = {
+    "github_user": "FREVA-CLINT",
+    "github_repo": "freva",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+html_sidebars = {
+    "community/index": [
+        "sidebar-nav-bs",
+        "custom-template",
+    ],  # This ensures we test for custom sidebars
+}
+
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/FREVA-CLINT/freva-deployment",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/freva-deployment",
+            "icon": "fa-custom fa-pypi",
+        },
+    ],
+    "navbar_center": ["navbar-nav"],  # Add navigation links to the left sidebar
+    "collapse_navigation": False,
+    "navigation_depth": 4,
+    "navbar_align": "left",
+    "show_nav_level": 2,
+    "secondary_sidebar_items": ["page-toc"],
+}
+html_sidebars = {"**": ["sidebar-nav-bs", "sidebar-ethical-ads"]}
+
+
+# -- MyST options ------------------------------------------------------------
+
+# This allows us to use ::: to denote directives, useful for admonitions
+myst_enable_extensions = ["colon_fence", "linkify", "substitution"]
+myst_heading_anchors = 2
+myst_substitutions = {
+    "rtd": "[Read the Docs](https://readthedocs.org/)",
+    "version": __version__,
+    "cli_tui": cli_tui,
+    "cli_cmd": cli_cmd,
+    "cli_mig": cli_mig,
+}
+myst_url_schemes = {
+    "http": None,
+    "https": None,
+    "release": f"https://github.com/FREVA-CLINT/freva-deployment/releases/tag/v{release}",
+    "exe": f"https://github.com/FREVA-CLINT/freva-deployment/releases/download/v{release}/deploy-freva-v{release}-{{{{path}}}}",
+}
+# Substitutions
+rst_prolog = """
+.. version replace:: {version}
+""".format(
+    version=__version__,
+)
+
+# ReadTheDocs has its own way of generating sitemaps, etc.
+if not os.environ.get("READTHEDOCS"):
+    extensions += ["sphinx_sitemap"]
+
+    html_baseurl = os.environ.get("SITEMAP_URL_BASE", "http://127.0.0.1:8000/")
+    sitemap_locales = [None]
+    sitemap_url_scheme = "{link}"
+
+# specifying the natural language populates some key tags
+language = "en"
