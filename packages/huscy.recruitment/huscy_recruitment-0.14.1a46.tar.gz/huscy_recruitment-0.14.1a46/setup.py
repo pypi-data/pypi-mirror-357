5ad07d1f4b288868de9939420452929b316cd48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:48 +0000

    Add package huscy.recruitment/0.14.1a46/huscy_recruitment-0.14.1a46.tar.gz

---
+++ packages/huscy.recruitment/huscy_recruitment-0.14.1a46.tar.gz/huscy_recruitment-0.14.1a46/setup.py
@@ -0,0 +1,47 @@
+from setuptools import find_namespace_packages, setup
+
+from huscy.recruitment import __version__
+
+
+setup(
+    name="huscy.recruitment",
+    version=__version__,
+    license='AGPLv3+',
+
+    author='Alexander Tyapkov, Mathias Goldau, Stefan Bunde',
+    author_email='tyapkov@gmail.com, goldau@cbs.mpg.de, stefanbunde+git@posteo.de',
+
+    packages=find_namespace_packages(include=['huscy.*']),
+
+    install_requires=[
+        'huscy.appointments',
+        'huscy.attributes',
+        'huscy.bookings',
+        'huscy.participations',
+        'huscy.project_design',
+        'python-dateutil',
+    ],
+    extras_require={
+        'development': ['psycopg2-binary'],
+        'testing': ['tox', 'watchdog'],
+    },
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+    ],
+)
