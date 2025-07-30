2b466187e147d0d556f11970ca0e6ab4cd277a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:27 +0000

    Add package pyprinty/1.0.1/pyprinty-1.0.1.tar.gz

---
+++ packages/pyprinty/pyprinty-1.0.1.tar.gz/pyprinty-1.0.1/src/pyprinty/__init__.py
@@ -0,0 +1,12 @@
+from ._effects import Effects, Cursor
+from ._animation import Animation
+from ._colors import Color, Colors
+from ._font import Font, Ready
+from ._terminal import good, size
+
+
+__all__ = ["Effects", "Cursor", "Animation", "Color", "Colors", "Ready", "Font", "size", "good"]
+__version__ = "1.0.1"
+__author__ = "Yedidya steinmetz"
+__email__ = "yd.sc.20@gmail.com"
+__license__ = "MIT"
