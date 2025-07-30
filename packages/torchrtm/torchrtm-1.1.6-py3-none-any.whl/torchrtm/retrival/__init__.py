23c7795347a598b5f3298c126794e7d8c8e2e327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:20 +0000

    Add package torchrtm/1.1.6/torchrtm-1.1.6-py3-none-any.whl

---
+++ packages/torchrtm/torchrtm-1.1.6-py3-none-any.whl/torchrtm/retrival/__init__.py
@@ -0,0 +1,13 @@
+"""
+torchrtm.models
+---------------
+
+High-level modeling utilities for integrated radiative transfer workflows.
+
+Includes:
+    - prosail_shell_v2: Full PROSAIL canopy reflectance simulation wrapper.
+"""
+
+from .fastLUT import call_torch
+from .fastLUT import Torchlut_pred
+from .fastNN import fast_ann
