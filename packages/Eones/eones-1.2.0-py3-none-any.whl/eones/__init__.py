5ef3397362e804650fa09c7e5a78ab1de20510ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:24 +0000

    Add package Eones/1.2.0/eones-1.2.0-py3-none-any.whl

---
+++ packages/Eones/eones-1.2.0-py3-none-any.whl/eones/__init__.py
@@ -0,0 +1,13 @@
+"""
+Eones: Navigate time with elegance.
+
+This package provides a semantically rich interface for
+handling dates and temporal logic. From parsing and formatting
+to complex deltas and period framing, Eones offers tools to  handling
+reason about time — not just measure it.
+"""
+
+from eones.interface import Eones
+
+__version__ = "1.2.0"
+__all__ = ["Eones"]
