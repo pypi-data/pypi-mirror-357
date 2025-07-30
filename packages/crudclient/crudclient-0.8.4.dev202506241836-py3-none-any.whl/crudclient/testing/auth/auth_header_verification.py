cc289304a42ca1fecc6123c4526a273fc0dd8167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:12 +0000

    Add package crudclient/0.8.4.dev202506241836/crudclient-0.8.4.dev202506241836-py3-none-any.whl

---
+++ packages/crudclient/crudclient-0.8.4.dev202506241836-py3-none-any.whl/crudclient/testing/auth/auth_header_verification.py
@@ -0,0 +1,5 @@
+"""Compatibility wrapper for authentication header verification utilities."""
+
+from apiconfig.testing.auth_verification import AuthHeaderVerification
+
+__all__ = ["AuthHeaderVerification"]
