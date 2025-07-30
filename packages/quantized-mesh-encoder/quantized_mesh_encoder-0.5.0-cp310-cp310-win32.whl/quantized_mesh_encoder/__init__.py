0439f514af707c3cd2588f25528791f487446b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:17 +0000

    Add package quantized-mesh-encoder/0.5.0/quantized_mesh_encoder-0.5.0-cp310-cp310-win32.whl

---
+++ packages/quantized-mesh-encoder/quantized_mesh_encoder-0.5.0-cp310-cp310-win32.whl/quantized_mesh_encoder/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for quantized_mesh_encoder."""
+
+__author__ = """Kyle Barron"""
+__email__ = "kylebarron2@gmail.com"
+__version__ = "0.5.0"
+
+from .constants import WGS84
+from .ellipsoid import Ellipsoid
+from .encode import encode
+from .extensions import MetadataExtension, VertexNormalsExtension, WaterMaskExtension
---
+++ packages/quantized-mesh-encoder/quantized_mesh_encoder-0.5.0-cp310-cp310-win32.whl/quantized_mesh_encoder/ellipsoid.py
@@ -0,0 +1,18 @@
+import attr
+
+
+@attr.s
+class Ellipsoid:
+    """Ellipsoid used for mesh calculations
+
+    Args:
+        a (float): semi-major axis
+        b (float): semi-minor axis
+    """
+
+    a: float = attr.ib()
+    b: float = attr.ib()
+    e2: float = attr.ib(init=False)
+
+    def __attrs_post_init__(self):
+        self.e2 = 1 - (self.b**2 / self.a**2)
