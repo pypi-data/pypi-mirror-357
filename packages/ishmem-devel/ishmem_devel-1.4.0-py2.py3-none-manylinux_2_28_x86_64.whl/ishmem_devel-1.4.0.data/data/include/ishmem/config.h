941a792724a729571042547cbf9cdbe5f46ecac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:51 +0000

    Add package ishmem-devel/1.4.0/ishmem_devel-1.4.0-py2.py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/ishmem-devel/ishmem_devel-1.4.0-py2.py3-none-manylinux_2_28_x86_64.whl/ishmem_devel-1.4.0.data/data/include/ishmem/config.h
@@ -0,0 +1,10 @@
+/* Copyright (C) 2024 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#define ENABLE_MPI
+#define ENABLE_OPENSHMEM
+/* #undef ENABLE_PMI */
+/* #undef ENABLE_ERROR_CHECKING */
+/* #undef ENABLE_REDUCED_LINK_ENGINES */
+#define ENABLE_DLMALLOC
