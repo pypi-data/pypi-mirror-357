4bd4370f649f302df58cfca4204771a3eefb8bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:43 +0000

    Add package Lires/1.8.7/lires-1.8.7.tar.gz

---
+++ packages/Lires/lires-1.8.7.tar.gz/lires-1.8.7/lires/types/dataT.py
@@ -0,0 +1,46 @@
+from typing import Any, List, Optional, Literal, TypeAlias
+from dataclasses import dataclass
+
+# FileTypeT = Literal['.pdf', '.html']     # should be one of the accepted extensions at config.py
+FileTypeT: TypeAlias = Literal['', '.pdf', '.html']
+
+@dataclass
+class DataPointSummary():
+    doc_type: str
+    has_file: bool
+    file_type: FileTypeT
+    year: Any
+    title: str
+    author: str
+    authors: List[str]
+    publication: Optional[str]
+    tags: List[str]
+    uuid: str
+    url: str
+    time_added: float
+    time_modified: float
+    bibtex: str
+    doc_size: float # in M.
+    note_linecount: int
+    has_abstract: bool
+
+    def json(self):
+        return {
+            "doc_type": self.doc_type,
+            "has_file": self.has_file,
+            "file_type": self.file_type,
+            "year": self.year,
+            "title": self.title,
+            "author": self.author,
+            "authors": self.authors,
+            "publication": self.publication,
+            "tags": self.tags,
+            "uuid": self.uuid,
+            "url": self.url,
+            "time_added": self.time_added,
+            "time_modified": self.time_modified,
+            "bibtex": self.bibtex,
+            "doc_size": self.doc_size,
+            "note_linecount": self.note_linecount,
+            "has_abstract": self.has_abstract
+        }
---
+++ packages/Lires/lires-1.8.7.tar.gz/lires-1.8.7/lires_server/handlers/search.py
@@ -0,0 +1,116 @@
+from ._base import *
+from typing import TypedDict
+from lires.core.vecutils import query_feature_index
+import json
+
+
+class BasicFilterT(TypedDict):
+    tags: list[str]
+    search_by: str 
+    search_content: str
+
+    top_k: int
+
+class BasicFilterHandler(RequestHandlerBase):
+    # Get data by a single tag and search filter
+
+    @authenticate()
+    async def post(self):
+        # url encoded form
+        self.set_header("Content-Type", "application/json")
+        db = await self.db()
+
+        # Get the tag and search filter from form
+        tags = json.loads(self.get_argument("tags"))
+        search_by = self.get_argument("search_by")
+        search_content = self.get_argument("search_content")
+        top_k = int(self.get_argument("top_k"))
+
+        await self.logger.debug(f"tags: {tags}, search_by: {search_by}, search_content: {search_content}, top_k: {top_k}")
+
+        # Get the data
+        if (not search_content) and (not tags):
+            return self.write(json.dumps({
+                'uids': await db.keys(),
+                'scores': None
+            }))
+
+        if tags:
+            cadidate_ids = await db.ids_from_tags(tags)
+            res = cadidate_ids
+        else:
+            cadidate_ids = None
+            res = await db.keys()
+        scores = None
+
+        if not search_content:
+            pass
+
+        elif search_by == 'title':
+            res = await db.conn.filter(strict=False, ignore_case=True, from_uids=cadidate_ids, title=search_content)
+        
+        elif search_by == 'year':
+            q = None
+            if search_content.isnumeric():
+                q = int(search_content)
+            else:
+                for sep in ['-', 'to', ',']:
+                    if sep in search_content:
+                        q = tuple([int(x.strip()) for x in search_content.split(sep)])
+                        if not len(q) == 2: q = q[0]
+                        break
+            if q is None:
+                raise tornado.web.HTTPError(400, "Invalid search year value")
+            res = await db.conn.filter(strict=False, ignore_case=True, from_uids=cadidate_ids, year=q)
+        
+        elif search_by == 'publication':
+            res = await db.conn.filter(strict=False, ignore_case=True, from_uids=cadidate_ids, publication=search_content)
+        
+        elif search_by == 'note':
+            res = await db.conn.filter(strict=False, ignore_case=True, from_uids=cadidate_ids, note=search_content)
+        
+        elif search_by == 'uuid':
+            res = []
+            for uid in await db.keys():
+                if uid.startswith(search_content):
+                    res.append(uid)
+        
+        elif search_by == 'feature':
+            q_res = await query_feature_index(
+                iconn=self.iconn,
+                query=search_content,
+                n_return=top_k,
+                vector_collection= await (await self.vec_db()).get_collection("doc_feature")
+            )
+            res_ = [x["entry"]["uid"] for x in q_res]
+            scores_ = [x["score"] for x in q_res]
+            if cadidate_ids is not None:
+                candidate_set = set(cadidate_ids)    # convert to set may be faster?
+                res = []
+                scores = []
+                for uid, score in zip(res_, scores_):
+                    if uid in candidate_set:
+                        res.append(uid)
+                        scores.append(score)
+            else:
+                res = res_
+                scores = scores_
+        
+        elif search_by == 'author':
+            # TODO: Optimize this to unify the author name
+            res = await db.conn.filter(strict=False, ignore_case=True, from_uids=cadidate_ids, authors=[search_content])
+        
+        else:
+            raise tornado.web.HTTPError(400, "Invalid search_by value")
+        
+        await self.logger.debug(f"returning {len(res)} results.")
+
+        # Sort the result if no scores are provided
+        if scores is None:
+            res = await db.conn.sort_keys(res)
+
+        self.write(json.dumps({
+            'uids': res,
+            'scores': scores
+        }))
+        return
