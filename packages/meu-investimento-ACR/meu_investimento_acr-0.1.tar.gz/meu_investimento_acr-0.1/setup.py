d54e267c143c8dcc95041aa194d5403cb340bc9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:16 +0000

    Add package meu-investimento-ACR/0.1/meu_investimento_acr-0.1.tar.gz

---
+++ packages/meu-investimento-ACR/meu_investimento_acr-0.1.tar.gz/meu_investimento_acr-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='meu_investimento_ACR',
+   version='0.1',
+   packages=find_packages(),
+   install_requires=[],
+   author='Angelo Rossi',
+   author_email='yrc_legna@hotmail.com',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='https://github.com/AngeloCRossi/MLET_PYTHON_ML_IA_AULA2.git',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)
