c0c44256496f55ce07b76d43bccbf92955974f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:32 +0000

    Add package msldap-bAD/0.5.25/msldap_bad-0.5.25-py3-none-any.whl

---
+++ packages/msldap-bAD/msldap_bad-0.5.25-py3-none-any.whl/msldap/_version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.5.25"
+__banner__ = \
+"""
+# msldap %s 
+# Original Author: Tamas Jos (@skelsec)
+# Current Maintainer: Baptiste Crépin (baptiste@cravaterouge.com)
+""" % __version__
