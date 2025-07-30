f932b1a4b7112236b5b80aab10d47ebf30108c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:21 +0000

    Add package django-smart-cache/0.1.0/django_smart_cache-0.1.0-py3-none-any.whl

---
+++ packages/django-smart-cache/django_smart_cache-0.1.0-py3-none-any.whl/django_smart_cache/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class DjangoSmartCacheConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_smart_cache"
+    verbose_name = "Django Smart Cache"
+
+    def ready(self):
+        from tests import views, viewsets
