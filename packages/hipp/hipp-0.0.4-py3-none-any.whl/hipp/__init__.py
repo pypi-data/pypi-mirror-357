e80176c2ae64968d61766a6572ac7bca4d652176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:14 +0000

    Add package hipp/0.0.4/hipp-0.0.4-py3-none-any.whl

---
+++ packages/hipp/hipp-0.0.4-py3-none-any.whl/hipp/__init__.py
@@ -0,0 +1,4 @@
+from . import aerial, dataquery, image, kh9pc, tools
+from .aerial.aerial_preprocessing import AerialPreprocessing
+
+__all__ = ["aerial", "AerialPreprocessing", "dataquery", "tools", "kh9pc", "image"]
