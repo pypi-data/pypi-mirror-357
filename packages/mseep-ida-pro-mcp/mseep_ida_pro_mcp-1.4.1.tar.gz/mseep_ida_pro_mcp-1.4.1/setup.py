de7faeeb713711d9416e8d8c7cb6ee55eac95679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:03 +0000

    Add package mseep-ida-pro-mcp/1.4.1/mseep_ida_pro_mcp-1.4.1.tar.gz

---
+++ packages/mseep-ida-pro-mcp/mseep_ida_pro_mcp-1.4.1.tar.gz/mseep_ida_pro_mcp-1.4.1/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-ida-pro-mcp",
+    version="1.4.1",
+    description="Vibe reversing with IDA Pro",
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    maintainer="mseep",
+    maintainer_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.6.0'],
+    keywords=["mseep"] + ['ida', 'mcp', 'llm', 'plugin'],
+)
