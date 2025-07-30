5dfb37dffe9a3d5e0e2ae62a7835b6eade66ad2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:35 +0000

    Add package logdev/0.0.1/logdev-0.0.1.tar.gz

---
+++ packages/logdev/logdev-0.0.1.tar.gz/logdev-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="logdev",
+    version="0.0.1",
+    description="Reserved package name for logdev.",
+    author="Zayry",
+    author_email="admin@zayry.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
