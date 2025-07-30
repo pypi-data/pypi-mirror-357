5c2f574c217c003af9af446371fe8566f14143d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:47 +0000

    Add package latent-calendar/1.6.0/latent_calendar-1.6.0-py3-none-any.whl

---
+++ packages/latent-calendar/latent_calendar-1.6.0-py3-none-any.whl/latent_calendar/__init__.py
@@ -0,0 +1,7 @@
+import latent_calendar.extensions  # noqa
+from latent_calendar.model.latent_calendar import (  # noqa
+    LatentCalendar,
+    ConjugateModel,
+    DummyModel,
+    MarginalModel,
+)
