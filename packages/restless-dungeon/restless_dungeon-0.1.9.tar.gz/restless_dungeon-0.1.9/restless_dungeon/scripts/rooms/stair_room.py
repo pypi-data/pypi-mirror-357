9bd42928659d7237c732dceca2137a22e2eede4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:32 +0000

    Add package restless-dungeon/0.1.9/restless_dungeon-0.1.9.tar.gz

---
+++ packages/restless-dungeon/restless_dungeon-0.1.9.tar.gz/restless_dungeon-0.1.9/restless_dungeon/scripts/rooms/stair_room.py
@@ -0,0 +1,20 @@
+from rich import print
+from time import sleep
+
+def stair_room(player):
+    print("You come across a staircase.")
+    sleep(1)
+    print("You can go up or down.")
+    sleep(1)
+    print("Which way do you climb?")
+    choice = input(">> ").lower()
+    if choice == "up":
+        print("You reach the top of the stairs and find the next door.")
+    elif choice == "down":
+        print("[red]As you descend, a deep growl fills the silence.[/red]")
+        print("[red]You turn around and sprint up the stairs.[/red]")
+        player.consume_energy(3)
+        print("You reach the top of the stairs and find the next door.")
+    else:
+        print("You cannot do that.")
+        stair_room(player)
