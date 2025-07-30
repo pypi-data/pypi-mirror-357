dca5955ed09a7e10893837988fb384f0c98562a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:23 +0000

    Add package async-rithmic/1.5.1/async_rithmic-1.5.1-py3-none-any.whl

---
+++ packages/async-rithmic/async_rithmic-1.5.1-py3-none-any.whl/async_rithmic/plants/pnl.py
@@ -0,0 +1,45 @@
+from .base import BasePlant
+from .. import protocol_buffers as pb
+
+class PnlPlant(BasePlant):
+    infra_type = pb.request_login_pb2.RequestLogin.SysInfraType.PNL_PLANT
+
+    @property
+    def _accounts(self):
+        return self.client.plants["order"].accounts
+
+    @property
+    def _fcm_id(self):
+        return self.client.plants["order"].login_info["fcm_id"]
+
+    @property
+    def _ib_id(self):
+        return self.client.plants["order"].login_info["ib_id"]
+
+    async def _login(self):
+        await super()._login()
+        await self._subscribe_to_position_updates()
+
+    async def _subscribe_to_position_updates(self):
+        for account in self._accounts:
+            await self._send_request(
+                template_id=400,
+                fcm_id=self._fcm_id,
+                ib_id=self._ib_id,
+                account_id=account.account_id,
+                request=pb.request_pnl_position_updates_pb2.RequestPnLPositionUpdates.Request.SUBSCRIBE
+            )
+
+    async def list_positions(self, **kwargs):
+        return await self._send_and_collect(
+            template_id=402,
+            expected_response=dict(template_id=450, is_snapshot=True),
+            **kwargs
+        )
+
+    async def list_account_summary(self, **kwargs):
+        return await self._send_and_collect(
+            template_id=402,
+            expected_response=dict(template_id=451, is_snapshot=True),
+            **kwargs
+        )
