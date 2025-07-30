3acd9870f4a85ec1712115baf2f561dce520b2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:04 +0000

    Add package MeUtils/2025.6.24.11.13.32/MeUtils-2025.6.24.11.13.32-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2025.6.24.11.13.32-py3-none-any.whl/meutils/llm/openai_utils/usage_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : usage_utils
+# @Time         : 2025/6/24 08:53
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+"""
+1. 同步任务（流 非流）
+    - 按次
+    - 按量
+2. 异步任务
+    - 按次
+    - 按量
+"""
+
+from meutils.pipe import *
