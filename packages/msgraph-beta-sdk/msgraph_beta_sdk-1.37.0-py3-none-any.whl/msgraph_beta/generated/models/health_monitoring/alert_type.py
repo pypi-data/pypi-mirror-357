f2eca770aafd91895cbdca053a92bf8f9586c1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:12 +0000

    Add package msgraph-beta-sdk/1.37.0/msgraph_beta_sdk-1.37.0-py3-none-any.whl

---
+++ packages/msgraph-beta-sdk/msgraph_beta_sdk-1.37.0-py3-none-any.whl/msgraph_beta/generated/models/health_monitoring/alert_type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class AlertType(str, Enum):
+    Unknown = "unknown",
+    MfaSignInFailure = "mfaSignInFailure",
+    ManagedDeviceSignInFailure = "managedDeviceSignInFailure",
+    CompliantDeviceSignInFailure = "compliantDeviceSignInFailure",
+    UnknownFutureValue = "unknownFutureValue",
+    ConditionalAccessBlockedSignIn = "conditionalAccessBlockedSignIn",
+
