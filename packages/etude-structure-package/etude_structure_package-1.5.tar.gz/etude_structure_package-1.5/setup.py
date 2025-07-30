b5c41a1299d3dd4e9496fd3f1696116556751f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:40 +0000

    Add package etude-structure-package/1.5/etude_structure_package-1.5.tar.gz

---
+++ packages/etude-structure-package/etude_structure_package-1.5.tar.gz/etude_structure_package-1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='etude_structure_package',
+    version='1.5',
+    # package_dir={"": "src"},
+    packages=find_packages(),
+    description='Un package destiné à montrer la structure d\'un package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Georges CAPLANDE',
+    author_email='georges.caplande@gmail.com',
+        classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    python_requires='>=3.6',
+    install_requires=[],
+)
