bce8ae83463b266ccae13aa0f95a0212a7474287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:37 +0000

    Add package ddtrace/3.9.4/ddtrace-3.9.4.tar.gz

---
+++ packages/ddtrace/ddtrace-3.9.4.tar.gz/ddtrace-3.9.4/tests/contrib/azure_functions/azure_function_app/function_app.py
@@ -0,0 +1,88 @@
+import os
+
+from ddtrace import patch
+
+
+patch(azure_functions=True, requests=True)
+
+import azure.functions as func  # noqa: E402
+import requests  # noqa: E402
+
+
+app = func.FunctionApp()
+
+
+@app.route(route="httpgetok", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.GET])
+def http_get_ok(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("Hello Datadog!")
+
+
+@app.route(route="httpgetokasync", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.GET])
+async def http_get_ok_async(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("Hello Datadog!")
+
+
+@app.route(route="httpgeterror", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.GET])
+def http_get_error(req: func.HttpRequest) -> func.HttpResponse:
+    raise Exception("Test Error")
+
+
+@app.route(route="httppostok", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.POST])
+def http_post_ok(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("Hello Datadog!")
+
+
+@app.route(
+    route="httpgettriggerarg",
+    auth_level=func.AuthLevel.ANONYMOUS,
+    methods=[func.HttpMethod.GET],
+    trigger_arg_name="reqarg",
+)
+def http_get_trigger_arg(reqarg: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("Hello Datadog!")
+
+
+@app.function_name(name="functionnamedecorator")
+@app.route(route="httpgetfunctionnamedecorator", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.GET])
+def http_get_function_name_decorator(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("Hello Datadog!")
+
+
+@app.route(route="httpgetfunctionnamenodecorator", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.GET])
+def http_get_function_name_no_decorator(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("Hello Datadog!")
+
+
+@app.route(route="httpgetroot", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.GET])
+def http_get_root(req: func.HttpRequest) -> func.HttpResponse:
+    requests.get(f"http://localhost:{os.environ['AZURE_FUNCTIONS_TEST_PORT']}/api/httpgetchild", timeout=5)
+    return func.HttpResponse("Hello Datadog!")
+
+
+@app.route(route="httpgetchild", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.GET])
+def http_get_child(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("Hello Datadog!")
+
+
+@app.function_name(name="servicebusqueue")
+@app.service_bus_queue_trigger(arg_name="msg", queue_name="queue.1", connection="CONNECTION_SETTING")
+def service_bus_queue(msg: func.ServiceBusMessage):
+    pass
+
+
+@app.function_name(name="servicebustopic")
+@app.service_bus_topic_trigger(
+    arg_name="msg", topic_name="topic.1", connection="CONNECTION_SETTING", subscription_name="subscription.1"
+)
+def service_bus_topic(msg: func.ServiceBusMessage):
+    pass
+
+
+@app.timer_trigger(schedule="0 0 0 1 1 *", arg_name="timer")
+def timer(timer: func.TimerRequest) -> None:
+    pass
+
+
+@app.timer_trigger(schedule="0 0 0 1 1 *", arg_name="timer")
+async def timer_async(timer: func.TimerRequest) -> None:
+    pass
---
+++ packages/ddtrace/ddtrace-3.9.4.tar.gz/ddtrace-3.9.4/tests/contrib/botocore/bedrock_utils.py
@@ -0,0 +1,128 @@
+import os
+
+import boto3
+
+
+try:
+    import vcr
+except ImportError:
+    vcr = None
+    get_request_vcr = None
+
+from ddtrace.internal.utils.version import parse_version
+
+
+BOTO_VERSION = parse_version(boto3.__version__)
+
+bedrock_converse_args_with_system_and_tool = {
+    "system": "You are an expert swe that is to use the tool fetch_concept",
+    "user_message": "Explain the concept of distributed tracing in a simple way",
+    "tools": [
+        {
+            "toolSpec": {
+                "name": "fetch_concept",
+                "description": "Fetch an expert explanation for a concept",
+                "inputSchema": {
+                    "json": {
+                        "type": "object",
+                        "properties": {"concept": {"type": "string", "description": "The concept to explain"}},
+                        "required": ["concept"],
+                    }
+                },
+            }
+        },
+    ],
+}
+
+
+def create_bedrock_converse_request(user_message=None, tools=None, system=None):
+    request_params = {
+        "modelId": "anthropic.claude-3-sonnet-20240229-v1:0",
+        "messages": [{"role": "user", "content": [{"text": user_message}]}],
+        "inferenceConfig": {"temperature": 0.7, "topP": 0.9, "maxTokens": 1000, "stopSequences": []},
+    }
+    if system:
+        request_params["system"] = [{"text": system}]
+    if tools:
+        request_params["toolConfig"] = {"tools": tools}
+    return request_params
+
+
+_MODELS = {
+    "ai21": "ai21.j2-mid-v1",
+    "amazon": "amazon.titan-tg1-large",
+    "anthropic": "anthropic.claude-instant-v1",
+    "anthropic_message": "anthropic.claude-3-sonnet-20240229-v1:0",
+    "cohere": "cohere.command-light-text-v14",
+    "meta": "meta.llama2-13b-chat-v1",
+}
+
+_REQUEST_BODIES = {
+    "ai21": {
+        "prompt": "Explain like I'm a five-year old: what is a neural network?",
+        "temperature": 0.9,
+        "topP": 1.0,
+        "maxTokens": 10,
+        "stopSequences": [],
+    },
+    "amazon": {
+        "inputText": "Command: can you explain what Datadog is to someone not in the tech industry?",
+        "textGenerationConfig": {"maxTokenCount": 50, "stopSequences": [], "temperature": 0, "topP": 0.9},
+    },
+    "anthropic": {
+        "prompt": "\n\nHuman: %s\n\nAssistant: What makes you better than Chat-GPT or LLAMA?",
+        "temperature": 0.9,
+        "top_p": 1,
+        "top_k": 250,
+        "max_tokens_to_sample": 50,
+        "stop_sequences": ["\n\nHuman:"],
+    },
+    "anthropic_message": {
+        "messages": [
+            {
+                "role": "user",
+                "content": [{"type": "text", "text": "summarize the plot to the lord of the rings in a dozen words"}],
+            }
+        ],
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 50,
+        "temperature": 0,
+    },
+    "cohere": {
+        "prompt": "\n\nHuman: %s\n\nAssistant: Can you explain what a LLM chain is?",
+        "temperature": 0.9,
+        "p": 1.0,
+        "k": 0,
+        "max_tokens": 10,
+        "stop_sequences": [],
+        "stream": False,
+        "num_generations": 1,
+    },
+    "meta": {
+        "prompt": "What does 'lorem ipsum' mean?",
+        "temperature": 0.9,
+        "top_p": 1.0,
+        "max_gen_len": 60,
+    },
+}
+
+
+# VCR is used to capture and store network requests made to OpenAI and other APIs.
+# This is done to avoid making real calls to the API which could introduce
+# flakiness and cost.
+# To (re)-generate the cassettes: pass a real API key with
+# {PROVIDER}_API_KEY, delete the old cassettes and re-run the tests.
+# NOTE: be sure to check that the generated cassettes don't contain your
+#       API key. Keys should be redacted by the filter_headers option below.
+# NOTE: that different cassettes have to be used between sync and async
+#       due to this issue: https://github.com/kevin1024/vcrpy/issues/463
+#       between cassettes generated for requests and aiohttp.
+def get_request_vcr():
+    return vcr.VCR(
+        cassette_library_dir=os.path.join(os.path.dirname(__file__), "bedrock_cassettes/"),
+        record_mode="once",
+        match_on=["path"],
+        filter_headers=["authorization", "X-Amz-Security-Token"],
+        # Ignore requests to the agent
+        ignore_localhost=True,
+    )
