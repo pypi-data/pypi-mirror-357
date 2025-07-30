97f9426a59467f542847f8af2cf85fce8288e27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:20 +0000

    Add package py-aiowialon/1.3.5/py_aiowialon-1.3.5.tar.gz

---
+++ packages/py-aiowialon/py_aiowialon-1.3.5.tar.gz/py_aiowialon-1.3.5/aiowialon/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Async Wialon Remote API wrapper for Python 3
+"""
+
+__author__ = "o-murphy"
+__copyright__ = (
+    "Copyright 2013-2016, Gurtam; ",
+    "Copyright 2022 Dmytro Yaroshenko; "
+)
+
+__credits__ = [
+    "Alex Chernetsky",
+    "Aleksey Shmigelski",
+    "Mike Turchunovich",
+    "Dmytro Yaroshenko",
+]
+
+from aiowialon.logger import *
+from aiowialon.utils import *
+from aiowialon.types import *
+from aiowialon.exceptions import *
+from aiowialon.api import *
+from aiowialon.validators import *
+from aiowialon.shortcuts import *
