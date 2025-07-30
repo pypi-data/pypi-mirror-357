eb800f84a5c171f8fb6862fbff7d878a36b57cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:24 +0000

    Add package inngest/0.4.23a2/inngest-0.4.23a2.tar.gz

---
+++ packages/inngest/inngest-0.4.23a2.tar.gz/inngest-0.4.23a2/inngest/_internal/execution_lib/base.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import typing
+
+from .models import (
+    CallResult,
+    Context,
+    FunctionHandlerAsync,
+    FunctionHandlerSync,
+    ReportedStep,
+    ReportedStepSync,
+)
+
+if typing.TYPE_CHECKING:
+    from inngest._internal import client_lib, function, step_lib
+
+
+class BaseExecution(typing.Protocol):
+    version: str
+
+    async def report_step(
+        self,
+        step_info: step_lib.StepInfo,
+    ) -> ReportedStep: ...
+
+    async def run(
+        self,
+        client: client_lib.Inngest,
+        ctx: Context,
+        handler: typing.Union[
+            FunctionHandlerAsync,
+            FunctionHandlerSync,
+        ],
+        fn: function.Function,
+    ) -> CallResult: ...
+
+
+class BaseExecutionSync(typing.Protocol):
+    version: str
+
+    def report_step(
+        self,
+        step_info: step_lib.StepInfo,
+    ) -> ReportedStepSync: ...
+
+    def run(
+        self,
+        client: client_lib.Inngest,
+        ctx: Context,
+        handler: typing.Union[
+            FunctionHandlerAsync,
+            FunctionHandlerSync,
+        ],
+        fn: function.Function,
+    ) -> CallResult: ...
---
+++ packages/inngest/inngest-0.4.23a2.tar.gz/inngest-0.4.23a2/inngest/_internal/execution_lib/utils.py
@@ -0,0 +1,26 @@
+import asyncio
+import inspect
+import typing
+
+import typing_extensions
+
+from ..execution_lib.models import FunctionHandlerAsync, FunctionHandlerSync
+
+
+def is_function_handler_async(
+    value: typing.Union[FunctionHandlerAsync, FunctionHandlerSync],
+) -> typing_extensions.TypeGuard[FunctionHandlerAsync]:
+    return inspect.iscoroutinefunction(value)
+
+
+def is_function_handler_sync(
+    value: typing.Union[FunctionHandlerAsync, FunctionHandlerSync],
+) -> typing_extensions.TypeGuard[FunctionHandlerSync]:
+    return not inspect.iscoroutinefunction(value)
+
+
+async def wait_for_next_loop() -> None:
+    loop = asyncio.get_event_loop()
+    fut = asyncio.Future[None]()
+    loop.call_soon(lambda: fut.set_result(None))
+    await fut
