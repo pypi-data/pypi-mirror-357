04f0315dc7ab0504706a14f49d4f2fdcd06f90c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:43 +0000

    Add package ramifice/0.3.34/ramifice-0.3.34-py3-none-any.whl

---
+++ packages/ramifice/ramifice-0.3.34-py3-none-any.whl/ramifice/fields/general/text_group.py
@@ -0,0 +1,28 @@
+"""General additional parameters for text fields."""
+
+
+class TextGroup:
+    """General additional parameters for text fields.
+
+    Attributes:
+        input_type -- Input type for a web form field.
+        placeholder -- Displays prompt text.
+        required -- Required field.
+        readonly -- Specifies that the field cannot be modified by the user.
+        unique -- The unique value of a field in a collection.
+    """
+
+    def __init__(  # noqa: D107
+        self,
+        input_type: str = "",
+        placeholder: str = "",
+        required: bool = False,
+        readonly: bool = False,
+        unique: bool = False,
+    ):
+        self.input_type = input_type
+        self.value: str | None = None
+        self.placeholder = placeholder
+        self.required = required
+        self.readonly = readonly
+        self.unique = unique
