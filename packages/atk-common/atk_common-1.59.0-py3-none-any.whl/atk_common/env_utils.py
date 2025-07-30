a131e65577dad02a8b45f121449c4c79f00360cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:44 +0000

    Add package atk-common/1.59.0/atk_common-1.59.0-py3-none-any.whl

---
+++ packages/atk-common/atk_common-1.59.0-py3-none-any.whl/atk_common/env_utils.py
@@ -0,0 +1,13 @@
+import os
+from atk_common.log_utils import add_log_item
+
+def get_env_value(key, default_value):
+    try:
+        if default_value is None:
+            return None
+        val = os.environ[key]
+        add_log_item(key + ':' + val)
+        return val
+    except (Exception) as error:
+        add_log_item(key + ':' + default_value)
+        return default_value
