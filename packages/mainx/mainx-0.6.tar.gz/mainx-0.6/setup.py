5aff4dbe69b1d642e435dcba7e558c23c04d0b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:47 +0000

    Add package mainx/0.6/mainx-0.6.tar.gz

---
+++ packages/mainx/mainx-0.6.tar.gz/mainx-0.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mainx",
+    version="0.6",  # 🔁 Bump version every upload!
+    py_modules=["main1", "runner", "safe_runner"],
+    entry_points={
+        "console_scripts": [
+            "mainx = runner:main",
+            "mainx2 = safe_runner:main",
+        ],
+    },
+    author="Your Name",
+    description="Educational bug simulation and system info tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",                      # ✅ network support
+        'pywin32; platform_system=="Windows"'  # ✅ only on Windows
+    ],
+)
