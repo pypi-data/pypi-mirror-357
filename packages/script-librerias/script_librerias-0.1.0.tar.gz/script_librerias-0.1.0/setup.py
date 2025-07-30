81fa58362670b8fc7db81fd5ad42119f850b19bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:31 +0000

    Add package script-librerias/0.1.0/script_librerias-0.1.0.tar.gz

---
+++ packages/script-librerias/script_librerias-0.1.0.tar.gz/script_librerias-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="script_librerias",
+    version="0.1.0",
+    author="BI Minor Hotels",
+    author_email="b.intelligence@minor-hotels.com",
+    description="Repository containing the Minor BI team's Python library, featuring features we use every day.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/nhbigithub/Python_Library",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
