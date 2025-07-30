f4a23ba296fff039fe65ede07fb0302a0c3fa7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:57 +0000

    Add package sais/0.3.7/sais-0.3.7.tar.gz

---
+++ packages/sais/sais-0.3.7.tar.gz/sais-0.3.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sais',
+    version='0.3.7',
+    packages=find_packages(),
+    description='Symbiotic Artificial Immune Systems',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Junhao',
+    author_email='junhao.song23@imperial.ac.uk',
+    url='https://github.com/Rqcker/SymbioticAIS',
+    install_requires=[
+        # requires list and version
+        'numpy>=1.23.5'
+    ]
+)
