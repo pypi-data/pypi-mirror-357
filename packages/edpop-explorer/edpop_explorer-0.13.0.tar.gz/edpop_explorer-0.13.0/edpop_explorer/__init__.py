a831bac0e3ddd36549d8864096094dea874d1627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:19 +0000

    Add package edpop-explorer/0.13.0/edpop_explorer-0.13.0.tar.gz

---
+++ packages/edpop-explorer/edpop_explorer-0.13.0.tar.gz/edpop_explorer-0.13.0/edpop_explorer/__init__.py
@@ -0,0 +1,36 @@
+__all__ = [
+    'EDPOPREC', 'RELATORS', 'bind_common_namespaces',
+    'Field', 'FieldError', 'LocationField', 'DigitizationField',
+    'Reader', 'ReaderError', 'NotFoundError',
+    'GetByIdBasedOnQueryMixin', 'DatabaseFileMixin',
+    'BasePreparedQuery', 'PreparedQueryType',
+    'Record', 'RawData', 'RecordError', 'BibliographicalRecord',
+    'BiographicalRecord', 'LazyRecordMixin',
+    'SRUReader', 'CERLReader',
+    'Marc21Data', 'Marc21Field', 'Marc21BibliographicalRecord',
+    'Marc21DataMixin', 'SRUMarc21Reader', 'SRUMarc21BibliographicalReader',
+    'BIBLIOGRAPHICAL', 'BIOGRAPHICAL'
+]
+
+# Define here to avoid circular imports
+# ruff: noqa
+BIBLIOGRAPHICAL = "bibliographical"
+BIOGRAPHICAL = "biographical"
+
+from .rdf import EDPOPREC, RELATORS, bind_common_namespaces
+from .fields import Field, FieldError, LocationField, DigitizationField
+from .reader import (
+    Reader, ReaderError, GetByIdBasedOnQueryMixin, BasePreparedQuery,
+    PreparedQueryType, NotFoundError, DatabaseFileMixin
+)
+from .record import (
+    Record, RawData, RecordError, BibliographicalRecord, BiographicalRecord,
+    LazyRecordMixin
+)
+from .srureader import SRUReader
+from .srumarc21reader import (
+    Marc21Data, Marc21Field, Marc21BibliographicalRecord, Marc21DataMixin,
+    SRUMarc21Reader, SRUMarc21BibliographicalReader
+)
+from .cerl import CERLReader
+
