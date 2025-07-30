4fdb57716b4b0bfdb3177ea237a98e54f799de51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:45 +0000

    Add package apek/0.0.4a1/apek-0.0.4a1.tar.gz

---
+++ packages/apek/apek-0.0.4a1.tar.gz/apek-0.0.4a1/apek/meta.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+This module defines the package's metadata.
+
+Constants:
+    version (str): The package version number.
+"""
+
+
+
+# 小段的文本直接定义，大段的文本要从_text模块导入。
+from . import _text
+# 版本号。
+version = "0.0.4a1"
+# 作者。
+author = "apeking1819"
+# 许可证。
+lic = _text.lic
