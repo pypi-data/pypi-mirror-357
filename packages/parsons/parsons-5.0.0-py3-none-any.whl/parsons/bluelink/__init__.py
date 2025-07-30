a3effb08c08cc2483609be2146a8395e6bf9705b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:11 +0000

    Add package parsons/5.0.0/parsons-5.0.0-py3-none-any.whl

---
+++ packages/parsons/parsons-5.0.0-py3-none-any.whl/parsons/bluelink/__init__.py
@@ -0,0 +1,21 @@
+from parsons.bluelink.bluelink import Bluelink
+from parsons.bluelink.person import (
+    BluelinkPerson,
+    BluelinkEmail,
+    BluelinkAddress,
+    BluelinkPhone,
+    BluelinkIdentifier,
+    BluelinkTag,
+    BluelinkScore,
+)
+
+__all__ = [
+    "Bluelink",
+    "BluelinkPerson",
+    "BluelinkEmail",
+    "BluelinkAddress",
+    "BluelinkPhone",
+    "BluelinkIdentifier",
+    "BluelinkTag",
+    "BluelinkScore",
+]
