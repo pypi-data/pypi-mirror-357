0c8e50617c9c402f6b01f3cd6476d4ce59ca7155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:57 +0000

    Add package xhscreator-data-collection/1.0.2/xhscreator_data_collection-1.0.2-py3-none-any.whl

---
+++ packages/xhscreator-data-collection/xhscreator_data_collection-1.0.2-py3-none-any.whl/XhscreatorDataCollection/note_manager/_dict.py
@@ -0,0 +1,12 @@
+class NoteManagerDict:
+    note__detail = {
+        '发布时间': 'time',
+        '评论量': 'comments_count',
+        '笔记ID': 'id',
+        '收藏量': 'collected_count',
+        '笔记标题': 'display_title',
+        '分享量': 'shared_count',
+        '浏览量': 'view_count',
+        '点赞量': 'likes',
+        'xsec_token': 'xsec_token',
+    }
