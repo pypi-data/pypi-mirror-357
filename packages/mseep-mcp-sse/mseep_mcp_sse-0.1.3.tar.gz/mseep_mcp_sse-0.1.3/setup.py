712b1e02debabd2912c54d6b8f6428b84de7a205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:32 +0000

    Add package mseep-mcp-sse/0.1.3/mseep_mcp_sse-0.1.3.tar.gz

---
+++ packages/mseep-mcp-sse/mseep_mcp_sse-0.1.3.tar.gz/mseep_mcp_sse-0.1.3/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-mcp-sse",
+    version="0.1.3",
+    description="SSE-based MCP Server and Client",
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    maintainer="mseep",
+    maintainer_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['anthropic>=0.45.1', 'argparse>=1.4.0', 'httpx>=0.28.1', 'mcp[cli]>=1.2.1', 'python-dotenv>=1.0.1'],
+    keywords=["mseep"] + [],
+)
