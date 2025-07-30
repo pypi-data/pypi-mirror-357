113f81d1b8435b4a69f5f278fcf407b02c89f0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:06 +0000

    Add package square-authentication/7.0.0/square_authentication-7.0.0-py3-none-any.whl

---
+++ packages/square-authentication/square_authentication-7.0.0-py3-none-any.whl/square_authentication/pydantic_models/core.py
@@ -0,0 +1,47 @@
+from enum import Enum
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class RegisterUsernameV0(BaseModel):
+    username: str
+    password: str
+    app_id: Optional[int] = None
+
+
+class LoginUsernameV0(BaseModel):
+    username: str
+    password: str
+    app_id: int
+    assign_app_id_if_missing: bool = False
+
+
+class DeleteUserV0(BaseModel):
+    password: str
+
+
+class UpdatePasswordV0(BaseModel):
+    old_password: str
+    new_password: str
+
+
+class TokenType(Enum):
+    access_token = "access_token"
+    refresh_token = "refresh_token"
+
+
+class LogoutAppsV0(BaseModel):
+    app_ids: List[int]
+
+
+class ResetPasswordAndLoginUsingBackupCodeV0(BaseModel):
+    backup_code: str
+    username: str
+    new_password: str
+    app_id: int
+
+
+class SendResetPasswordEmailV0(BaseModel):
+    username: str
+    app_id: int
