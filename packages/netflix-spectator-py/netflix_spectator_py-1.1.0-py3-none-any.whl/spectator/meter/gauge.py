418d1d7f9f99254e389c24ed9b53d3b2324d7efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:19 +0000

    Add package netflix-spectator-py/1.1.0/netflix_spectator_py-1.1.0-py3-none-any.whl

---
+++ packages/netflix-spectator-py/netflix_spectator_py-1.1.0-py3-none-any.whl/spectator/meter/gauge.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from spectator.meter import Meter
+from spectator.meter.meter_id import MeterId
+from spectator.writer.new_writer import new_writer, WriterUnion
+
+
+class Gauge(Meter):
+    """The value is a number that was sampled at a point in time. The default time-to-live (TTL)
+    for gauges is 900 seconds (15 minutes) - they will continue reporting the last value set for
+    this duration of time. An optional ttl_seconds may be set to control the lifespan of these
+    values. SpectatorD enforces a minimum TTL of 5 seconds."""
+
+    def __init__(self, meter_id: MeterId, writer: Optional[WriterUnion] = None,
+                 ttl_seconds: Optional[int] = None) -> None:
+        if writer is None:
+            writer = new_writer("none")
+
+        if ttl_seconds is None:
+            meter_type_symbol = "g"
+        else:
+            meter_type_symbol = f"g,{ttl_seconds}"
+
+        super().__init__(meter_id, writer, meter_type_symbol)
+
+    def set(self, value: float) -> None:
+        line = f"{self._meter_type_symbol}:{self._id.spectatord_id}:{value}"
+        self._writer.write(line)
