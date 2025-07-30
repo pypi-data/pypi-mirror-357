542947cb8ed7f9446ac7e3c6911ba8cf2e5dec44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:44 +0000

    Add package flask-iiif/1.2.1/flask_iiif-1.2.1.tar.gz

---
+++ packages/flask-iiif/flask_iiif-1.2.1.tar.gz/flask_iiif-1.2.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Flask-IIIF
+# Copyright (C) 2014, 2015, 2016, 2017 CERN.
+# Copyright (C) 2020 data-futures.
+# Copyright (C) 2022 Graz University of Technology.
+#
+# Flask-IIIF is free software; you can redistribute it and/or modify
+# it under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+"""Flask-IIIF extension provides easy IIIF API standard integration."""
+
+from setuptools import setup
+
+setup()
