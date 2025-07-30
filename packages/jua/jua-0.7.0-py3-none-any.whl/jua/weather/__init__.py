f52752fc9de7780805548dfb2c08fec7a5234501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:40 +0000

    Add package jua/0.7.0/jua-0.7.0-py3-none-any.whl

---
+++ packages/jua/jua-0.7.0-py3-none-any.whl/jua/weather/__init__.py
@@ -0,0 +1,7 @@
+from jua.weather._jua_dataset import JuaDataset
+from jua.weather._model import Model
+from jua.weather._weather import Weather
+from jua.weather.models import Models
+from jua.weather.variables import Variables
+
+__all__ = ["Model", "Models", "Variables", "JuaDataset", "Weather"]
