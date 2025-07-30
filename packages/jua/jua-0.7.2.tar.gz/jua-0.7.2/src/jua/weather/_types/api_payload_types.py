17feb26de8182b9b6020cc8ffd73ac5c28c9fd77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:59 +0000

    Add package jua/0.7.2/jua-0.7.2.tar.gz

---
+++ packages/jua/jua-0.7.2.tar.gz/jua-0.7.2/src/jua/weather/_types/api_payload_types.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+from jua.types.geo import LatLon
+
+
+class ForecastRequestPayload(BaseModel):
+    points: list[LatLon] | None = None
+    min_lead_time: int = 0
+    max_lead_time: int = 0
+    variables: list[str] | None = None
+    full: bool = False
