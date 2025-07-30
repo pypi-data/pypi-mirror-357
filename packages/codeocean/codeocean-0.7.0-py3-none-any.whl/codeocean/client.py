81c16805934b4fb4f5992f1b05084d2915b0be49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:24 +0000

    Add package codeocean/0.7.0/codeocean-0.7.0-py3-none-any.whl

---
+++ packages/codeocean/codeocean-0.7.0-py3-none-any.whl/codeocean/client.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from requests_toolbelt.adapters.socket_options import TCPKeepAliveAdapter
+from requests_toolbelt.sessions import BaseUrlSession
+from typing import Optional
+from urllib3.util import Retry
+
+from codeocean.capsule import Capsules
+from codeocean.computation import Computations
+from codeocean.data_asset import DataAssets
+
+
+@dataclass
+class CodeOcean:
+
+    domain: str
+    token: str
+    retries: Optional[Retry | int] = 0
+
+    def __post_init__(self):
+        self.session = BaseUrlSession(base_url=f"{self.domain}/api/v1/")
+        self.session.auth = (self.token, "")
+        self.session.headers.update({"Content-Type": "application/json"})
+        self.session.hooks["response"] = [
+            lambda response, *args, **kwargs: response.raise_for_status()
+        ]
+        self.session.mount(self.domain, TCPKeepAliveAdapter(max_retries=self.retries))
+
+        self.capsules = Capsules(client=self.session)
+        self.computations = Computations(client=self.session)
+        self.data_assets = DataAssets(client=self.session)
