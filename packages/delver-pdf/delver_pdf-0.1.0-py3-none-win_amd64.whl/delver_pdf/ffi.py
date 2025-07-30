32c0f0dd9c22b8dc7cbed808aa7de26d57404bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:15 +0000

    Add package delver-pdf/0.1.0/delver_pdf-0.1.0-py3-none-win_amd64.whl

---
+++ packages/delver-pdf/delver_pdf-0.1.0-py3-none-win_amd64.whl/delver_pdf/ffi.py
@@ -0,0 +1,9 @@
+# auto-generated file
+import _cffi_backend
+
+ffi = _cffi_backend.FFI('ffi',
+    _version = 0x2601,
+    _types = b'\x00\x00\x00\x09',
+    _struct_unions = ((b'\x00\x00\x00\x00\x00\x00\x00\x10Matrix',),),
+    _typenames = (b'\x00\x00\x00\x00Matrix',),
+)
