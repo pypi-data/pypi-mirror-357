1ad4572ba72ca23d1fe4d7666ae4e290e7e05bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:34 +0000

    Add package olarmconnect/1.0.0/olarmconnect-1.0.0-py3-none-any.whl

---
+++ packages/olarmconnect/olarmconnect-1.0.0-py3-none-any.whl/olarmconnect/__init__.py
@@ -0,0 +1,5 @@
+"""OlarmConnect - An async Python client for connecting to Olarm services."""
+
+from .olarmconnect import OlarmConnectApiError, OlarmConnect
+
+__all__ = ["OlarmConnectApiError", "OlarmConnect"]
