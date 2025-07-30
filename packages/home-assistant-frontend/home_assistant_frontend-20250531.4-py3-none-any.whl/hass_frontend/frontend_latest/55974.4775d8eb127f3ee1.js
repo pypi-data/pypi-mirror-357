63f966d10155a9785775b8482b514217a969d483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:01 +0000

    Add package home-assistant-frontend/20250531.4/home_assistant_frontend-20250531.4-py3-none-any.whl

---
+++ packages/home-assistant-frontend/home_assistant_frontend-20250531.4-py3-none-any.whl/hass_frontend/frontend_latest/55974.4775d8eb127f3ee1.js
@@ -0,0 +1,2 @@
+export const __webpack_ids__=["55974"];export const __webpack_modules__={21940:function(o,t,e){e.r(t),e.d(t,{HaIconPrev:()=>n});var c=e(73742),r=e(26292),_=e(83054),a=e(993);class n extends a.HaSvgIcon{constructor(...o){super(...o),this.path="rtl"===_.mainWindow.document.dir?"M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z":"M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"}}(0,c.__decorate)([(0,r.Cb)()],n.prototype,"path",void 0),n=(0,c.__decorate)([(0,r.Mo)("ha-icon-prev")],n)}};
+//# sourceMappingURL=55974.4775d8eb127f3ee1.js.map
