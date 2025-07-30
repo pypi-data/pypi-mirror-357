fda4c9a8b9c96629f73ba2e6c93b7cb344a2c920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:04 +0000

    Add package flytekitplugins-ray/1.14.9/flytekitplugins_ray-1.14.9.tar.gz

---
+++ packages/flytekitplugins-ray/flytekitplugins_ray-1.14.9.tar.gz/flytekitplugins_ray-1.14.9/flytekitplugins/ray/task.py
@@ -0,0 +1,121 @@
+import base64
+import json
+import os
+import typing
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, Optional
+
+import yaml
+from flytekitplugins.ray.models import (
+    HeadGroupSpec,
+    RayCluster,
+    RayJob,
+    WorkerGroupSpec,
+)
+from google.protobuf.json_format import MessageToDict
+
+from flytekit import lazy_module
+from flytekit.configuration import SerializationSettings
+from flytekit.core.context_manager import ExecutionParameters, FlyteContextManager
+from flytekit.core.python_function_task import PythonFunctionTask
+from flytekit.extend import TaskPlugins
+from flytekit.models.task import K8sPod
+
+ray = lazy_module("ray")
+
+
+@dataclass
+class HeadNodeConfig:
+    ray_start_params: typing.Optional[typing.Dict[str, str]] = None
+    k8s_pod: typing.Optional[K8sPod] = None
+
+
+@dataclass
+class WorkerNodeConfig:
+    group_name: str
+    replicas: int
+    min_replicas: typing.Optional[int] = None
+    max_replicas: typing.Optional[int] = None
+    ray_start_params: typing.Optional[typing.Dict[str, str]] = None
+    k8s_pod: typing.Optional[K8sPod] = None
+
+
+@dataclass
+class RayJobConfig:
+    worker_node_config: typing.List[WorkerNodeConfig]
+    head_node_config: typing.Optional[HeadNodeConfig] = None
+    enable_autoscaling: bool = False
+    runtime_env: typing.Optional[dict] = None
+    address: typing.Optional[str] = None
+    shutdown_after_job_finishes: bool = False
+    ttl_seconds_after_finished: typing.Optional[int] = None
+
+
+class RayFunctionTask(PythonFunctionTask):
+    """
+    Actual Plugin that transforms the local python code for execution within Ray job.
+    """
+
+    _RAY_TASK_TYPE = "ray"
+
+    def __init__(self, task_config: RayJobConfig, task_function: Callable, **kwargs):
+        super().__init__(
+            task_config=task_config,
+            task_type=self._RAY_TASK_TYPE,
+            task_function=task_function,
+            **kwargs,
+        )
+        self._task_config = task_config
+
+    def pre_execute(self, user_params: ExecutionParameters) -> ExecutionParameters:
+        init_params = {"address": self._task_config.address}
+
+        ctx = FlyteContextManager.current_context()
+        if not ctx.execution_state.is_local_execution():
+            working_dir = os.getcwd()
+            init_params["runtime_env"] = {
+                "working_dir": working_dir,
+                "excludes": ["script_mode.tar.gz", "fast*.tar.gz"],
+            }
+
+        ray.init(**init_params)
+        return user_params
+
+    def get_custom(self, settings: SerializationSettings) -> Optional[Dict[str, Any]]:
+        cfg = self._task_config
+
+        # Deprecated: runtime_env is removed KubeRay >= 1.1.0. It is replaced by runtime_env_yaml
+        runtime_env = base64.b64encode(json.dumps(cfg.runtime_env).encode()).decode() if cfg.runtime_env else None
+
+        runtime_env_yaml = yaml.dump(cfg.runtime_env) if cfg.runtime_env else None
+
+        ray_job = RayJob(
+            ray_cluster=RayCluster(
+                head_group_spec=(
+                    HeadGroupSpec(cfg.head_node_config.ray_start_params, cfg.head_node_config.k8s_pod)
+                    if cfg.head_node_config
+                    else None
+                ),
+                worker_group_spec=[
+                    WorkerGroupSpec(
+                        c.group_name,
+                        c.replicas,
+                        c.min_replicas,
+                        c.max_replicas,
+                        c.ray_start_params,
+                        c.k8s_pod,
+                    )
+                    for c in cfg.worker_node_config
+                ],
+                enable_autoscaling=(cfg.enable_autoscaling if cfg.enable_autoscaling else False),
+            ),
+            runtime_env=runtime_env,
+            runtime_env_yaml=runtime_env_yaml,
+            ttl_seconds_after_finished=cfg.ttl_seconds_after_finished,
+            shutdown_after_job_finishes=cfg.shutdown_after_job_finishes,
+        )
+        return MessageToDict(ray_job.to_flyte_idl())
+
+
+# Inject the Ray plugin into flytekits dynamic plugin loading system
+TaskPlugins.register_pythontask_plugin(RayJobConfig, RayFunctionTask)
---
+++ packages/flytekitplugins-ray/flytekitplugins_ray-1.14.9.tar.gz/flytekitplugins_ray-1.14.9/tests/test_ray.py
@@ -0,0 +1,78 @@
+import base64
+import json
+
+import ray
+import yaml
+from flytekitplugins.ray import HeadNodeConfig
+from flytekitplugins.ray.models import RayCluster, RayJob, WorkerGroupSpec, HeadGroupSpec
+from flytekitplugins.ray.task import RayJobConfig, WorkerNodeConfig
+from google.protobuf.json_format import MessageToDict
+from flytekit.models.task import K8sPod
+
+from flytekit import PythonFunctionTask, task
+from flytekit.configuration import Image, ImageConfig, SerializationSettings
+
+config = RayJobConfig(
+    worker_node_config=[WorkerNodeConfig(group_name="test_group", replicas=3, min_replicas=0, max_replicas=10, k8s_pod=K8sPod(pod_spec={"str": "worker", "int": 1}))],
+    head_node_config=HeadNodeConfig(k8s_pod=K8sPod(pod_spec={"str": "head", "int": 2})),
+    runtime_env={"pip": ["numpy"]},
+    enable_autoscaling=True,
+    shutdown_after_job_finishes=True,
+    ttl_seconds_after_finished=20,
+)
+
+
+def test_ray_task():
+    @task(task_config=config)
+    def t1(a: int) -> str:
+        assert ray.is_initialized()
+        inc = a + 2
+        return str(inc)
+
+    assert t1.task_config is not None
+    assert t1.task_config == config
+    assert t1.task_type == "ray"
+    assert isinstance(t1, PythonFunctionTask)
+
+    default_img = Image(name="default", fqn="test", tag="tag")
+    settings = SerializationSettings(
+        project="proj",
+        domain="dom",
+        version="123",
+        image_config=ImageConfig(default_image=default_img, images=[default_img]),
+        env={},
+    )
+
+    ray_job_pb = RayJob(
+        ray_cluster=RayCluster(worker_group_spec=[WorkerGroupSpec(group_name="test_group", replicas=3, min_replicas=0, max_replicas=10, k8s_pod=K8sPod(pod_spec={"str": "worker", "int": 1}))], head_group_spec=HeadGroupSpec(k8s_pod=K8sPod(pod_spec={"str": "head", "int": 2})), enable_autoscaling=True),
+        runtime_env=base64.b64encode(json.dumps({"pip": ["numpy"]}).encode()).decode(),
+        runtime_env_yaml=yaml.dump({"pip": ["numpy"]}),
+        shutdown_after_job_finishes=True,
+        ttl_seconds_after_finished=20,
+    ).to_flyte_idl()
+
+    assert t1.get_custom(settings) == MessageToDict(ray_job_pb)
+
+    assert t1.get_command(settings) == [
+        "pyflyte-execute",
+        "--inputs",
+        "{{.input}}",
+        "--output-prefix",
+        "{{.outputPrefix}}",
+        "--raw-output-data-prefix",
+        "{{.rawOutputDataPrefix}}",
+        "--checkpoint-path",
+        "{{.checkpointOutputPrefix}}",
+        "--prev-checkpoint",
+        "{{.prevCheckpointPrefix}}",
+        "--resolver",
+        "flytekit.core.python_auto_container.default_task_resolver",
+        "--",
+        "task-module",
+        "tests.test_ray",
+        "task-name",
+        "t1",
+    ]
+
+    assert t1(a=3) == "5"
+    assert ray.is_initialized()
