9b4fd7f3cf4f8ae432640bd09cea30204a442c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:17 +0000

    Add package positionbt/1.3.2/positionbt-1.3.2.tar.gz

---
+++ packages/positionbt/positionbt-1.3.2.tar.gz/positionbt-1.3.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="positionbt",
+    version="v1.3.2",
+    author="Xuan Ronaldo",
+    author_email="xuanronaldo@outlook.com",
+    description="PositionBT is a simple, fast, and customizable backtesting framework "
+    + "that directly evaluates trading strategies through position data (ranging from -1 to 1).",
+    url="https://github.com/xuanronaldo/positionbt",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={
+        "positionbt.visualization": ["templates/*.html"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        "polars>=1.13.0",
+        "pandas>=2.0.0",
+        "plotly>=5.18.0",
+    ],
+)
