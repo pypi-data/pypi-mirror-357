4a45d3fccc616600ccd06313a313c1b9c997ad9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:03 +0000

    Add package busylib/0.0.1/busylib-0.0.1.tar.gz

---
+++ packages/busylib/busylib-0.0.1.tar.gz/busylib-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='busylib',
+    version='0.0.1',
+    description='Package name reserved',
+    long_description='This package name is reserved for future use',
+    url='https://github.com/yourusername/busylib',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
+
