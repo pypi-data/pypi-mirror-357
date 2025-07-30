a362b780260215933f9babc3d9399843933347b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:08 +0000

    Add package scor/0.1.3/scor-0.1.3.tar.gz

---
+++ packages/scor/scor-0.1.3.tar.gz/scor-0.1.3/README.md
@@ -0,0 +1,10 @@
+# Stochastic Covariance Regularization for Imbalanced Datasets
+
+## imagenet.py
+
+This file is for training models with various loss functions on the ImageNet dataset.
+Code from [here](https://github.com/pytorch/examples/tree/main/imagenet) is adapted 
+to be used with loss functions defined in this repository.
+
+A "loss" option is included to the adapted code. Run ```python imagenet.py --help```
+for a proper description.
---
+++ packages/scor/scor-0.1.3.tar.gz/scor-0.1.3/scor/metrics.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+from itertools import chain
+
+def perturbations(model: nn.Module, epsilon: float = 1e-07):
+    ranks = []
+    max_singulars = []
+    min_singulars = []
+    s: torch.Tensor
+
+    # Model must have all layers in self.sequence
+    for name, param in model.sequence.named_parameters():
+        if "bias" in name:
+            continue
+
+        s = torch.linalg.svdvals(param)
+        ranks.append(torch.sum(s > epsilon).item())
+        max_singulars.append(s[0].item())
+        min_singulars.append(s[-1].item())
+
+    return ranks, max_singulars, min_singulars
+
+def spectral_dist(model: nn.Module, epsilon: float = 1e-07):
+    ranks = []
+    singulars = []
+    s: torch.Tensor
+
+    for name, param in model.sequence.named_parameters():
+        if "bias" in name:
+            continue
+
+        s = torch.linalg.svdvals(param)
+        ranks.append(torch.sum(s > epsilon).item())
+        singulars.append(s.tolist())
+
+    return ranks, list(chain(*singulars))
+
+def perturbationsMultiple(model: nn.Module, epsilon: float = 1e-07):
+    ranks = []
+    max_singulars = []
+    min_singulars = []
+    s: torch.Tensor
+
+    # Model must have all layers in self.sequence
+    for name, param in model.sequence.named_parameters():
+        if "bias" in name or param.ndim < 2:
+            continue
+
+        s = torch.linalg.svdvals(param)
+        ranks.append(torch.sum(s > epsilon).item())
+        max_singulars.append(torch.mean(s[0]).item())
+        min_singulars.append(torch.mean(s[-1]).item())
+
+    return ranks, max_singulars, min_singulars
