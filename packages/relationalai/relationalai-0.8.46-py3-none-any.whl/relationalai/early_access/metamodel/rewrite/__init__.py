2093a6fb3792879a365d77865b42556816de2479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:03 +0000

    Add package relationalai/0.8.46/relationalai-0.8.46-py3-none-any.whl

---
+++ packages/relationalai/relationalai-0.8.46-py3-none-any.whl/relationalai/early_access/metamodel/rewrite/__init__.py
@@ -0,0 +1,5 @@
+from .splinter import Splinter
+from .list_types import RewriteListTypes
+from .gc_nodes import GarbageCollectNodes
+
+__all__ = ["Splinter", "RewriteListTypes", "GarbageCollectNodes"]
