4bacfdb14b68831b8c45c6d74b1f410b372dde5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:32 +0000

    Add package talentro-definitions/0.7.3/talentro_definitions-0.7.3.tar.gz

---
+++ packages/talentro-definitions/talentro_definitions-0.7.3.tar.gz/talentro_definitions-0.7.3/src/talentro/iam/models.py
@@ -0,0 +1,14 @@
+import uuid
+
+from sqlmodel import Field
+
+from ..general.models import IAMModel
+from sqlalchemy import Column, JSON
+
+
+class CompositeRole(IAMModel, table=True):
+    name: str = Field(index=True)
+    roles: list = Field(sa_column=Column(JSON))
+    organization: uuid.UUID = Field(index=True, nullable=True)
+    manageable_roles: list = Field(sa_column=Column(JSON))
+    permission_level: int = Field(default=0)
