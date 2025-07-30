3cfecf40ad65d5e642aff682a19120519116577e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:38 +0000

    Add package pynanolab/3.1.0/pynanolab-3.1.0-cp311-cp311-win_amd64.whl

---
+++ packages/pynanolab/pynanolab-3.1.0-cp311-cp311-win_amd64.whl/pynanolab/apps/signalfrequency/ephys_nlm/__init__.py
@@ -0,0 +1,12 @@
+"""ephys_nlm
+
+Non-local means denoising of multi-channel electrophysiology timeseries using PyTorch.
+
+Authors: Jeremy Magland, Center for Computational Mathematics, Flatiron Institute
+
+Created January 2019
+"""
+
+from .ephys_nlm import ephys_nlm_v1, ephys_nlm_v1_opts
+
+from . import example_datasets
---
+++ packages/pynanolab/pynanolab-3.1.0-cp311-cp311-win_amd64.whl/pynanolab/apps/visualization/pnlfig/base/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env
+# -*- coding: UTF-8 -*-
+# @Author:wnight
+# @FileName:__init__.py
+# @DateTime:2023/2/25 11:35
+# @SoftWare: PyCharm
+# You are not expected to understand this
