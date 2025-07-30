7efa5c7a3de19f66bc2e1864ce73d47b2c6954c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:21:47 +0000

    Add package hojichar/0.13.1/hojichar-0.13.1.tar.gz

---
+++ packages/hojichar/hojichar-0.13.1.tar.gz/hojichar-0.13.1/hojichar/__init__.py
@@ -0,0 +1,36 @@
+"""
+.. include:: ../README.md
+"""
+
+from .core.composition import Compose
+from .core.filter_interface import Filter, TokenFilter
+from .core.inspection import StatsContainer
+from .core.models import Document, Token
+from .core.parallel import Parallel
+from .filters import (
+    deduplication,
+    document_filters,
+    language_identification,
+    token_filters,
+    tokenization,
+)
+
+__version__ = "0.0.0"  # Replaced by uv-dynamic-versioning when deploying
+
+__all__ = [
+    "core",
+    "filters",
+    "utils",
+    "Compose",
+    "Filter",
+    "TokenFilter",
+    "Document",
+    "Token",
+    "Parallel",
+    "StatsContainer",
+    "deduplication",
+    "document_filters",
+    "language_identification",
+    "token_filters",
+    "tokenization",
+]
