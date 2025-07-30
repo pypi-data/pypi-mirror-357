a99ecaf77a3289d6632b4ba80d44735d16706d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:09 +0000

    Add package autosynthesis/0.0.24/autosynthesis-0.0.24.tar.gz

---
+++ packages/autosynthesis/autosynthesis-0.0.24.tar.gz/autosynthesis-0.0.24/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 20:30:00 2022
+
+@author: sarangbhagwat
+"""
+
+from setuptools import setup
+
+setup(
+    name='autosynthesis',
+    packages=['autosynthesis'],
+    version='0.0.24',    
+    description='A toolkit for automated synthesis of biorefinery processes including pretreatment, conversion, separation, and upgrading.',
+    url='https://github.com/sarangbhagwat/autosynthesis',
+    author='Sarang S. Bhagwat',
+    author_email='sarang.bhagwat.git@gmail.com',
+    license='MIT',
+    # packages=['contourplots'],
+    install_requires=[
+                      'biosteam>=2.33.2',
+                      'xlsxwriter',
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: University of Illinois/NCSA Open Source License',  
+        'Operating System :: Microsoft :: Windows',        
+        'Programming Language :: Python :: 3.8'
+    ],
+)
