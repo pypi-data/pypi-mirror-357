7466f74bb9c7756b2ea9f1c022e38a375402a4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:21 +0000

    Add package langchain-couchbase/0.4.0/langchain_couchbase-0.4.0-py3-none-any.whl

---
+++ packages/langchain-couchbase/langchain_couchbase-0.4.0-py3-none-any.whl/langchain_couchbase/vectorstores/__init__.py
@@ -0,0 +1,9 @@
+from langchain_couchbase.vectorstores.search_vector_store import (
+    CouchbaseSearchVectorStore,
+)
+from langchain_couchbase.vectorstores.vectorstores import CouchbaseVectorStore
+
+__all__ = [
+    "CouchbaseSearchVectorStore",
+    "CouchbaseVectorStore",
+]
