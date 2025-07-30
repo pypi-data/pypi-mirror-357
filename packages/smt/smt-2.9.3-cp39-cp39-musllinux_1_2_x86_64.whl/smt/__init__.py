1b3bda077327d5eb66073951d8c2c8d074729ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:33 +0000

    Add package smt/2.9.3/smt-2.9.3-cp39-cp39-musllinux_1_2_x86_64.whl

---
+++ packages/smt/smt-2.9.3-cp39-cp39-musllinux_1_2_x86_64.whl/smt/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "2.9.3"
+
+__all__ = [
+    "surrogate_models",
+    "kernels",
+    "design_space",
+    "applications",
+    "examples",
+    "sampling_methods",
+    "utils",
+    "tests",
+    "src",
+    "problems",
+]
