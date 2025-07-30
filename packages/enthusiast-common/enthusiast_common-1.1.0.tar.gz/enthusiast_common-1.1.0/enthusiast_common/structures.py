166b34b96faef938316b13dbb484cf0aef91369c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:23 +0000

    Add package enthusiast-common/1.1.0/enthusiast_common-1.1.0.tar.gz

---
+++ packages/enthusiast-common/enthusiast_common-1.1.0.tar.gz/enthusiast_common-1.1.0/enthusiast_common/structures.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ProductDetails:
+    entry_id: str
+    name: str
+    slug: str
+    description: str
+    sku: str
+    properties: str
+    categories: str
+    price: float
+
+
+@dataclass
+class DocumentDetails:
+    url: str
+    title: str
+    content: str
