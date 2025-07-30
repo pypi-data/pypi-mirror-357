eda52faee6b7d4027bd04ae11e61b69f3b56bbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:25 +0000

    Add package mseep-mcp-server-vegalite/0.0.4/mseep_mcp_server_vegalite-0.0.4.tar.gz

---
+++ packages/mseep-mcp-server-vegalite/mseep_mcp_server_vegalite-0.0.4.tar.gz/mseep_mcp_server_vegalite-0.0.4/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-mcp-server-vegalite",
+    version="0.0.4",
+    description="A simple Data Visualization MCP server using Vega-Lite",
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    maintainer="mseep",
+    maintainer_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.0.0', 'vl-convert-python'],
+    keywords=["mseep"] + [],
+)
