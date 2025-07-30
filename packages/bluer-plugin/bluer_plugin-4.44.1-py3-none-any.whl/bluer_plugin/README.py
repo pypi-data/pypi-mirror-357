593499b110029269b9a12fe74427630e17f7ec1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:42 +0000

    Add package bluer-plugin/4.44.1/bluer_plugin-4.44.1-py3-none-any.whl

---
+++ packages/bluer-plugin/bluer_plugin-4.44.1-py3-none-any.whl/bluer_plugin/README.py
@@ -0,0 +1,29 @@
+import os
+
+from bluer_objects import file, README
+
+from bluer_plugin import NAME, VERSION, ICON, REPO_NAME
+
+
+items = README.Items(
+    [
+        {
+            "name": f"feature {index}",
+            "marquee": "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true",
+            "description": f"description of feature {index} ...",
+            "url": "#",
+        }
+        for index in range(1, 4)
+    ]
+)
+
+
+def build():
+    return README.build(
+        items=items,
+        path=os.path.join(file.path(__file__), ".."),
+        ICON=ICON,
+        NAME=NAME,
+        VERSION=VERSION,
+        REPO_NAME=REPO_NAME,
+    )
