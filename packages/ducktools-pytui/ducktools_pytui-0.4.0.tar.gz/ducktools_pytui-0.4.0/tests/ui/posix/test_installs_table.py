f16d1df6df4ece2d121ece26c7348229db4ceba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:40 +0000

    Add package ducktools-pytui/0.4.0/ducktools_pytui-0.4.0.tar.gz

---
+++ packages/ducktools-pytui/ducktools_pytui-0.4.0.tar.gz/ducktools_pytui-0.4.0/tests/ui/posix/test_installs_table.py
@@ -0,0 +1,30 @@
+import sys
+from operator import itemgetter
+from unittest.mock import patch
+
+import pytest
+
+from ducktools.pytui.ui import ManagerApp, MANAGED_BY_MAPPING, substitute_home
+
+from textual.worker import WorkerFailed
+
+
+@patch("ducktools.pytui.ui.HOME", "/home/ducksual")
+@pytest.mark.flaky(reruns=5)
+async def test_runtime_table(runtimes):
+    app = ManagerApp()
+    async with app.run_test() as pilot:
+        row_data = [app._runtime_table.get_row_at(i) for i in range(0, app._runtime_table.row_count)]
+        row_data.sort(key=itemgetter(3))  # Sort by Path
+
+        expected = [
+            [
+                py.version_str if py.implementation_version_str == py.version_str else f"{py.version_str} / {py.implementation_version_str}",
+                MANAGED_BY_MAPPING.get(py.managed_by, py.managed_by),
+                py.implementation,
+                substitute_home(py.executable)
+            ]
+            for py in runtimes
+        ]
+        expected.sort(key=itemgetter(3))
+        assert row_data == expected
