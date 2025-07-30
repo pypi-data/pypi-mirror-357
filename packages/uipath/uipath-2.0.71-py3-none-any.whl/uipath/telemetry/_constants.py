8d7f24c8c6d3efae181311e28b04980a2cfe1903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:04 +0000

    Add package uipath/2.0.71/uipath-2.0.71-py3-none-any.whl

---
+++ packages/uipath/uipath-2.0.71-py3-none-any.whl/uipath/telemetry/_constants.py
@@ -0,0 +1,16 @@
+_CONNECTION_STRING = "InstrumentationKey=0d8dca8b-9a76-4f2e-adeb-09d9e2c210a0;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=ff68f440-0d82-4bab-9192-ad91f67e0835"
+
+_APP_INSIGHTS_EVENT_MARKER_ATTRIBUTE = "APPLICATION_INSIGHTS_EVENT_MARKER_ATTRIBUTE"
+_OTEL_RESOURCE_ATTRIBUTES = "OTEL_RESOURCE_ATTRIBUTES"
+_SDK_VERSION = "SdkVersion"
+
+_CODE_FILEPATH = "code.filepath"
+_CODE_FUNCTION = "code.function"
+_CODE_LINENO = "code.lineno"
+
+_CLOUD_ORG_ID = "CloudOrganizationId"
+_CLOUD_TENANT_ID = "CloudTenantId"
+_CLOUD_URL = "CloudUrl"
+_APP_NAME = "ApplicationName"
+
+_UNKNOWN = ""
