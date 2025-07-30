628d8efc68fad4e69af449d3921e0238cf42d2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:08 +0000

    Add package riemannax/0.0.2/riemannax-0.0.2-py3-none-any.whl

---
+++ packages/riemannax/riemannax-0.0.2-py3-none-any.whl/manifolds/__init__.py
@@ -0,0 +1,17 @@
+"""Riemannian manifold implementations for optimization."""
+
+from .base import DimensionError, Manifold, ManifoldError
+from .grassmann import Grassmann
+from .so import SpecialOrthogonal
+from .sphere import Sphere
+from .stiefel import Stiefel
+
+__all__ = [
+    "DimensionError",
+    "Grassmann",
+    "Manifold",
+    "ManifoldError",
+    "SpecialOrthogonal",
+    "Sphere",
+    "Stiefel",
+]
