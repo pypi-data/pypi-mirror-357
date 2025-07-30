7ebd52f16ab0a71ba01254e6b71874543ee917df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:38 +0000

    Add package inventory-monitor/10.3.5/inventory-monitor-10.3.5.tar.gz

---
+++ packages/inventory-monitor/inventory-monitor-10.3.5.tar.gz/inventory-monitor-10.3.5/inventory_monitor/navigation.py
@@ -0,0 +1,70 @@
+from netbox.navigation import get_model_item
+from netbox.plugins import PluginMenu, PluginMenuItem
+
+menu = PluginMenu(
+    label="Inventory Monitor",
+    icon_class="mdi mdi-monitor",
+    groups=(
+        (
+            "Assets",
+            (
+                get_model_item("plugins:inventory_monitor", "asset", "Assets"),
+                PluginMenuItem(
+                    link="plugins:inventory_monitor:assettype_list",
+                    link_text="Asset Types",
+                    permissions=["inventory_monitor.view_assettype"],
+                ),
+                PluginMenuItem(
+                    link="plugins:inventory_monitor:rma_list",
+                    link_text="RMA",
+                    permissions=["inventory_monitor.view_rma"],
+                ),
+                PluginMenuItem(
+                    link="plugins:inventory_monitor:abra_list",
+                    link_text="ABRA",
+                    permissions=["inventory_monitor.view_abra"],
+                ),
+                PluginMenuItem(
+                    link="plugins:inventory_monitor:assetservice_list",
+                    link_text="Services",
+                    permissions=["inventory_monitor.view_assetservice"],
+                ),
+            ),
+        ),
+        (
+            "Network Probe",
+            (
+                PluginMenuItem(
+                    link="plugins:inventory_monitor:probe_list",
+                    link_text="Probes",
+                    permissions=["inventory_monitor.view_probe"],
+                ),
+                PluginMenuItem(
+                    link="plugins:inventory_monitor:probediff",
+                    link_text="Network Changes",
+                    permissions=["inventory_monitor.view_probediff"],
+                ),
+            ),
+        ),
+        (
+            "Contracts",
+            (
+                PluginMenuItem(
+                    link="plugins:inventory_monitor:contractor_list",
+                    link_text="Contractors",
+                    permissions=["inventory_monitor.view_contractor"],
+                ),
+                PluginMenuItem(
+                    link="plugins:inventory_monitor:contract_list",
+                    link_text="Contracts",
+                    permissions=["inventory_monitor.view_contract"],
+                ),
+                PluginMenuItem(
+                    link="plugins:inventory_monitor:invoice_list",
+                    link_text="Invoices",
+                    permissions=["inventory_monitor.view_invoice"],
+                ),
+            ),
+        ),
+    ),
+)
