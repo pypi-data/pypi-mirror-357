79cb6097ca2b853f1f0ded709d1c18aa7a751db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:09 +0000

    Add package python-openapi/0.2.0/python_openapi-0.2.0-py3-none-any.whl

---
+++ packages/python-openapi/python_openapi-0.2.0-py3-none-any.whl/pyopenapi/__init__.py
@@ -0,0 +1,14 @@
+"""
+Generate an OpenAPI specification from a Python class definition
+
+Copyright 2022-2025, Levente Hunyadi
+
+:see: https://github.com/hunyadi/pyopenapi
+"""
+
+__version__ = "0.2.0"
+__author__ = "Levente Hunyadi"
+__copyright__ = "Copyright 2022-2025, Levente Hunyadi"
+__license__ = "MIT"
+__maintainer__ = "Levente Hunyadi"
+__status__ = "Production"
