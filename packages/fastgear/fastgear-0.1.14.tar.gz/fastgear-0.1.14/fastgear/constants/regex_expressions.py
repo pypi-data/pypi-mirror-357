64e2e359106a64ae9ed120ff9e606c752006ef94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:05 +0000

    Add package fastgear/0.1.14/fastgear-0.1.14.tar.gz

---
+++ packages/fastgear/fastgear-0.1.14.tar.gz/fastgear-0.1.14/fastgear/constants/regex_expressions.py
@@ -0,0 +1,8 @@
+# Regular expression to match an ORDER BY clause with 'ASC' or 'DESC'.
+ORDER_BY_QUERY = "(ASC|DESC)$"
+
+# Regular expression to match any character (zero or more times).
+ANY_CHAR = ".*"
+
+# Regular expression to match logical operators 'AND' or 'OR'.
+OPERATOR = "(AND|OR)$"
