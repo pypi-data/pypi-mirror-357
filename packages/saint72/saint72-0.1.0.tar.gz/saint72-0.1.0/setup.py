12be957f44900f6727c1334fd264b1fbd58b5623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:05 +0000

    Add package saint72/0.1.0/saint72-0.1.0.tar.gz

---
+++ packages/saint72/saint72-0.1.0.tar.gz/saint72-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="saint72",
+    version="0.1.0",
+    description="Генератор файла с кодом в корне проекта",
+    author="Ваше Имя",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
