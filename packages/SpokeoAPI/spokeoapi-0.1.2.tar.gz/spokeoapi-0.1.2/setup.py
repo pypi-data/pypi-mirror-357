96593ed2fcd8394ff8102ef3f9fcc9e3a9dfa5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:56 +0000

    Add package SpokeoAPI/0.1.2/spokeoapi-0.1.2.tar.gz

---
+++ packages/SpokeoAPI/spokeoapi-0.1.2.tar.gz/spokeoapi-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SpokeoAPI',
+    version='0.1.2',  
+    description='official API wrapper for Spokeo lookup service',
+    long_description=open('readme.md').read(),
+    long_description_content_type='text/markdown',
+    author='Wei Zhang',
+    author_email='zhangwei.api@protonmail.com',
+    url='https://github.com/zhangxinxu',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        
+        'SpokeoAPI': [
+            'pyarmor_runtime_000000/*',
+        ],
+    },
+    install_requires=[
+        'requests',
+        'tls_client'
+    ],
+    python_requires='>=3.6',
+)
