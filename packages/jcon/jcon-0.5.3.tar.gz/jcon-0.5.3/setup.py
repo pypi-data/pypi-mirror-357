2489dbe238409800007c6ca752b606bd0de8bcf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:41 +0000

    Add package jcon/0.5.3/jcon-0.5.3.tar.gz

---
+++ packages/jcon/jcon-0.5.3.tar.gz/jcon-0.5.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="jcon",
+    version="0.5.3",
+    author="Jett S",
+    author_email="fallinganvil8@gmail.com",
+    description="Move Windows desktop icons programmatically in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # <- this tells PyPI to render Markdown
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
