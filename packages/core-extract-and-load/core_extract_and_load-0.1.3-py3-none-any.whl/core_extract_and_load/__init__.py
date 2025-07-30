c0a77d8fce972f368d18900906a0b6bc330f03ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:37 +0000

    Add package core-extract-and-load/0.1.3/core_extract_and_load-0.1.3-py3-none-any.whl

---
+++ packages/core-extract-and-load/core_extract_and_load-0.1.3-py3-none-any.whl/core_extract_and_load/__init__.py
@@ -0,0 +1,12 @@
+from .main import extract_and_load, ValueDTO, ResponseDTO, fetch_data, clean_data, save_data
+
+__all__ = [
+    'extract_and_load',
+    'ValueDTO', 
+    'ResponseDTO',
+    'fetch_data',
+    'clean_data',
+    'save_data'
+]
+
+__version__ = '0.1.0'
