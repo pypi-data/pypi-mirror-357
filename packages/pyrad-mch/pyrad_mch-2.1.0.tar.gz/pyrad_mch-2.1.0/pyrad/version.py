bafc0839055846d02d85592c615fb39b642a2e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:23:25 +0000

    Add package pyrad-mch/2.1.0/pyrad_mch-2.1.0.tar.gz

---
+++ packages/pyrad-mch/pyrad_mch-2.1.0.tar.gz/pyrad_mch-2.1.0/pyrad/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM PYRAD_PROC SETUP.PY
+short_version = '2.1.0'
+version = '2.1.0'
+full_version = '2.1.0'
+git_revision = '00c6a5a6c3721c8fefd56ebce7f3b5e3874eba1c'
+compile_date_time = '2025-06-24 11:38'
+username = 'runner'
+release = True
+
+if not release:
+    version = full_version
