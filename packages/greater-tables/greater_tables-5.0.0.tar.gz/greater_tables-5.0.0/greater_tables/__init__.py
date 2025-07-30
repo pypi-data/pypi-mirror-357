cefb1fd999f8d14484ab2e12802bd9792729f5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:50 +0000

    Add package greater-tables/5.0.0/greater_tables-5.0.0.tar.gz

---
+++ packages/greater-tables/greater_tables-5.0.0.tar.gz/greater_tables-5.0.0/greater_tables/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '5.0.0'
+__project__ = 'greater_tables'
+__author__ = 'Stephen J Mildenhall'
+
+from . core import GT
+from . fabrications import Fabricator
+from . etcher import Etcher
+
+# from . gtbreaks import Breakability
+
