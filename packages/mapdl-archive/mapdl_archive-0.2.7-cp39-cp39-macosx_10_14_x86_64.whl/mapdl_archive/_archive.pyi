fcbbf50949d766b76fa3f2bc08c76b93d6ebdd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:44 +0000

    Add package mapdl-archive/0.2.7/mapdl_archive-0.2.7-cp39-cp39-macosx_10_14_x86_64.whl

---
+++ packages/mapdl-archive/mapdl_archive-0.2.7-cp39-cp39-macosx_10_14_x86_64.whl/mapdl_archive/_archive.pyi
@@ -0,0 +1,48 @@
+from typing import TypeVar
+
+import numpy as np
+from numpy.typing import NDArray
+
+T = TypeVar("T", np.float32, np.float64)
+
+def write_nblock(
+    filename: str,
+    max_node_id: int,
+    node_id: NDArray[np.int32],
+    nodes: NDArray[np.float64],
+    angles: NDArray[np.float64],
+    sig_digits: int,
+    mode: str,
+) -> None: ...
+def write_eblock(
+    filename: str,
+    n_elem: int,
+    elem_id: NDArray[np.int32],
+    etype: NDArray[np.int32],
+    mtype: NDArray[np.int32],
+    rcon: NDArray[np.int32],
+    elem_nnodes: NDArray[np.int32],
+    celltypes: NDArray[np.uint8],
+    offset: NDArray[np.int32],
+    cells: NDArray[np.int32],
+    typenum: NDArray[np.int32],
+    nodenum: NDArray[np.int32],
+    mode: str,
+) -> None: ...
+def cmblock_items_from_array(array: NDArray[np.int32]) -> NDArray[np.int32]: ...
+def reset_midside(
+    celltypes: NDArray[np.uint8],
+    cells: NDArray[np.int32],
+    offset: NDArray[np.int32],
+    points: NDArray[T],
+) -> None: ...
+def overwrite_nblock(
+    filename_in: str,
+    filename_out: str,
+    nodes: NDArray[np.float64],
+    nblock_start: int,
+    ilen: int,
+    width: int,
+    d: int,
+    e: int,
+) -> None: ...
