b57d25f1a7585391084306c72522dff2f708e740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:24 +0000

    Add package ob-metaflow-stubs/6.0.3.184/ob_metaflow_stubs-6.0.3.184-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.184-py2.py3-none-any.whl/metaflow-stubs/info_file.pyi
@@ -0,0 +1,17 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-25T20:19:31.217247                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+CURRENT_DIRECTORY: str
+
+INFO_FILE: str
+
+def read_info_file():
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.184-py2.py3-none-any.whl/metaflow-stubs/metadata_provider/util.pyi
@@ -0,0 +1,19 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-25T20:19:31.248510                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def copy_tree(src, dst, update = False):
+    ...
+
+def sync_local_metadata_to_datastore(metadata_local_dir, task_ds):
+    ...
+
+def sync_local_metadata_from_datastore(metadata_local_dir, task_ds):
+    ...
+
